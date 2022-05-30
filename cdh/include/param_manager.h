1a8f6e06166c6cd1d2557cce90e85f955e8d4f2d
dgobalak <dgobalak@uwaterloo.ca>
2022-05-30 19:37:16 -0400

    Added .h and .c files for the parameter manager

---
+++ cdh/include/param_manager.h
@@ -0,0 +1,4 @@
+#ifndef PARAM_MANAGER_H
+#define PARAM_MANAGER_H
+
+#endif /* PARAM_MANAGER_H */
--- main.c
+++ main.c
@@ -1,11 +1,39 @@
-#include "gio.h"    // Imports the driver functions to interact with gio pins
+#include "sys_common.h"
+#include "gio.h"
 
-int main(void) {
-    gioInit();      // Initializes the gio Module
-    while (1) {
-        if (!gioGetBit(gioPORTB, 2)) {      // remember hardcoded pull up
-            gioToggleBit(gioPORTB, 1);      // toggles the gio bit that controls the led
-        }
+/* FreeRTOS Kernel includes. */
+#include "FreeRTOS.h"
+#include "os_portmacro.h"
+#include "os_task.h"
+
+#define BLINKY_STACK_SIZE 1024
+
+#define BLINK_NAME "Blink"
+// #define BLINK_PRIORITY 5
+#define BLINK_DELAY_TICKS 800
+
+xTaskHandle xTask1Handle;
+
+void blinkLED(void)
+{
+    while (1)
+    {
+        gioToggleBit(gioPORTB, 1);
+        vTaskDelay(BLINK_DELAY_TICKS);
     }
-    return 0;
+}
+
+int main(void)
+{
+    gioInit(); // Initializes the gio Module
+    BaseType_t retval1;
+
+    retval1 = xTaskCreate(blinkLED, BLINK_NAME, configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, &xTask1Handle);
+
+    if (retval1)
+    {
+        vTaskStartScheduler();
+    }
+
+    return 1;
 }
