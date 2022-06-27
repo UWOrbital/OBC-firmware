47b85ef6f1ba60bbcb4b49527974a4536592bbef
dgobalak <dgobalak@uwaterloo.ca>
2022-06-26 20:32:36 -0400

    Moved required posix functions to posix_utils directory

--- cdh/testing/main.c
+++ cdh/testing/main.c
@@ -1,11 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include <signal.h>
-#include <errno.h>
-#include <sys/select.h>
-
 #include "FreeRTOS.h"
 #include "os_task.h"
 
@@ -17,11 +9,6 @@
     #define BUILD         "./"
 #endif
 
-/* This demo uses heap_3.c (the libc provided malloc() and free()). */
-
-void vApplicationMallocFailedHook( void );
-void vApplicationIdleHook( void );
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char * pcTaskName );
 void dummy_task();
 
 int main( void ) {
@@ -39,36 +26,4 @@ void dummy_task() {
         vTaskDelay(xDelay);
         console_print("Hello World!\n");
     }
-}
-
-void vApplicationMallocFailedHook( void ) {
-    vAssertCalled( __FILE__, __LINE__ );
-}
-
-void vApplicationIdleHook( void ) {
-    usleep( 15000 );
-}
-
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char * pcTaskName ) {
-    ( void ) pcTaskName;
-    ( void ) pxTask;
-    vAssertCalled( __FILE__, __LINE__ );
-}
-
-void vAssertCalled( const char * const pcFileName, unsigned long ulLine )
-{
-    static BaseType_t xPrinted = pdFALSE;
-    volatile uint32_t ulSetToNonZeroInDebuggerToContinue = 0;
-    ( void ) ulLine;
-    ( void ) pcFileName;
-    taskENTER_CRITICAL(); {
-        if( xPrinted == pdFALSE ) {
-            xPrinted = pdTRUE;
-        }
-        while( ulSetToNonZeroInDebuggerToContinue == 0 ) {
-            __asm volatile ( "NOP" );
-            __asm volatile ( "NOP" );
-        }
-    }
-    taskEXIT_CRITICAL();
 }
