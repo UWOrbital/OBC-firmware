fdaf9863898c4743f1464d4e96a69dc0eb2c33d7
dgobalak <dgobalak@uwaterloo.ca>
2022-07-01 23:24:33 -0400

    Enabled I2C in Halcogen

--- hal/source/notification.c
+++ hal/source/notification.c
@@ -52,6 +52,7 @@
 #include "sys_selftest.h"
 #include "gio.h"
 #include "sci.h"
+#include "i2c.h"
 #include "sys_dma.h"
 
 /* USER CODE BEGIN (0) */
@@ -100,6 +101,15 @@ void gioNotification(gioPORT_t *port, uint32 bit)
 }
 
 /* USER CODE BEGIN (20) */
+/* USER CODE END */
+void i2cNotification(i2cBASE_t *i2c, uint32 flags)      
+{
+/*  enter user code between the USER CODE BEGIN and USER CODE END. */
+/* USER CODE BEGIN (21) */
+/* USER CODE END */
+}
+
+/* USER CODE BEGIN (22) */
 /* USER CODE END */
 
 void sciNotification(sciBASE_t *sci, uint32 flags)     
