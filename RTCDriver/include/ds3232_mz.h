74457acfb29273b8b4d94320352a0aede824fc2e
ShourryaGuha <guhashourrya@gmail.com>
2022-07-13 23:39:45 -0400

    added required registers

---
+++ RTCDriver/include/ds3232_mz.h
@@ -0,0 +1,14 @@
+#ifndef DS3232_MZH
+#define DS3232_MZH
+
+#include "stdint.h"
+
+/*DS3232 registers*/
+
+#define DS3232_SECONDS      0x00
+#define DS3232_MINUTES      0x01
+#define DS3232_HOURS        0x02
+#define DS3232_DAY          0x03
+#define DS3232_DATE         0X04
+#define DS3232_MONTH        0x05
+#define DS3232_YEAR         0x06
