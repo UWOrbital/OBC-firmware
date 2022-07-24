c24082b07549d4f89bd641114f921275e3197ae4
ShourryaGuha <guhashourrya@gmail.com>
2022-07-24 16:57:50 -0400

    done with everything except hours and time

--- RTCDriver/source/ds3232_mz.c
+++ RTCDriver/source/ds3232_mz.c
@@ -4,7 +4,7 @@
 
 #include "stdio.h"
 const uint8_t LOW_BIT_MASK = 15; //00001111
-const uint8_t HIGH_BIT_MASK = 112; // 01110000
+const uint8_t HIGH_BIT_MASK = 240; // 01110000
 
 uint8_t get_rtc_seconds(uint8_t *seconds) {
     uint8_t data[1];
@@ -24,10 +24,20 @@ uint8_t get_rtc_minutes(uint8_t *minutes) {
         sci_print_text((uint8_t*)"Failed to read minutes data\r\n", 35);
         return 0;
     }
-    *minutes = data[0];
+    uint8_t one_minutes = data[0] & LOW_BIT_MASK;
+    uint8_t ten_minutes = (data[0] & HIGH_BIT_MASK) >> 4;
+
+    *minutes = ten_minutes*10 + one_minutes;;
 }
 
-// uint8_t get_rtc_hour(uint8_t *hours) /having a hard time understanfing how data will be sent
+uint8_t get_rtc_hour(uint8_t *hours){
+    uint8_t data[1];
+    if(i2c_read_register(DS3232_I2C_ADDRESS, DS3232_HOURS, data, 1) == 0) {
+        sci_print_text((uint8_t*)"Failed to read hours data\r\n", 35);
+        return 0;
+    }
+
+} //having a hard time understanfing how data will be sent
 
 uint8_t get_rtc_time(int time[3]) {
     uint8_t min, sec, hours;
@@ -45,7 +55,10 @@ uint8_t get_rtc_date(uint8_t* date) {
         sci_print_text((uint8_t*)"Failed to read date data\r\n", 35);
         return 0;
     }
-    *date = data[0];
+    uint8_t one_date = data[0] & LOW_BIT_MASK;
+    uint8_t ten_date = (data[0] & HIGH_BIT_MASK) >> 4;
+
+    *date = ten_date*10 + one_date;
 }
 
 uint8_t get_rtc_year(uint8_t* year) {
@@ -54,5 +67,8 @@ uint8_t get_rtc_year(uint8_t* year) {
         sci_print_text((uint8_t*)"Failed to read year data\r\n", 35);
         return 0;
     }
-    *year = data[0];
+    uint8_t one_year = data[0] & LOW_BIT_MASK;
+    uint8_t ten_year = (data[0] & HIGH_BIT_MASK) >> 4;
+
+    *year = ten_year*10 + one_year;
 }
