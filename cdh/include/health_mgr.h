1b53e62a9ccd631c01166222e42d4a7396dd7248
CyStorm <fumblevore53664337@hotmail.com>
2022-11-11 20:05:11 -0500

    health parameter type and setter methods

--- cdh/include/health_mgr.h
+++ cdh/include/health_mgr.h
@@ -3,16 +3,43 @@
 
 #include <sys_common.h>
 
-#define HEALTH_MGR_POLL_TIME 10     //placeholder
+#define HEALTH_MGR_POLL_TIME 69     //placeholder
+#define NUM_HEALTH_PARAMS 2     //placeholder
 
 
-#define HEALTH_STATUS_NORMAL 0
-#define HEALTH_STATUS_BAD1 1
-#define HEALTH_STATUS_BAD2 2
+typedef enum {
+    HEALTH_STATUS_NORMAL,
+    HEALTH_STATUS_BAD1,
+    HEALTH_STATUS_BAD2,
+} health_status_t;
+
+// typedef union {
+//     int i;
+//     float f;
+// } health_param_data_t;
+
+
+
+typedef enum {
+    HEALTH_PARAM_VERIF_EQUALS,
+    HEALTH_PARAM_VERIF_GREATER_THAN,
+    HEALTH_PARAM_VERIF_LESS_THAN,
+} health_param_verif_t;
+
+
+typedef struct {
+    uint8_t id;
+    uint32_t value;
+    uint32_t expected;
+    health_param_verif_t verif;
+} health_param_t;
 
 
 void initHealthMgr(void);
 void heathMgrTakeAction(void);
+void heathParamSetExpected(health_param_t* param);
+void heathParamSetValue(health_param_t* param);
+uint8_t heathVerifParam(health_param_t* param);
 
 
 
--- cdh/source/health_mgr.c
+++ cdh/source/health_mgr.c
@@ -1,40 +1,71 @@
 #include "health_mgr.h"
 
 
-static uint8_t healthStatusCode;
+static health_status_t healthStatusCode;
+
+static health_param_t paramStore[NUM_HEALTH_PARAMS];
 
 /**
  * @brief	HealthMgr task.
  * @param	pvParameters	Task parameters.
  */
-static void vHealthMgrTask(void * pvParameters);
+static void vHealthMgrTask(void* pvParameters);
 
 
 void initHealthMgr(void) {
-    healthStatusCode = 0;
-    
+    healthStatusCode = HEALTH_STATUS_NORMAL;
+
+}
+
+void heathParamSetExpected(health_param_t* param) {
+    uint32_t exp = 0;
+    param->expected = exp;
+}
+
+void heathParamSetValue(health_param_t* param) {
+    uint32_t val = 0;
+    param->value = val;
+}
+
+uint8_t heathVerifParam(health_param_t* param) {
+    uint8_t result = 0;     // do it like this instead of return in case, if we want to indicate 0 success or 1 success
+    switch (param->verif) {
+        case HEALTH_PARAM_VERIF_EQUALS:
+            result = param->value == param->expected;
+            break;
+        case HEALTH_PARAM_VERIF_GREATER_THAN:
+            result = param->value > param->expected;
+            break;
+        case HEALTH_PARAM_VERIF_LESS_THAN:
+            result = param->value < param->expected;
+            break;
+        default:
+            // some debug should handle every case
+            break;
+    }
+    return result;
 }
 
 void healthMgrTakeAction(void) {
     switch (healthStatusCode) {
-    case HEALTH_STATUS_NORMAL:
-        // do nothing
-        break;
-    case HEALTH_STATUS_BAD1:
-        // send a command to do something
-        break;
-    case HEALTH_STATUS_BAD2:
-        // do another thing
-        break;
-    default:
-        // some debug should handle every case
-        break;
-    }
+        case HEALTH_STATUS_NORMAL:
+            // do nothing
+            break;
+        case HEALTH_STATUS_BAD1:
+            // send a command to do something
+            break;
+        case HEALTH_STATUS_BAD2:
+            // do another thing
+            break;
+        default:
+            // some debug should handle every case
+            break;
+        }
 }
 
-static void vHealthMgrTask(void * pvParameters) {
+static void vHealthMgrTask(void* pvParameters) {
     while (1) {
-
+        // some logic here to set the health status 
         healthMgrTakeAction();
     }
 }
