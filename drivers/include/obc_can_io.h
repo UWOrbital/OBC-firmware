1bdae68005b2e603bda65a983e6b63aa0ea062d1
Nathan Tang <nathantangca@outlook.com>
2022-11-12 15:30:23 -0500

    Added functions for transmiting and recieving CAN messages

--- drivers/include/obc_can_io.h
+++ drivers/include/obc_can_io.h
@@ -8,4 +8,24 @@
  */
 void initCANMutex(void);
 
+/**
+ * @brief Send a buffer of bytes to the CAN bus
+ * @param canReg Pointer to CAN node 
+ * @param messageBox Message box number of CAN node
+ * @param data TX data to transmit
+ * @return 1 if succcesful, 0 if not
+*/
+uint8_t canSendMessage(canBASE_t *canReg, uint32_t messageBox, const uint8_t *data);
+
+/**
+ * @brief Recieve a buffer of bytes from the CAN bus
+ * @param canReg Pointer to CAN node 
+ * @param messageBox Message box number of CAN node
+ * @param data RX data to recieve
+ * @return 1 if succcesful, 0 if not
+*/
+uint8_t canGetMessage(canBASE_t *canReg, uint32_t messageBox, const uint8_t *data);
+
+
+
 #endif
--- drivers/source/obc_can_io.c
+++ drivers/source/obc_can_io.c
@@ -18,3 +18,25 @@ void initCANMutex(void) {
 
     configASSERT(canMutex);
 }
+
+uint8_t canSendMessage(canBASE_t *canReg, uint32_t messageBox, const uint8_t *txData) {
+    ASSERT(canMutex != NULL);
+
+    uint8_t status = 0;
+    // might not need mutex protection because of arbitration embedded in CAN protocol
+    if(xSemaphoreTake(canMutex, portMAX_DELAY) == pdTRUE) {
+        taskENTER_CRITICAL(); // might not be needed
+
+        status = canTransmit(canReg, messageBox, txData);
+
+        taskEXIT_CRITICAL();
+    }
+
+    return status;
+}
+
+uint8_t canGetMessage(canBASE_t *canReg, uint32_t messageBox, const uint8_t *rxData) {
+    while(!canIsRxMessageArrived(canReg, messageBox));
+    
+    return canGetData(canReg, messageBox, rxData);
+}
