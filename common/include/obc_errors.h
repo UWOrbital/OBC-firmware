5925a32f22b0a51a06ab1b34bc8d3223610db270
dgobalak <dgobalak@uwaterloo.ca>
2022-11-13 20:58:48 -0500

    Removed stuff

--- common/include/obc_errors.h
+++ common/include/obc_errors.h
@@ -1,14 +1,6 @@
 #ifndef COMMON_INCLUDE_OBC_ERRORS_H_
 #define COMMON_INCLUDE_OBC_ERRORS_H_
 
-#define NLK_ERR_CODE_SUCCESS     (uint32_t) 0
-#define NLK_ERR_CODE_UNKNOWN     (uint32_t) 1
-#define NLK_ERR_CODE_TIMEOUT     (uint32_t) 2
-#define NLK_ERR_CODE_INVALID_ARG (uint32_t) 3
-#define NLK_ERR_CODE_INVALID_STATE  (uint32_t) 4
-#define NLK_ERR_CODE_BUS_BUSY (uint32_t) 5
-
-
 typedef enum {
     /* Common Errors 0 - 99 */
     OBC_ERR_CODE_SUCCESS = 0,
