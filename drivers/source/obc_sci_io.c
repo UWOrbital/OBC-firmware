4a44ec6013b2ce5a176e72332e47be480795d3b9
Daniel Gobalakrishnan <63618806+dgobalak@users.noreply.github.com>
2022-11-16 15:09:29 -0500

    Defined UART_PRINT_REG to choose UART based on the board (#28)
    * Defined a better macro name for sciREG and scilinREG
    
    * Renamed printTextSci to sciPrintText
    
    * Used configASSERT in obc_sci_io.c
    
    Co-authored-by: Max <maxjiang123@gmail.com>

--- drivers/source/obc_sci_io.c
+++ drivers/source/obc_sci_io.c
@@ -1,6 +1,7 @@
 #include "obc_sci_io.h"
 
 #include <FreeRTOS.h>
+#include <FreeRTOSConfig.h>
 #include <os_portmacro.h>
 #include <os_semphr.h>
 #include <os_task.h>
@@ -10,6 +11,7 @@
 #include <stdio.h>
 
 #define MAX_PRINTF_SIZE 128U
+#define UART_MUTEX_BLOCK_TIME portMAX_DELAY
 
 static SemaphoreHandle_t sciMutex = NULL;
 static StaticSemaphore_t sciMutexBuffer;
@@ -17,13 +19,12 @@ static SemaphoreHandle_t sciLinMutex = NULL;
 static StaticSemaphore_t sciLinMutexBuffer;
 
 /**
- * @brief Iterate through an array of bytes and transmit them via SCI or SCI2.
+ * @brief Iterate through an array of bytes and transmit them via UART_PRINT_REG.
  * 
- * @param sci The SCI register to use (sciREG or scilinREG).
  * @param bytes The array of bytes to transmit.
  * @param length The length of the array of bytes to transmit.
  */
-static void sciSendBytes(sciBASE_t *sci, unsigned char *bytes, uint32_t length);
+static void sciSendBytes(unsigned char *bytes, uint32_t length);
 
 void initSciMutex(void) {
     if (sciMutex == NULL) {
@@ -37,41 +38,34 @@ void initSciMutex(void) {
     configASSERT(sciLinMutex);
 }
 
-uint8_t printTextSci(sciBASE_t *sci, unsigned char *text, uint32_t length) {
-    /* initSciMutex must be called before printing is allowed */
-    ASSERT(sciMutex != NULL && sciLinMutex != NULL);
+uint8_t sciPrintText(unsigned char *text, uint32_t length) {
+    configASSERT(text != NULL);
+    configASSERT((UART_PRINT_REG == sciREG) || (UART_PRINT_REG == scilinREG));
 
-    if (sci == scilinREG) {
-        if (sciLinMutex != NULL) {
-            if (xSemaphoreTake(sciLinMutex, portMAX_DELAY) == pdTRUE) {
-                sciSendBytes(sci, text, length);
-                xSemaphoreGive(sciLinMutex);
-                return 1;
-            }
-        }
-    } else if (sci == sciREG) {
-        if (sciMutex != NULL) {
-            if (xSemaphoreTake(sciMutex, portMAX_DELAY) == pdTRUE) {
-                sciSendBytes(sci, text, length);
-                xSemaphoreGive(sciMutex);
-                return 1;
-            }
+    SemaphoreHandle_t mutex = (UART_PRINT_REG == sciREG) ? sciMutex : sciLinMutex;
+
+    if (mutex != NULL){
+        if (xSemaphoreTake(mutex, UART_MUTEX_BLOCK_TIME) == pdTRUE){
+            sciSendBytes(text, length);
+            xSemaphoreGive(mutex);
+            return 1;
         }
     }
+    
     return 0;
 }
 
-static void sciSendBytes(sciBASE_t *sci, unsigned char *bytes, uint32_t length) {
+static void sciSendBytes(unsigned char *bytes, uint32_t length) {
     for (int i = 0; i < length; i++) {
         if (bytes[i] == '\0')
             return;
         // sciSendByte waits for the transmit buffer to be empty before sending
-        sciSendByte(sci, bytes[i]);
+        sciSendByte(UART_PRINT_REG, bytes[i]);
     }
 }
 
-uint8_t sciPrintf(sciBASE_t *sci, const char *s, ...){
-    if (sci == NULL || s == NULL){
+uint8_t sciPrintf(const char *s, ...){
+    if (s == NULL){
         return 0;
     }
 
@@ -87,11 +81,11 @@ uint8_t sciPrintf(sciBASE_t *sci, const char *s, ...){
         return 0;
     }
 
-    uint8_t ret = printTextSci(sci, (unsigned char *)buf, MAX_PRINTF_SIZE);
+    uint8_t ret = sciPrintText((unsigned char *)buf, MAX_PRINTF_SIZE);
     return ret;
 }
 
 void uartAssertFailed(char *file, int line, char *expr){
-    sciPrintf(scilinREG, "ASSERTION FAILED: %s, file %s, line %d\r\n",
+    sciPrintf("ASSERTION FAILED: %s, file %s, line %d\r\n",
                             expr, file, line);
 }
