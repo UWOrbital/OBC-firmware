712590667c11296f189aef6a7a440431c9e25493
dgobalak <dgobalak@uwaterloo.ca>
2022-12-20 01:15:00 -0500

    Updated demo code

--- examples/sd_reliance_edge_demo/main.c
+++ examples/sd_reliance_edge_demo/main.c
@@ -16,47 +16,79 @@ static StackType_t taskStack[1024];
 
 void vTask1(void *pvParameters) {
     const char * fname = "/demo.txt";
+    int32_t ret; 
 
-    int32_t ret;    
     ret = red_init();
-    sciPrintf("red_init() returned %d\r\n", ret);
+    if (ret != 0) {
+        sciPrintf("red_init() returned %d\r\n", ret);
+        while (1);
+    }
 
-    if(ret == 0) {
-        ret = red_format("");
+    ret = red_format("");
+    if (ret != 0) {
         sciPrintf("red_format() returned %d\r\n", ret);
-        if(ret == 0) {
-            ret = red_mount("");
-            sciPrintf("red_mount() returned %d\r\n", ret);
-            
-            if (ret == 0) {
-                vTaskDelay(1000);
-                int32_t file = red_open(fname, RED_O_RDWR | RED_O_CREAT);
-                sciPrintf("red_open() opened %d\r\n", file);
+        while (1);
+    }
 
-                ret = red_write(file, "TESTING 1...2...3\r\n", strlen("TESTING 1...2...3\r\n"));
-                sciPrintf("red_write() returned %d\r\n", ret);
+    ret = red_mount("");
+    if (ret != 0) {
+        sciPrintf("red_mount() returned %d\r\n", ret);
+        while (1);
+    }
 
-                ret = red_close(file);
-                sciPrintf("red_close() returned %d\r\n", ret);
+    sciPrintf("Reliance Edge initialized and mounted\r\n");
 
-                file = red_open(fname, RED_O_RDWR);
-                sciPrintf("red_open() opened %d\r\n", file);
+    int32_t file = red_open(fname, RED_O_RDWR | RED_O_CREAT);
+    if (file < 0) {
+        sciPrintf("red_open() returned %d\r\n", file);
+        while (1);
+    }
+    sciPrintf("Successfully opened %s\r\n", fname);
+    
+    ret = red_write(file, "TESTING 1...2...3\r\n", strlen("TESTING 1...2...3\r\n"));
+    if (ret < 0) {
+        sciPrintf("red_write() returned %d\r\n", ret);
+        while (1);
+    }
+    sciPrintf("Successfully wrote to %s\r\n", fname);
 
-                unsigned char readBuf[100] = {0};
-                ret = red_read(file, readBuf, 100);
-                sciPrintf("red_read() returned %d\r\n", ret);
+    ret = red_close(file);
+    if (ret < 0) {
+        sciPrintf("red_close() returned %d\r\n", ret);
+        while (1);
+    }
+    sciPrintf("Successfully closed %s\r\n", fname);
 
-                if (ret > 0)
-                    sciPrintText(readBuf, ret);
+    file = red_open(fname, RED_O_RDWR);
+    if (file < 0) {
+        sciPrintf("red_open() returned %d\r\n", file);
+        while (1);
+    }
+    sciPrintf("Successfully opened %s\r\n", fname);
 
-                ret = red_close(file);
-                sciPrintf("red_close() returned %d\r\n", ret);
+    unsigned char readBuf[100] = {0};
+    ret = red_read(file, readBuf, 100);
+    if (ret < 0) {
+        sciPrintf("red_read() returned %d\r\n", ret);
+        while (1);
+    }
+    sciPrintf("Successfully read from %s\r\n", fname);
 
-                ret = red_unlink(fname);
-                sciPrintf("red_unlink() returned %d\r\n", ret);
+    sciPrintf("Text read from %s: ", fname);
+    sciPrintText(readBuf, ret);
 
-            }
-        }
+    ret = red_close(file);
+    if (ret < 0) {
+        sciPrintf("red_close() returned %d\r\n", ret);
+        while (1);
+    }
+    sciPrintf("Successfully closed %s\r\n", fname);
+
+    ret = red_unlink(fname);
+    if (ret < 0) {
+        sciPrintf("red_unlink() returned %d\r\n", ret);
+    } else {
+        sciPrintf("Successfully unlinked %s\r\n", fname);
     }
     
     while(1);
@@ -68,6 +100,8 @@ int main(void) {
     
     initSciMutex();
     initSpiMutex();
+
+    sciPrintf("Starting Reliance Edge Demo\r\n");
 
     xTaskCreateStatic(vTask1, "RelianceEdgeDemo", 1024, NULL, 1, taskStack, &taskBuffer);
 
