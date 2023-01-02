6f90ab50909477a758be337ae703b57a5807ac29
Nathan Tang <nathantangca@outlook.com>
2023-01-01 20:59:56 -0500

    removed implicit declaration of static functions from .h

--- drivers/include/adc_helper.h
+++ drivers/include/adc_helper.h
@@ -8,50 +8,6 @@
 #define FIFOSIZE 6U
 
 /**
- * @brief Function to start conversion of a single adc channel
- * 
- * @param adc Pointer to ADC Module
- *        - adcREG1: ADC1 module pointer
- *        - adcREG2: ADC2 module pointer
- * @param channel ADC channel to be converted
- * @param fifo_size Size of FIFO queue to store converted values
- * @param group ADC group to be converted
- * 
- * @return 1 if successful, 0 if not 
- */ 
-
-static uint8_t adcStartConversion_selChn(adcBASE_t *adc, uint8_t channel, uint8_t fifoSize, uint8_t group);
-
-/**
- * @brief Function to get ADC converted data
- * 
- * @param adc Pointer to ADC Module
- *        - adcREG1: ADC1 module pointer
- *        - adcREG2: ADC2 module pointer
- * @param group ADC group to get converted data from
- * @param data Pointer to store ADC converted data
- * 
- * @return 1 if successful, 0 if not 
- */ 
-
-static uint8_t adcGetSingleData(adcBASE_t *adc, uint8_t group, adcData_t *data);
-
-/**
- * @brief Function to get ADC digital voltage
- * 
- * @param adc Pointer to ADC Module
- *        - adcREG1: ADC1 module pointer
- *        - adcREG2: ADC2 module pointer
- * @param group ADC group to be converted
- * @param channel ADC channel to be converted
- * @param adcData Pointer to converted data
- * 
- * @return 1 if successful, 0 if not 
- */ 
-
-static uint8_t adcDigitalVoltage(adcBASE_t *adc, uint8_t group, uint8_t channel, adcData_t *adcData);
-
-/**
  * @brief Function to get ADC analog voltage
  * 
  * @param adc Pointer to ADC Module
--- drivers/source/adc_helper.c
+++ drivers/source/adc_helper.c
@@ -3,6 +3,19 @@
 #include <FreeRTOS.h>
 #include <os_semphr.h>
 
+/**
+ * @brief Function to start conversion of a single adc channel
+ * 
+ * @param adc Pointer to ADC Module
+ *        - adcREG1: ADC1 module pointer
+ *        - adcREG2: ADC2 module pointer
+ * @param channel ADC channel to be converted
+ * @param fifo_size Size of FIFO queue to store converted values
+ * @param group ADC group to be converted
+ * 
+ * @return 1 if successful, 0 if not 
+ */ 
+
 static uint8_t adcStartConversion_selChn(adcBASE_t *adc, uint8_t channel, uint8_t fifoSize, uint8_t group) {
     if(adc == NULL) {
         return 0;
@@ -14,6 +27,18 @@ static uint8_t adcStartConversion_selChn(adcBASE_t *adc, uint8_t channel, uint8_
 
     return 1;
 }
+
+/**
+ * @brief Function to get ADC converted data
+ * 
+ * @param adc Pointer to ADC Module
+ *        - adcREG1: ADC1 module pointer
+ *        - adcREG2: ADC2 module pointer
+ * @param group ADC group to get converted data from
+ * @param data Pointer to store ADC converted data
+ * 
+ * @return 1 if successful, 0 if not 
+ */ 
 
 static uint8_t adcGetSingleData(adcBASE_t *adc, uint8_t group, adcData_t *data) {
     if((adc == NULL) || (data == NULL)) {
@@ -31,6 +56,19 @@ static uint8_t adcGetSingleData(adcBASE_t *adc, uint8_t group, adcData_t *data) 
 
     return 1;
 }
+
+/**
+ * @brief Function to get ADC digital voltage
+ * 
+ * @param adc Pointer to ADC Module
+ *        - adcREG1: ADC1 module pointer
+ *        - adcREG2: ADC2 module pointer
+ * @param group ADC group to be converted
+ * @param channel ADC channel to be converted
+ * @param adcData Pointer to converted data
+ * 
+ * @return 1 if successful, 0 if not 
+ */ 
 
 static uint8_t adcDigitalVoltage(adcBASE_t *adc, uint8_t group, uint8_t channel, adcData_t *adcData) {
     if((adc == NULL) || (adcData == NULL)) {
