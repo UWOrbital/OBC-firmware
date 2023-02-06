23a5eff4fe3b96220a2820fce129316b53eef20f
dgobalak <dgobalak@uwaterloo.ca>
2023-02-05 20:16:36 -0800

    Updated to use RETURN_IF_ERROR_CODE macro

--- drivers/include/lm75bd.h
+++ drivers/include/lm75bd.h
@@ -5,56 +5,18 @@
 
 #include <stdint.h>
 
-/* LM75BD I2C Device Addresses */
-#define LM75BD_BASE_I2C_ADDR 0x9U
-#define LM75BD_OBC_I2C_ADDR 0x4FU /* (LM75BD_BASE_I2C_ADDR << 3) | 0x7 */
-
-/* LM75BD Registers (p.8) */
-#define LM75BD_REG_TEMP 0x00U  /* Temperature Register (R) */
-#define LM75BD_REG_CONF 0x01U  /* Configuration Register (R/W) */
-#define LM75BD_REG_THYST 0x02U /* Hysteresis Register (R/W) */
-#define LM75BD_REG_TOS 0x03U   /* Overtemperature Shutdown Register (R/W) */
+/* LM75BD I2C Device Address */
+#define LM75BD_OBC_I2C_ADDR 0x4FU /* (0x9U << 3) | 0x7 */
 
 /* LM75BD Configuration Values */
 #define LM75BD_DEV_OP_MODE_NORMAL 0x00U
 #define LM75BD_DEV_OP_MODE_SHUTDOWN 0x01U
+
 #define LM75BD_OS_POL_ACTIVE_LOW 0x00U
 #define LM75BD_OS_POL_ACTIVE_HIGH 0x01U
+
 #define LM75BD_OS_OP_MODE_COMP 0x00U
 #define LM75BD_OS_OP_MODE_INT 0x01U
-
-/* LM75BD Default State (p.12) */
-#define LM75BD_DEFAULT_DEV_OP_MODE LM75BD_DEV_OP_MODE_NORMAL
-#define LM75BD_DEFAULT_OS_OP_MODE LM75BD_OS_OP_MODE_COMP
-#define LM75BD_DEFAULT_TEMP_TOS 80.0f /* Degrees Celsius */
-#define LM75BD_DEFAULT_TEMP_HYS 75.0f /* Degrees Celsius */
-#define LM75BD_DEFAULT_OS_POL LM75BD_OS_POL_ACTIVE_LOW
-#define LM75BD_DEFAULT_OS_FAULT_QUEUE_SIZE 1U /* OS_F_QUE = 0 */
-
-/* LM75BD Temperature Resolutions */
-#define LM75BD_THYST_RES 0.5f /* Degrees Celsius */
-#define LM75BD_TOS_RES 0.5f /* Degrees Celsius */
-#define LM75BD_TEMP_RES 0.125f /* Degrees Celsius */
-
-/* LM75BD Number of Bits in Right Padding */
-#define LM75BD_THYST_LSB_PADDING 7U /* Number of bits */
-#define LM75BD_TOS_LSB_PADDING 7U /* Number of bits */
-#define LM75BD_TEMP_LSB_PADDING 5U /* Number of bits */
-
-/* Buffer Size of Registers (i.e. Number of Bytes to R/W) */
-#define LM75BD_TEMP_BUFF_SIZE 2U
-#define LM75BD_CONF_BUFF_SIZE 1U
-#define LM75BD_THYST_BUFF_SIZE 2U
-#define LM75BD_TOS_BUFF_SIZE 2U
-
-/* LM75BD TOS and THYST Limits */
-#define LM75BD_TEMP_THRESH_MAX 127.5f /* Degrees Celsius */
-
-/* LM75BD Config Reg Bit Masks */
-#define LM75BD_OS_FAULT_QUEUE_MASK 0b11000
-#define LM75BD_OS_POL_MASK 0b100
-#define LM75BD_OS_OP_MODE_MASK 0b010
-#define LM75BD_DEV_OP_MODE_MASK 0b001
 
 /**
  * @struct Configuration struct for LM75BD,118 temperature sensor
--- drivers/source/lm75bd.c
+++ drivers/source/lm75bd.c
@@ -1,12 +1,40 @@
 #include "lm75bd.h"
 #include "obc_i2c_io.h"
-#include "logging.h"
+#include "obc_logging.h"
 #include "obc_errors.h"
 
 #include <stdint.h>
 #include <math.h>
 
+/* LM75BD Registers (p.8) */
+#define LM75BD_REG_TEMP 0x00U  /* Temperature Register (R) */
+#define LM75BD_REG_CONF 0x01U  /* Configuration Register (R/W) */
+#define LM75BD_REG_THYST 0x02U /* Hysteresis Register (R/W) */
+#define LM75BD_REG_TOS 0x03U   /* Overtemperature Shutdown Register (R/W) */
+
+/* LM75BD Temperature Resolutions */
+#define LM75BD_THYST_RES 0.5f /* Degrees Celsius */
+#define LM75BD_TOS_RES 0.5f /* Degrees Celsius */
+#define LM75BD_TEMP_RES 0.125f /* Degrees Celsius */
+
+/* Buffer Size of Registers (i.e. Number of Bytes to R/W) */
+#define LM75BD_TEMP_BUFF_SIZE 2U
+#define LM75BD_CONF_BUFF_SIZE 1U
+#define LM75BD_THYST_BUFF_SIZE 2U
+#define LM75BD_TOS_BUFF_SIZE 2U
+
+/* LM75BD TOS and THYST Limits */
+#define LM75BD_TEMP_THRESH_MAX 127.5f /* Degrees Celsius */
+
+/* LM75BD Config Reg Bit Masks */
+#define LM75BD_OS_FAULT_QUEUE_MASK 0b11000
+#define LM75BD_OS_POL_MASK 0b100
+#define LM75BD_OS_OP_MODE_MASK 0b010
+#define LM75BD_DEV_OP_MODE_MASK 0b001
+
 obc_error_code_t lm75bdInit(lm75bd_config_t *config) {
+    obc_error_code_t errCode;
+
     if (config == NULL)
         return OBC_ERR_CODE_INVALID_ARG;
     
@@ -14,31 +42,21 @@ obc_error_code_t lm75bdInit(lm75bd_config_t *config) {
     if (config->hysteresisThresholdCelsius >= config->overTempThresholdCelsius)
         return OBC_ERR_CODE_INVALID_ARG;
     
-    obc_error_code_t err;
-
-    err = writeConfigLM75BD(config->devAddr, config->osFaultQueueSize, config->osPolarity, config->osOperationMode, config->devOperationMode);
-    if (err != OBC_ERR_CODE_SUCCESS) return err;
-
-    err = writeThystLM75BD(config->devAddr, config->hysteresisThresholdCelsius);
-    if (err != OBC_ERR_CODE_SUCCESS) return err;
-    
-    err = writeTosLM75BD(config->devAddr, config->overTempThresholdCelsius);
-    if (err != OBC_ERR_CODE_SUCCESS) return err;
+    RETURN_IF_ERROR_CODE(writeConfigLM75BD(config->devAddr, config->osFaultQueueSize, config->osPolarity, config->osOperationMode, config->devOperationMode));
+    RETURN_IF_ERROR_CODE(writeThystLM75BD(config->devAddr, config->hysteresisThresholdCelsius));
+    RETURN_IF_ERROR_CODE(writeTosLM75BD(config->devAddr, config->overTempThresholdCelsius));
     
     return OBC_ERR_CODE_SUCCESS;
 }
 
 obc_error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
+    obc_error_code_t errCode;
     uint8_t tempBuff[LM75BD_TEMP_BUFF_SIZE] = {0};
 
     if (temp == NULL)
         return OBC_ERR_CODE_INVALID_ARG;
     
-    obc_error_code_t err = i2cReadReg(devAddr, LM75BD_REG_TEMP, tempBuff, LM75BD_TEMP_BUFF_SIZE);
-    if (err != OBC_ERR_CODE_SUCCESS) {
-        LOG_ERROR("Failed to read temperature from LM75BD - Addr 0x%02X", devAddr);
-        return err;
-    }
+    RETURN_IF_ERROR_CODE(i2cReadReg(devAddr, LM75BD_REG_TEMP, tempBuff, LM75BD_TEMP_BUFF_SIZE));
 
     /* Combine the 11 MSB into a 16-bit signed integer */
     int16_t value = ( (int8_t)tempBuff[0] << 3 ) | ( tempBuff[1]  >> 5 );
@@ -50,6 +68,7 @@ obc_error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
 }
 
 obc_error_code_t readConfigLM75BD(lm75bd_config_t *config) {
+    obc_error_code_t errCode;
     const uint8_t faultQueueMapping[] = {1, 2, 4, 6}; // Maps the fault queue bits to the falt queue size
 
     uint8_t configBuff[LM75BD_CONF_BUFF_SIZE] = {0};
@@ -57,11 +76,7 @@ obc_error_code_t readConfigLM75BD(lm75bd_config_t *config) {
     if (config == NULL)
         return OBC_ERR_CODE_INVALID_ARG;
 
-    obc_error_code_t err = i2cReadReg(config->devAddr, LM75BD_REG_CONF, configBuff, LM75BD_CONF_BUFF_SIZE);
-    if (err != OBC_ERR_CODE_SUCCESS) {
-        LOG_ERROR("Failed to read config from LM75BD - Addr 0x%02X", config->devAddr);
-        return err;
-    }
+    RETURN_IF_ERROR_CODE(i2cReadReg(config->devAddr, LM75BD_REG_CONF, configBuff, LM75BD_CONF_BUFF_SIZE));
 
     uint8_t osFaltQueueRegData = (configBuff[0] & LM75BD_OS_FAULT_QUEUE_MASK) >> 3;
     if (osFaltQueueRegData > 3)
@@ -78,6 +93,7 @@ obc_error_code_t readConfigLM75BD(lm75bd_config_t *config) {
 obc_error_code_t writeConfigLM75BD(uint8_t devAddr, uint8_t osFaultQueueSize, uint8_t osPolarity, uint8_t osOperationMode, 
                                   uint8_t devOperationMode)
 {
+    obc_error_code_t errCode;
     uint8_t configBuff[LM75BD_CONF_BUFF_SIZE] = {0};
 
     uint8_t osFaltQueueRegData;
@@ -103,26 +119,18 @@ obc_error_code_t writeConfigLM75BD(uint8_t devAddr, uint8_t osFaultQueueSize, ui
     configBuff[0] |= (osOperationMode << 1);
     configBuff[0] |= devOperationMode;
 
-    obc_error_code_t err = i2cWriteReg(devAddr, LM75BD_REG_CONF, configBuff, LM75BD_CONF_BUFF_SIZE);
-    if (err != OBC_ERR_CODE_SUCCESS) {
-        LOG_ERROR("Failed to write config to LM75BD - Addr 0x%02X", devAddr);
-        return err;
-    }
-
+    RETURN_IF_ERROR_CODE(i2cWriteReg(devAddr, LM75BD_REG_CONF, configBuff, LM75BD_CONF_BUFF_SIZE));
     return OBC_ERR_CODE_SUCCESS;
 }
 
 obc_error_code_t readThystLM75BD(uint8_t devAddr, float *hysteresisThresholdCelsius) {
+    obc_error_code_t errCode;
     uint8_t thystBuff[LM75BD_THYST_BUFF_SIZE] = {0};
 
     if (hysteresisThresholdCelsius == NULL)
         return OBC_ERR_CODE_INVALID_ARG;
 
-    obc_error_code_t err = i2cReadReg(devAddr, LM75BD_REG_THYST, thystBuff, LM75BD_THYST_BUFF_SIZE);
-    if (err != OBC_ERR_CODE_SUCCESS) {
-        LOG_ERROR("Failed to read hysteresis threshold from LM75BD - Addr 0x%02X", devAddr);
-        return err;
-    }
+    RETURN_IF_ERROR_CODE(i2cReadReg(devAddr, LM75BD_REG_THYST, thystBuff, LM75BD_THYST_BUFF_SIZE));
 
     /* Combine the 9 MSB into a 16-bit signed integer */
     int16_t value = ( (int8_t)thystBuff[0] << 1 ) | ( thystBuff[1]  >> 7 );
@@ -134,38 +142,30 @@ obc_error_code_t readThystLM75BD(uint8_t devAddr, float *hysteresisThresholdCels
 }
 
 obc_error_code_t writeThystLM75BD(uint8_t devAddr, float hysteresisThresholdCelsius) {
+    obc_error_code_t errCode;
     uint8_t thystBuff[LM75BD_THYST_BUFF_SIZE] = {0};
 
     /* Threshold must be a multiple of the resolution and less than 127.5 degrees Celsius */
     if (fmod(hysteresisThresholdCelsius, LM75BD_THYST_RES) != 0 || fabs(hysteresisThresholdCelsius) > LM75BD_TEMP_THRESH_MAX)
         return OBC_ERR_CODE_INVALID_ARG;
 
-
     // Convert celsius to 2's complement reg data
     int16_t converted = (int16_t)(hysteresisThresholdCelsius / LM75BD_THYST_RES) << 7;
     thystBuff[0] = converted >> 8;
     thystBuff[1] = converted & 0xFF;
 
-    obc_error_code_t err = i2cWriteReg(devAddr, LM75BD_REG_THYST, thystBuff, LM75BD_THYST_BUFF_SIZE);
-    if (err != OBC_ERR_CODE_SUCCESS) {
-        LOG_ERROR("Failed to write hysteresis threshold to LM75BD - Addr 0x%02X", devAddr);
-        return err;
-    }
-
+    RETURN_IF_ERROR_CODE(i2cWriteReg(devAddr, LM75BD_REG_THYST, thystBuff, LM75BD_THYST_BUFF_SIZE));
     return OBC_ERR_CODE_SUCCESS;
 }
 
 obc_error_code_t readTosLM75BD(uint8_t devAddr, float *overTempThresholdCelsius) {
+    obc_error_code_t errCode;
     uint8_t tosBuff[LM75BD_TOS_BUFF_SIZE] = {0};
 
     if (overTempThresholdCelsius == NULL)
         return OBC_ERR_CODE_INVALID_ARG;
 
-    obc_error_code_t err = i2cReadReg(devAddr, LM75BD_REG_TOS, tosBuff, LM75BD_TOS_BUFF_SIZE);
-    if (err != OBC_ERR_CODE_SUCCESS) {
-        LOG_ERROR("Failed to read over temp threshold from LM75BD - Addr 0x%02X", devAddr);
-        return err;
-    }
+    RETURN_IF_ERROR_CODE(i2cReadReg(devAddr, LM75BD_REG_TOS, tosBuff, LM75BD_TOS_BUFF_SIZE));
 
     /* Combine the 9 MSB into a 16-bit signed integer */
     int16_t value = ( (int8_t)tosBuff[0] << 1 ) | ( tosBuff[1]  >> 7 );
@@ -177,6 +177,7 @@ obc_error_code_t readTosLM75BD(uint8_t devAddr, float *overTempThresholdCelsius)
 }
 
 obc_error_code_t writeTosLM75BD(uint8_t devAddr, float overTempThresholdCelsius) {
+    obc_error_code_t errCode;
     uint8_t tosBuff[LM75BD_TOS_BUFF_SIZE] = {0};
 
     /* Threshold must be a multiple of the resolution and less than 127.5 degrees Celsius */
@@ -188,12 +189,7 @@ obc_error_code_t writeTosLM75BD(uint8_t devAddr, float overTempThresholdCelsius)
     tosBuff[0] = converted >> 8;
     tosBuff[1] = converted & 0xFF;
 
-    obc_error_code_t err = i2cWriteReg(devAddr, LM75BD_REG_TOS, tosBuff, LM75BD_TOS_BUFF_SIZE);
-    if (err != OBC_ERR_CODE_SUCCESS) {
-        LOG_ERROR("Failed to write over temp threshold to LM75BD - Addr 0x%02X", devAddr);
-        return err;
-    }
-
+    RETURN_IF_ERROR_CODE(i2cWriteReg(devAddr, LM75BD_REG_TOS, tosBuff, LM75BD_TOS_BUFF_SIZE));
     return OBC_ERR_CODE_SUCCESS;
 } 
 
