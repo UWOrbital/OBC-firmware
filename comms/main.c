91d3aebb66077f232bb38c09a82194f44372138d
Aryan Kalia <kalia.aryan2013@gmail.com>
2023-02-08 20:06:31 -0500

    change build process for subsystems

---
+++ comms/main.c
@@ -0,0 +1,28 @@
+#include "obc_logging.h"
+#include "obc_sci_io.h"
+#include "obc_spi_io.h"
+
+#include <FreeRTOS.h>
+#include <os_task.h>
+
+#include <sys_common.h>
+#include <gio.h>
+#include <sci.h>
+#include <spi.h>
+
+int main(void) {
+    gioInit();
+    sciInit();
+    spiInit();
+
+    // Initialize logger
+    initLogger();
+    
+    // Initialize bus mutexes
+    initSciMutex();
+    initSpiMutex();
+
+    sciPrintf("Hello from comms!\n");
+
+    vTaskStartScheduler();
+}
