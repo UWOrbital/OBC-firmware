633da1a52ad7505a62218b8451eba780e18f6b35
dgobalak <dgobalak@uwaterloo.ca>
2023-02-11 00:50:30 -0800

    Updated examples

--- examples/unit_test_demo/src/Example.c
+++
@@ -1,6 +0,0 @@
-#include "Example.h"
-
-int8_t AverageThreeBytes(int8_t a, int8_t b, int8_t c)
-{
-return (int8_t)(((int16_t)a + (int16_t)b + (int16_t)c) / 3);
-}
--- examples/unit_test_demo/src/Example.h
+++
@@ -1,3 +0,0 @@
-#include <stdint.h>
-
-int8_t AverageThreeBytes(int8_t a, int8_t b, int8_t c);
