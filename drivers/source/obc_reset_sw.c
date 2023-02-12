9f872eddf9ed84674e30825fc246e7df869e44e2
Jac2125 <jace4188@gmail.com>
2023-02-11 23:58:45 -0500

    Added an appropriate way to change bit number

--- drivers/source/obc_reset_sw.c
+++ drivers/source/obc_reset_sw.c
@@ -4,10 +4,10 @@
 void resetSW(reset_t reason){
 
     uint32_t* resetAddress = (uint32_t*)(systemREG1 -> SYSECR);
-    resetAddress += 0x000F;
+
     if(reason != NULLREASON){
 
-        *resetAddress = (uint32_t)0x0001;
+        *resetAddress =  *resetAddress | (uint32_t)0x80000000;
     }
 
 }
