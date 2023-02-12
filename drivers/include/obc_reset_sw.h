319fe902a97206aa3accfef9cb25eae0965fe7e4
Jac2125 <jace4188@gmail.com>
2023-02-11 23:01:28 -0500

    Added obc_reset_sw.h and obc_reset_sw.c

---
+++ drivers/include/obc_reset_sw.h
@@ -0,0 +1,11 @@
+#ifndef DRIVERS_INCLUDE_OBC_RESET_SW_H
+#define DRIVERS_INCLUDE_OBC_RESET_SW_H
+
+typedef enum{
+    //Example reset reason
+    NULLREASON = 0,
+    OVERTEMPERATURE = 1
+
+}reset_t;
+
+#endif //DRIVERS_INCLUDE_OBC_RESET_SW_H
