27e9cd32c81cbe4205dab342e04c60e7d4aff6eb
Jac2125 <jace4188@gmail.com>
2023-02-11 23:18:45 -0500

    Deleted duplicative header and changed type casting

--- drivers/source/obc_reset_sw.c
+++ drivers/source/obc_reset_sw.c
@@ -1,10 +1,9 @@
-#include "errata_SSWF021_45_defs.h"
 #include "obc_reset_sw.h"
 #include "reg_system.h"    
 
 void resetSW(reset_t reason){
 
-    uint32_t* resetAddress = systemREG1 -> SYSECR;
+    uint32_t* resetAddress = (uint32_t*)(systemREG1 -> SYSECR);
     resetAddress += 0x000F;
     if(reason != NULLREASON){
 
