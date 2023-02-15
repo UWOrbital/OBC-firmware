123d9bb610c81f1920394927f20a22f6da75831f
dgobalak <dgobalak@uwaterloo.ca>
2023-02-15 00:09:59 -0800

    Moved files from unit testing branch

---
+++ tests/test_shape_container.c
@@ -0,0 +1,26 @@
+#include "unity.h" // The testing framework
+
+#include "shape_container.h" // The header for the code we are testing
+#include "mock_rectangle.h" // A mock header
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_shape_container_init(void) {
+    // Set up known values
+    uint16_t length = 4;
+    uint16_t width = 3;
+
+    //State, in order of call, what expectations we have, and the expected values to be returned, if any
+    rectangle_init_Expect(length, width);
+
+    //Run Actual Function Under Test
+    shape_container_init(length, width);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_shape_container_init);
+    return UNITY_END();
+}
