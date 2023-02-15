9301e7c9d459adcbb5c3df274293d9cc66c672dd
dgobalak <dgobalak@uwaterloo.ca>
2023-02-14 22:00:48 -0800

    Changed variable to macro; removed excess whitespace

--- drivers/source/obc_reset.c
+++ drivers/source/obc_reset.c
@@ -2,18 +2,13 @@
 #include "reg_system.h" 
 #include "obc_privilege.h"
 
-uint32_t ResetAddrMask = (1 << 15);
+#define RESET_SYSTEM_MASK (1 << 15)
 
 // TODO: Save the reason to persistent storage
-void resetSystem(obc_reset_reason_t reason){
-    
+void resetSystem(obc_reset_reason_t reason) {
     BaseType_t xRunningPrivileged = prvRaisePrivilege();
     
-    
-    systemREG1->SYSECR |= ResetAddrMask;
+    systemREG1->SYSECR |= RESET_SYSTEM_MASK;
 
     portRESET_PRIVILEGE(xRunningPrivileged);
-
-    
-
 }
