95d2a0a3b7a3d1be2b706169296cc9983b3fd99e
shouza1712 <71526713+shouza1712@users.noreply.github.com>
2023-02-14 20:08:59 -0500

    initial push

---
+++ cdh/source/timekeeper_rj.c
@@ -0,0 +1,11 @@
+/// first create a Queue using freeRTOS provided functions xqueuecreate, sample length of 20
+///  
+
+/// 1. Set Alarm : Direct function call in the driver
+/// 2. Handle different alarms:
+/// 3 and 4 ask daniel
+/// 5. Set current time: Direct function call in driver
+/// 6. Get current time: Direct function call in driver
+/// 7. Assign a mutex to each function
+/// 8. Reset function in RTC driver can be used to reset all current times
+/// 9. Feature not developed yet in the driver due to RTC limitations
