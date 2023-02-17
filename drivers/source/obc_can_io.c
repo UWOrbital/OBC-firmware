32c804688d3625d3fd4417cb53e4dc02db3e1e0d
Nathan Tang <nathantangca@outlook.com>
2023-02-16 21:03:47 -0500

    Just added some comments

--- drivers/source/obc_can_io.c
+++ drivers/source/obc_can_io.c
@@ -23,9 +23,11 @@ uint8_t canSendMessage(canBASE_t *canReg, uint32_t messageBox, const uint8_t *tx
     ASSERT(canMutex != NULL);
 
     uint8_t status = 0;
-    // might not need mutex protection because of arbitration embedded in CAN protocol
+
+    // Check that tx data is within 8 bytes (assuming standard CAN packet)
+
     if(xSemaphoreTake(canMutex, portMAX_DELAY) == pdTRUE) {
-        taskENTER_CRITICAL(); // might not be needed
+        taskENTER_CRITICAL(); 
 
         status = canTransmit(canReg, messageBox, txData);
 
@@ -36,7 +38,7 @@ uint8_t canSendMessage(canBASE_t *canReg, uint32_t messageBox, const uint8_t *tx
 }
 
 uint8_t canGetMessage(canBASE_t *canReg, uint32_t messageBox, const uint8_t *rxData) {
-    while(!canIsRxMessageArrived(canReg, messageBox));
+    while(!canIsRxMessageArrived(canReg, messageBox)); /* Poll for RX CAN message */
     
     return canGetData(canReg, messageBox, rxData);
 }
