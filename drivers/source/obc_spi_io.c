5d7765aaa9738948e7924361e75559c8e2099640
SarimMuqeet <sarim.muqeet2012@gmail.com>
2023-02-26 15:05:33 -0500

    Update functions to remain backwards compatible

--- drivers/source/obc_spi_io.c
+++ drivers/source/obc_spi_io.c
@@ -96,7 +96,70 @@ obc_error_code_t assertChipSelect(gioPORT_t *spiPort, uint8_t csNum) {
     return OBC_ERR_CODE_MUTEX_TIMEOUT;
 }
 
-obc_error_code_t spiTransmitAndReceiveBytes(spiBASE_t *spiReg, uint8_t *outBytes, uint8_t *inBytes, int numBytes) {
+obc_error_code_t spiTransmitAndReceiveByte(spiBASE_t *spiReg, uint8_t *outb, uint8_t *inb) {
+    if (spiReg == NULL)
+        return OBC_ERR_CODE_INVALID_ARG;
+
+    if (inb == NULL)
+        return OBC_ERR_CODE_INVALID_ARG;
+
+    int8_t spiRegIndex = spiRegToIndex(spiReg);
+    if (spiRegIndex < 0)
+        return OBC_ERR_CODE_INVALID_ARG;
+
+    if (xSemaphoreTake(spiMutexes[spiRegIndex], portMAX_DELAY) == pdTRUE) {        
+        spiDAT1_t spiData = {0};
+
+        // The SPI HAL functions take 16-bit arguments, but we're using 8-bit word size
+        uint16_t spiWordOut = (uint16_t)*outb;
+        uint16_t spiWordIn;
+
+        uint32_t spiErr = spiTransmitAndReceiveData(spiReg, &spiData, 1, &spiWordOut, &spiWordIn) & SPI_FLAG_ERR_MASK;
+        obc_error_code_t ret;
+
+        if (spiErr != SPI_FLAG_SUCCESS) {
+            // To-do: Log and handle errors
+            ret = OBC_ERR_CODE_SPI_FAILURE;
+        } else {
+            *inb = (uint8_t)spiWordIn;
+            ret = OBC_ERR_CODE_SUCCESS;
+        }
+
+        xSemaphoreGive(spiMutexes[spiRegIndex]);
+        return ret;
+    }
+    return OBC_ERR_CODE_MUTEX_TIMEOUT;
+}
+
+obc_error_code_t spiTransmitByte(spiBASE_t *spiReg, uint8_t *outb, size_t numBytes) {
+    obc_error_code_t errCode;
+    uint8_t inb;
+
+    if(numBytes > 1) {
+        spiTransmitBytes(spiReg, outb, numBytes);
+    }
+    else {
+        RETURN_IF_ERROR_CODE(spiTransmitAndReceiveByte(spiReg, outb, &inb));
+    }
+
+    return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t spiReceiveByte(spiBASE_t *spiReg, uint8_t *inb, size_t numBytes) {
+    obc_error_code_t errCode;
+    uint8_t outB = 0xFF;
+
+    if(numBytes > 1) {
+        spiReceiveBytes(spiReg, inb, numBytes);
+    }
+    else {
+        RETURN_IF_ERROR_CODE(spiTransmitAndReceiveByte(spiReg, &outB, inb));
+    }
+
+    return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t spiTransmitAndReceiveBytes(spiBASE_t *spiReg, uint8_t *outBytes, uint8_t *inBytes, size_t numBytes) {
     if (spiReg == NULL || inBytes == NULL || outBytes == NULL)
         return OBC_ERR_CODE_INVALID_ARG;
 
@@ -108,19 +171,20 @@ obc_error_code_t spiTransmitAndReceiveBytes(spiBASE_t *spiReg, uint8_t *outBytes
         spiDAT1_t spiData = {0};
         obc_error_code_t ret;
 
-        for(int currentByte = 0; currentByte < numBytes; currentByte++)
+        for(size_t i = 0; i < numBytes; i++)
         {
-            uint16_t spiWordOut = (uint16_t)outBytes[currentByte];
+            // The SPI HAL functions take 16-bit arguments, but we're using 8-bit word size
+            uint16_t spiWordOut = (uint16_t)outBytes[i];
             uint16_t spiWordIn;
 
             uint32_t spiErr = spiTransmitAndReceiveData(spiReg, &spiData, 1, &spiWordOut, &spiWordIn) & SPI_FLAG_ERR_MASK;
 
             if (spiErr != SPI_FLAG_SUCCESS) {
                 // To-do: Log and handle errors
-                ret = OBC_ERR_CODE_SPI_FAILURE;\
+                ret = OBC_ERR_CODE_SPI_FAILURE;
                 break;
             } else {
-                inBytes[currentByte] = (uint8_t)spiWordIn;
+                inBytes[i] = (uint8_t)spiWordIn;
                 ret = OBC_ERR_CODE_SUCCESS;
             }
         }
@@ -131,15 +195,21 @@ obc_error_code_t spiTransmitAndReceiveBytes(spiBASE_t *spiReg, uint8_t *outBytes
     return OBC_ERR_CODE_MUTEX_TIMEOUT;
 }
 
-obc_error_code_t spiTransmitBytes(spiBASE_t *spiReg, uint8_t *outBytes, int numBytes) {
+obc_error_code_t spiTransmitBytes(spiBASE_t *spiReg, uint8_t *outBytes, size_t numBytes) {
     obc_error_code_t errCode;
-    uint8_t inBytes;
+    size_t maxSize = 10000;
 
-    RETURN_IF_ERROR_CODE(spiTransmitAndReceiveBytes(spiReg, outBytes, &inBytes, numBytes));
-    return OBC_ERR_CODE_SUCCESS;
+    if(numBytes < maxSize) {
+        uint8_t inBytes[maxSize];
+        RETURN_IF_ERROR_CODE(spiTransmitAndReceiveBytes(spiReg, outBytes, inBytes, numBytes));
+        return OBC_ERR_CODE_SUCCESS;
+    }
+    else {
+        return OBC_ERR_CODE_SPI_FAILURE;
+    }
 }
 
-obc_error_code_t spiReceiveBytes(spiBASE_t *spiReg, uint8_t *inBytes, int numBytes) {
+obc_error_code_t spiReceiveBytes(spiBASE_t *spiReg, uint8_t *inBytes, size_t numBytes) {
     obc_error_code_t errCode;
     uint8_t outB = 0xFF;
     RETURN_IF_ERROR_CODE(spiTransmitAndReceiveBytes(spiReg, &outB, inBytes, numBytes));
