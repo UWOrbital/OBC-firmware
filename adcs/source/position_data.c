d0247e44d5ee99bf1f73d935ef47fd1c86ac2dad
Yarik-Popov <yarikpopov03@gmail.com>
2023-03-01 22:49:40 -0500

    Added linear search and access functions

--- adcs/source/position_data.c
+++ adcs/source/position_data.c
@@ -31,6 +31,16 @@ static void printPositionDataManager(const position_data_manager *manager){
 }
 
 /**
+ * @brief Returns the position at the given index % ADCS_POSITION_DATA_SIZE of the manager
+ * @attention Requires index is non-zero and manager is a valid pointer
+*/
+ static position_data accessPositionData(const position_data_manager *manager, int index) {
+    assert(manager);
+    assert(index >= 0);
+    return manager->data[index % ADCS_POSITION_DATA_SIZE];
+ }
+
+/**
  * @brief Reads the data from the manager at read_index and shifts the read_index over
 */
 static position_data readData(position_data_manager *manager) {
@@ -42,12 +52,35 @@ static position_data readData(position_data_manager *manager) {
 
 /**
  * @brief Writes the data to the manager at write_index and shifts the write_index over
+ * @attention manager must be a valid pointer; all julian dates in the manager and data are unique [not tested]
 */
 static void writeData(position_data_manager *manager, position_data data) {
     assert(manager);
     int index = manager->write_index;
     manager->data[index] = data;
     manager->write_index = (index + 1) % ADCS_POSITION_DATA_SIZE;
+}
+
+/**
+ * @brief Searchs for the julian_date in the manager and returns the index of the julian date in the manager that 
+ * is greater or equal to the julian_date
+ * @attention manager must be a valid pointer and julian_data is greater than 0
+*/
+static int searchManagerLinear(const position_data_manager *manager, float julian_date) {
+    assert(manager);
+    assert(julian_date > 0);
+
+    int write_index = manager->write_index;
+    int index = write_index;
+
+    while (1) {
+        if (julian_date <= (manager->data[index]).julian_date) {
+            return index;
+        } else if ((index + 1) % ADCS_POSITION_DATA_SIZE == write_index) {
+            return index;
+        }
+        index = (index + 1) % ADCS_POSITION_DATA_SIZE;
+    }
 }
 
 static position_data initPositionData(void) {
@@ -91,5 +124,21 @@ int main(void) {
 
     printPositionDataManager(&manager);
     printf("%d\n", sizeof(manager));
+
+    // Tests linear search based on above manager
+    assert(searchManagerLinear(&manager, 1.5) == 1);
+    assert(searchManagerLinear(&manager, 2) == 1);
+    assert(searchManagerLinear(&manager, 5) == 4);
+    assert(searchManagerLinear(&manager, 6.8) == 6);
+    assert(searchManagerLinear(&manager, 11) == 0);
+    assert(searchManagerLinear(&manager, 12) == 0);
+
+// Tests access based on above manager
+    assert(accessPositionData(&manager, 0).julian_date == 11);
+    assert(accessPositionData(&manager, 1).julian_date == 2);
+    assert(accessPositionData(&manager, 9).julian_date == 10);
+    assert(accessPositionData(&manager, 10).julian_date == 11);
+    assert(accessPositionData(&manager, 5).julian_date == 6);
+    printf("Assertions passed\n");
     return 0;
 }
