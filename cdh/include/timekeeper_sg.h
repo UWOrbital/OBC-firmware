3910e0cdb2780eec3ae863b726ba4d77b5bb85e6
shouza1712 <71526713+shouza1712@users.noreply.github.com>
2023-03-04 14:13:57 -0500

    direct call functions

---
+++ cdh/include/timekeeper_sg.h
@@ -0,0 +1,21 @@
+#ifndef CDH_INCLUDE_TIMEKEEPER_SG_H_
+#define CDH_INCLUDE_TIMEKEEPER_SG_H_
+
+#include <stdint.h>
+#include <gio.h>
+#include "ds3232_mz.h"
+
+obc_error_code_t setAlarm1(rtc_alarm_time_t alarmTime, rtc_alarm1_mode_t alarmMode);
+
+obc_error_code_t setAlarm2(rtc_alarm_time_t alarmTime, rtc_alarm2_mode_t alarmMode);
+
+/*An idea is to also have a generic alarm function which can take any mode from alarm 1 or alarm 2. A seperate enum will be made for that in this file which combines
+both the alarm enums from the rtc driver, based on the mode the function will choose which alarm to set and if a mode is available in both alarms maybe choose based on
+which alarm is available? If we go with this, then setalar1 and setAlarm2 functions should not be declared in the header file as it is not part of the interface we
+want the other seubteams to see*/
+
+obc_error_code_t setCurrentTime(rtc_time_t currentTime);
+
+obc_error_code_t getCurrentTime(rtc_time_t *getTime);
+
+#endif /*CDH_INCLUDE_TIMEKEEPER_SG_H_*/
--- cdh/source/timekeepr_sg.c
+++ cdh/source/timekeepr_sg.c
@@ -9,3 +9,28 @@
 /// 7. Assign a mutex to each function
 /// 8. Reset function in RTC driver can be used to reset all current times
 /// 9. Feature not developed yet in the driver due to RTC limitations
+
+#include <FreeRTOS.h>
+#include <os_portmacro.h>
+#include <os_queue.h>
+#include <os_task.h>
+#include "ds3232_mz.h"
+#include "timekeeper_sg.h"
+
+obc_error_code_t setAlarm1(rtc_alarm_time_t alarmTime, rtc_alarm1_mode_t alarmMode) {
+    return setAlarm1RTC(alarmMode, alarmTime);
+}
+
+obc_error_code_t setAlarm2(rtc_alarm_time_t alarmTime, rtc_alarm2_mode_t alarmMode) {
+    return setAlarm2RTC(alarmMode, alarmTime);
+}
+
+obc_error_code_t setCurrentTime(rtc_time_t currentTime) {
+    return setCurrentTimeRTC(currentTime);
+}
+
+obc_error_code_t getCurrentTime(rtc_time_t *getTime) {
+    return getCurrentTimeRTC(&getTime);
+}
+
+
