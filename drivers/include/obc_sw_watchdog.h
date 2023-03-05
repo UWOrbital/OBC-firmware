91ed16e16ec9e115a83d1c4b90165c2e801ec904
Jac2125 <jace4188@gmail.com>
2023-03-05 13:47:32 -0500

    "File name changed and function details changed

--- drivers/include/obc_sw_watchdog.h
+++ drivers/include/obc_sw_watchdog.h
@@ -1,18 +1,15 @@
 #ifndef _DRIVERS_INCLUDE_OBC_WATCHDOG_H_
 #define _DRIVERS_INCLUDE_OBC_WATCHDOG_H_
 
-#define DWWD_NAME "Digital Windowed Watchdog"
-#define DWWD_STACK_SIZE 128
-#define DWWD_PRIORITY 1
 
 /**
  * @brief Initialize Digital Windowed Watchdog
 */
-void initDWWD();
+obc_error_code_t initDWWD(double_t tExp);
 
 /**
  * @brief Initialize DWWDTask
 */
-uint8_t initDWWDTask();
+obc_error_code_t initDWWDTask(void);
 
 #endif //_DRIVERS_INCLUDE_OBC_WATCHDOG_H_
