d6ad5fc9d870cb7f60bc1292ce9c582be0fd5151
Jac2125 <jace4188@gmail.com>
2023-03-05 17:21:49 -0500

    Header ifndef changed

--- drivers/include/obc_sw_watchdog.h
+++ drivers/include/obc_sw_watchdog.h
@@ -1,5 +1,5 @@
-#ifndef _DRIVERS_INCLUDE_OBC_WATCHDOG_H_
-#define _DRIVERS_INCLUDE_OBC_WATCHDOG_H_
+#ifndef DRIVERS_INCLUDE_OBC_SW_WATCHDOG_H_
+#define DRIVERS_INCLUDE_OBC_SW_WATCHDOG_H_
 
 #include "obc_errors.h"
 #include <math.h>
@@ -14,4 +14,4 @@ obc_error_code_t initDWWD(float tExp);
 */
 obc_error_code_t initDWWDTask(void);
 
-#endif //_DRIVERS_INCLUDE_OBC_WATCHDOG_H_
+#endif //_DRIVERS_INCLUDE_OBC_SW_WATCHDOG_H_
