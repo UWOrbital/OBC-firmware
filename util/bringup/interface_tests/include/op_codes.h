49206e6c9d2054ce47eb4ab586a6e10586389ce9
Daniel Gobalakrishnan <63618806+dgobalak@users.noreply.github.com>
2023-03-10 23:21:35 -0800

    Created basic architecture for OBC bring-up testing tool (#56)

---
+++ util/bringup/interface_tests/include/op_codes.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_BRINGUP_INTERFACE_TESTS_INCLUDE_OP_CODES_H_
+#define UTIL_BRINGUP_INTERFACE_TESTS_INCLUDE_OP_CODES_H_
+
+#include <stdint.h>
+
+#define OP_CODE_SPI_TEST (uint8_t) '0'
+#define OP_CODE_SCI_TEST (uint8_t) '1'
+#define OP_CODE_I2C_TEST (uint8_t) '2'
+#define OP_CODE_CAN_TEST (uint8_t) '3'
+
+#define OP_CODE_ADC_TEST (uint8_t) '4'
+#define OP_CODE_GIO_TEST (uint8_t) '5'
+
+#endif // UTIL_BRINGUP_INTERFACE_TESTS_INCLUDE_OP_CODES_H_
---
+++ util/bringup/interface_tests/source/test_sci.c
@@ -0,0 +1,6 @@
+#include "test_sci.h"
+#include "obc_sci_io.h"
+
+void testSCI(void) {
+    sciPrintf("Testing SCI...\r\n");
+}
