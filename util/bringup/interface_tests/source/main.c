c2ef76bfb539ebf7ee7499f9ade5fd0a868eac25
Daniel Gobalakrishnan <63618806+dgobalak@users.noreply.github.com>
2023-03-10 23:21:35 -0800

    Created basic architecture for OBC bring-up testing tool (#56)

---
+++ util/bringup/interface_tests/source/main.c
@@ -0,0 +1,80 @@
+#include "obc_spi_io.h"
+#include "obc_sci_io.h"
+#include "obc_i2c_io.h"
+#include "obc_logging.h"
+
+#include "op_codes.h"
+
+#include "test_sci.h"
+#include "test_spi.h"
+#include "test_i2c.h"
+#include "test_can.h"
+#include "test_adc.h"
+#include "test_gio.h"
+
+#include <FreeRTOS.h>
+#include <os_task.h>
+
+#include <sys_common.h>
+#include <sci.h>
+#include <spi.h>
+#include <i2c.h>
+
+#include <string.h>
+
+#define TASK_STACK_SIZE 1024
+
+static StaticTask_t taskBuffer;
+static StackType_t taskStack[TASK_STACK_SIZE];
+
+typedef void (*testFunc_t)(void);
+
+#define NUM_COMMANDS_MAX 255 // Each command initiated by a single character
+
+static const testFunc_t testFuncs[NUM_COMMANDS_MAX] = {
+    [OP_CODE_SPI_TEST] = testSPI,
+    [OP_CODE_SCI_TEST] = testSCI,
+    [OP_CODE_I2C_TEST] = testI2C,
+    [OP_CODE_CAN_TEST] = testCAN,
+    [OP_CODE_ADC_TEST] = testADC,
+    [OP_CODE_GIO_TEST] = testGIO,
+};
+
+void utilityCLI(void *pvParameters) {
+    while (1) {
+        unsigned char cmdChar;
+        sciPrintf("Enter a command: ");
+        sciRead(&cmdChar, 1);
+        sciPrintf("\r\n");
+
+        if (cmdChar >= NUM_COMMANDS_MAX) {
+            sciPrintf("Invalid command\r\n");
+            continue;
+        }
+
+        if (testFuncs[cmdChar] == NULL) {
+            sciPrintf("Invalid command\r\n");
+            continue;
+        }
+
+        testFuncs[cmdChar]();
+    }
+}
+
+int main(void) {
+    sciInit();
+    spiInit();
+    i2cInit();
+
+    initSciMutex();
+    initSpiMutex();
+    initI2CMutex();
+
+    sciPrintf("Starting Bringup Utility...\r\n");
+
+    xTaskCreateStatic(utilityCLI, "Bringup Utility", TASK_STACK_SIZE, NULL, 1, taskStack, &taskBuffer);
+
+    vTaskStartScheduler();
+
+    while (1);
+}
