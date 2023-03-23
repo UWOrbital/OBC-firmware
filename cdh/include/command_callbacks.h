30207c06a70ea1040f7208c807e1d20eb199b66e
dgobalak <dgobalak@uwaterloo.ca>
2023-03-23 01:57:21 -0700

    Added comment with the command ID for each callback

--- cdh/include/command_callbacks.h
+++ cdh/include/command_callbacks.h
@@ -8,10 +8,13 @@ typedef obc_error_code_t (*cmd_callback_t)(cmd_msg_t *cmd);
 
 /* Declare all command callbacks below */
 
+// CMD_EXEC_OBC_RESET
 obc_error_code_t execObcResetCmdCallback(cmd_msg_t *cmd);
 
+// CMD_RTC_SYNC
 obc_error_code_t rtcSyncCmdCallback(cmd_msg_t *cmd);
 
+// CMD_DOWNLINK_LOGS_NEXT_PASS
 obc_error_code_t downlinkLogsNextPassCmdCallback(cmd_msg_t *cmd);
 
 #endif // CDH_INCLUDE_COMMAND_CALLBACKS_H_
