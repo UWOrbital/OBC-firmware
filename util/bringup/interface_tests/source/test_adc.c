0ce07baa76b50ce777f22580ff9a03b9e755823d
Junxin Gao <j269gao@uwaterloo.ca>
2023-04-02 14:48:24 -0400

    implement bring up utility adcs

--- util/bringup/interface_tests/source/test_adc.c
+++ util/bringup/interface_tests/source/test_adc.c
@@ -1,6 +1,16 @@
 #include "test_adc.h"
 #include "obc_sci_io.h"
+#include "adcs_manager.h"
 
 void testADC(void) {
     sciPrintf("Testing ADC...\r\n");
+
+    initADCSManager();
+    adcs_event_t adcEvent;
+    if (sendToADCSQueue(adcEvent) != OBC_ERR_CODE_SUCCESS)
+    {
+        sciPrintf("Failed sending event through ADC\r\n");
+        return;
+    }
+    sciPrintf("Successfully sending event through ADC \r\n");
 }
