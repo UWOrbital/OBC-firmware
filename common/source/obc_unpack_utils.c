050cb67310c515e68f1ab855f6b29e89339efb67
dgobalak <dgobalak@uwaterloo.ca>
2023-04-15 00:29:31 -0700

    Addressed strict aliasing concerns

--- common/source/obc_unpack_utils.c
+++ common/source/obc_unpack_utils.c
@@ -1,6 +1,7 @@
 #include "obc_unpack_utils.h"
 
 #include <stdint.h>
+#include <string.h>
 
 uint8_t unpackUint8(const uint8_t* buffer, uint32_t* offset) {
     uint8_t value = buffer[*offset];
@@ -25,19 +26,37 @@ uint32_t unpackUint32(const uint8_t* buffer, uint32_t* offset) {
 }
 
 int8_t unpackInt8(const uint8_t* buffer, uint32_t* offset) {
-    return (int8_t)unpackUint8(buffer, offset);
+    int8_t val;
+
+    uint8_t tmp = unpackUint8(buffer, offset);
+    memcpy(&val, &tmp, sizeof(val));
+
+    return val;
 }
 
 int16_t unpackInt16(const uint8_t* buffer, uint32_t* offset) {
-    return (int16_t)unpackUint16(buffer, offset);
+    int16_t val;
+
+    uint16_t tmp = unpackUint16(buffer, offset);
+    memcpy(&val, &tmp, sizeof(val));
+
+    return val;
 }
 
 int32_t unpackInt32(const uint8_t* buffer, uint32_t* offset) {
-    return (int32_t)unpackUint32(buffer, offset);
+    int32_t val;
+
+    uint32_t tmp = unpackUint32(buffer, offset);
+    memcpy(&val, &tmp, sizeof(val));
+
+    return val;
 }
 
 float unpackFloat(const uint8_t* buffer, uint32_t* offset) {
-    uint32_t tmp = unpackUint32(buffer, offset);
-    return *((float*)&tmp);
-}
+    float val;
 
+    uint32_t tmp = unpackUint32(buffer, offset);
+    memcpy(&val, &tmp, sizeof(val));
+
+    return val;
+}
