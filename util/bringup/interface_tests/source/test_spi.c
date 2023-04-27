60bfee7c2eea0ee8ef2804407986790e94fcd450
Jac2125 <jace4188@gmail.com>
2023-04-27 09:14:32 -0400

    Added Feature: test a desired register from user.

--- util/bringup/interface_tests/source/test_spi.c
+++ util/bringup/interface_tests/source/test_spi.c
@@ -3,15 +3,33 @@
 #include "obc_spi_io.h"
 #include "reg_spi.h"
 #include "spi.h"
+#include <stdlib.h>
+
+#define NUM_CHARS_TO_READ 1U
 
 void testSPI(void) {
     sciPrintf("Testing SPI...\r\n");
-    spiBASE_t spiReg;
-    spiDAT1_t spiDataFormat;
-    uint8_t *outBytes = {0x34, 0x00, 0x77, 0xFF, 0xA3};
-    size_t numBytes = 5;
+    sciPrintf("Which register module do you want to test?\n");
+
+    uint16_t regNum;
+    unsigned char buffer[NUM_CHARS_TO_READ] = {'\0'};
+    char *c;
+    while(1){
+        sciPrintf("Enter a valid register address\n");
+        obc_error_code_t error = sciRead(buffer, NUM_CHARS_TO_READ);
+        if (error == OBC_ERR_CODE_SUCCESS) {
+            regNum = strtol(buffer, &c, 10);
+            break;
+        }
+        sciPrintf("Error reading from SCI! - %d\r\n", (int)error);
+    }
+
     
-    if(spiTransmitBytes(&spiReg, &spiDataFormat, outBytes, numBytes)!=
+    uint32_t spiReg = spiREG1->GCR0+regNum;
+    spiDAT1_t spiDataFormat;
+    uint8_t outBytes[5] = {0x0F, 0x0F, 0x0F, 0x0F, 0x0F};
+    
+    if(spiTransmitBytes(&spiReg, &spiDataFormat, outBytes, sizeof(outBytes)/sizeof(uint8_t))!=
         OBC_ERR_CODE_SUCCESS){
             sciPrintf("Failed transimitting data through SPI\r\n");
             return;
