eab073cbb2bcb9f2978d273c99230f8ab6ee2d69
Jac2125 <jace4188@gmail.com>
2023-04-27 21:40:04 -0400

    SPI data format added

--- util/bringup/interface_tests/source/test_spi.c
+++ util/bringup/interface_tests/source/test_spi.c
@@ -12,8 +12,13 @@ void testSPI(void) {
     sciPrintf("Which register module do you want to test?\n");
 
     uint16_t regNum;
+    spiDAT1_t dataFmt = {.CS_HOLD = 0,
+                         .CSNR = SPI_CS_NONE,
+                         .DFSEL = SPI_FMT_0,
+                         .WDEL = 0};
     unsigned char buffer[NUM_CHARS_TO_READ] = {'\0'};
     char *c;
+
     while(1){
         sciPrintf("Enter a valid register address\n");
         obc_error_code_t error = sciRead(buffer, NUM_CHARS_TO_READ);
@@ -26,10 +31,9 @@ void testSPI(void) {
 
     
     uint32_t spiReg = spiREG1->GCR0+regNum;
-    spiDAT1_t spiDataFormat;
     uint8_t outBytes[5] = {0x0F, 0x0F, 0x0F, 0x0F, 0x0F};
     
-    if(spiTransmitBytes(&spiReg, &spiDataFormat, outBytes, sizeof(outBytes)/sizeof(uint8_t))!=
+    if(spiTransmitBytes(&spiReg, &dataFmt, outBytes, sizeof(outBytes)/sizeof(uint8_t))!=
         OBC_ERR_CODE_SUCCESS){
             sciPrintf("Failed transimitting data through SPI\r\n");
             return;
