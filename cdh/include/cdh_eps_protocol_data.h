fc761ca61ce325db2ffee95bf9a4fb9827298ca3
dgobalak <dgobalak@uwaterloo.ca>
2023-05-04 02:03:38 -0400

    Added static asserts for CAN protocol msg sizes

--- cdh/include/cdh_eps_protocol_data.h
+++ cdh/include/cdh_eps_protocol_data.h
@@ -2,6 +2,7 @@
 #define CDH_INCLUDE_CDH_EPS_PROTOCOL_DATA_H_
 
 #include "obc_logging.h"
+#include "obc_assert.h"
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -31,6 +32,10 @@ typedef struct {
     uint8_t reserved[6]; /* Reserved Bytes */
     uint8_t request; /* Request granted or denied */
 } cdh_eps_resp_msg_t;
+
+STATIC_ASSERT_INT_EQ(sizeof(cdh_eps_cmd_msg_t), 8);
+STATIC_ASSERT_INT_EQ(sizeof(cdh_eps_tle_msg_t), 8);
+STATIC_ASSERT_INT_EQ(sizeof(cdh_eps_resp_msg_t), 8);
 
 /* -------------------------- */
 /*      Union for Queue       */
