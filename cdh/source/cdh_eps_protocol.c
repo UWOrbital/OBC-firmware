eea199214a2cefe911bc49f23cd4954c07f9cf24
Nathan Tang <nathantangca@outlook.com>
2023-05-19 21:23:40 -0400

    Added check for msg.cmd.id

--- cdh/source/cdh_eps_protocol.c
+++ cdh/source/cdh_eps_protocol.c
@@ -27,35 +27,58 @@ static StaticQueue_t cdhepsRxQueue;
 static uint8_t cdhepsRxQueueStack[CDH_EPS_QUEUE_LENGTH * CDH_EPS_QUEUE_ITEM_SIZE];
 
 static const uint32_t txMessageBoxes[] = {
-    [CMD_SUBSYS_SHUTDDOWN] = canMESSAGE_BOX1,
-    [CMD_HEARTBEAT] = canMESSAGE_BOX3,
-    [CMD_GET_TELEMETRY] = canMESSAGE_BOX4,
+    [CMD_SUBSYS_SHUTDDOWN]      = canMESSAGE_BOX1,
+    [CMD_HEARTBEAT]             = canMESSAGE_BOX3,
+    [CMD_GET_TELEMETRY]         = canMESSAGE_BOX4,
+    [TLE_EPS_BOARD_TEMP]        = 0,
+    [TLE_SOLAR_PANEL_1_TEMP]    = 0,
+    [TLE_SOLAR_PANEL_2_TEMP]    = 0,
+    [TLE_SOLAR_PANEL_3_TEMP]    = 0,
+    [TLE_SOLAR_PANEL_4_TEMP]    = 0,
+    [TLE_COMMS_5V_CURRENT]      = 0,
+    [TLE_COMMS_3V3_CURRENT]     = 0,
+    [TLE_MAG_8V_CURRENT]        = 0,
+    [TLE_ADCS_5V_CURRENT]       = 0,
+    [TLE_ADCS_3V3_CURRENT]      = 0,
+    [TLE_OBC_CURRENT]           = 0,
+    [TLE_COMMS_5V_CURRENT]      = 0,
+    [TLE_COMMS_3V3_VOLTAGE]     = 0,
+    [TLE_MAG_8V_VOLTAGE]        = 0,
+    [TLE_ADCS_5V_VOLTAGE]       = 0,
+    [TLE_ADCS_3V3_VOLTAGE]      = 0,
+    [TLE_OBC_3V3_VOLTAGE]       = 0,
     [RESP_SUBSYS_SHUTDDOWN_ACK] = canMESSAGE_BOX6,
-    [RESP_HEARTBEAT_ACK] = canMESSAGE_BOX7
+    [RESP_HEARTBEAT_ACK]        = canMESSAGE_BOX7
 };
 
+#define TX_MSG_BOX_SIZE (sizeof(txMessageBoxes) / sizeof(uint32_t))
+
 static const rx_handler_t rxHandlers[] = {
-    [CMD_SUBSYS_SHUTDDOWN] = subsysShutdownCmdHandler,
-    [TLE_EPS_BOARD_TEMP] = tleMsgHandler,
-    [TLE_SOLAR_PANEL_1_TEMP] = tleMsgHandler,
-    [TLE_SOLAR_PANEL_2_TEMP] = tleMsgHandler,
-    [TLE_SOLAR_PANEL_3_TEMP] = tleMsgHandler,
-    [TLE_SOLAR_PANEL_4_TEMP] = tleMsgHandler,
-    [TLE_COMMS_5V_CURRENT] = tleMsgHandler,
-    [TLE_COMMS_3V3_CURRENT] = tleMsgHandler,
-    [TLE_MAG_8V_CURRENT] = tleMsgHandler,
-    [TLE_ADCS_5V_CURRENT] = tleMsgHandler,
-    [TLE_ADCS_3V3_CURRENT] = tleMsgHandler,
-    [TLE_OBC_CURRENT] = tleMsgHandler,
-    [TLE_COMMS_5V_CURRENT] = tleMsgHandler,
-    [TLE_COMMS_3V3_VOLTAGE] = tleMsgHandler,
-    [TLE_MAG_8V_VOLTAGE] = tleMsgHandler,
-    [TLE_ADCS_5V_VOLTAGE] = tleMsgHandler,
-    [TLE_ADCS_3V3_VOLTAGE] = tleMsgHandler,
-    [TLE_OBC_3V3_VOLTAGE] = tleMsgHandler,
+    [CMD_SUBSYS_SHUTDDOWN]      = subsysShutdownCmdHandler,
+    [CMD_HEARTBEAT]             = NULL,
+    [CMD_GET_TELEMETRY]         = NULL,
+    [TLE_EPS_BOARD_TEMP]        = tleMsgHandler,
+    [TLE_SOLAR_PANEL_1_TEMP]    = tleMsgHandler,
+    [TLE_SOLAR_PANEL_2_TEMP]    = tleMsgHandler,
+    [TLE_SOLAR_PANEL_3_TEMP]    = tleMsgHandler,
+    [TLE_SOLAR_PANEL_4_TEMP]    = tleMsgHandler,
+    [TLE_COMMS_5V_CURRENT]      = tleMsgHandler,
+    [TLE_COMMS_3V3_CURRENT]     = tleMsgHandler,
+    [TLE_MAG_8V_CURRENT]        = tleMsgHandler,
+    [TLE_ADCS_5V_CURRENT]       = tleMsgHandler,
+    [TLE_ADCS_3V3_CURRENT]      = tleMsgHandler,
+    [TLE_OBC_CURRENT]           = tleMsgHandler,
+    [TLE_COMMS_5V_CURRENT]      = tleMsgHandler,
+    [TLE_COMMS_3V3_VOLTAGE]     = tleMsgHandler,
+    [TLE_MAG_8V_VOLTAGE]        = tleMsgHandler,
+    [TLE_ADCS_5V_VOLTAGE]       = tleMsgHandler,
+    [TLE_ADCS_3V3_VOLTAGE]      = tleMsgHandler,
+    [TLE_OBC_3V3_VOLTAGE]       = tleMsgHandler,
     [RESP_SUBSYS_SHUTDDOWN_ACK] = respSubsysShutdownAckHandler,
-    [RESP_HEARTBEAT_ACK] = respHeartbeatAckHandler
+    [RESP_HEARTBEAT_ACK]        = respHeartbeatAckHandler
 };
+
+#define RX_HANDLERS_SIZE (sizeof(rxHandlers) / sizeof(rx_handler_t))
 
 /**
  * @brief Task that manages the CAN TX and RX between CDH and EPS
@@ -143,20 +166,23 @@ static void processTxMessages(void)
 {
     cdh_eps_queue_msg_t msg;
 
-    if (xQueueReceive(cdhepsTxQueueHandle, &msg, portMAX_DELAY) == pdPASS)
+    if (xQueueReceive(cdhepsTxQueueHandle, &msg, 0) == pdPASS)
     {
+        LOG_DEBUG("Message to transmit to EPS with ID: %u", msg.cmd.id);
         uint32_t txMessageBox;
 
         // Check if the ID has a messge box
-        if (txMessageBoxes[msg.cmd.id] == 0)
+        if(msg.cmd.id >= TX_MSG_BOX_SIZE) {
+            LOG_ERROR_CODE(OBC_ERR_CODE_UNSUPPORTED_CMD);
+            return;
+        }
 
-        {
+        if(!txMessageBoxes[msg.cmd.id]) {
             LOG_ERROR_CODE(OBC_ERR_CODE_UNSUPPORTED_CAN_MSG);
+            return;
         }
-        else
-        {
-            txMessageBox = txMessageBoxes[msg.cmd.id];
-        }
+        
+        txMessageBox = txMessageBoxes[msg.cmd.id];
 
         /* Send tx message to eps over CAN */
         if(canSendMessage(canREG1, txMessageBox, (uint8_t *)&msg) == OBC_ERR_CODE_CAN_FAILURE) {
@@ -174,20 +200,24 @@ static void processRxMessages(void)
 {
     cdh_eps_queue_msg_t msg;
     /* Process recieved messages from EPS */
-    if (xQueueReceive(cdhepsRxQueueHandle, &msg, portMAX_DELAY) == pdPASS)
+    if (xQueueReceive(cdhepsRxQueueHandle, &msg, 0) == pdPASS)
     {
         LOG_DEBUG("Recieved CAN msg from EPS with ID: %u", msg.cmd.id);
+        if(msg.cmd.id >= RX_HANDLERS_SIZE) {
+            LOG_ERROR_CODE(OBC_ERR_CODE_UNSUPPORTED_CMD);
+            return;
+        }
+
         // Check if the ID has a handler, if it does, execute
         if (rxHandlers[msg.cmd.id] == NULL)
         {
             LOG_ERROR_CODE(OBC_ERR_CODE_UNSUPPORTED_CAN_MSG);
             return;
         }
-        else
-        { // Execute callback
-            obc_error_code_t errCode = rxHandlers[msg.cmd.id](&msg);
-            LOG_IF_ERROR_CODE(errCode);
-        }
+
+        // Execute callback
+        obc_error_code_t errCode = rxHandlers[msg.cmd.id](&msg);
+        LOG_IF_ERROR_CODE(errCode);
     }
 }
 
