f0a0d047a00a42207eab5181883d841276ae3d53
Aryan Kalia <kalia.aryan2013@gmail.com>
2023-05-23 01:27:43 -0400

    remove unnecessary length check

--- comms/source/decode_telemetry.c
+++ comms/source/decode_telemetry.c
@@ -140,7 +140,7 @@ static void vDecodeTask(void * pvParameters){
 static obc_error_code_t decodePacket(packed_ax25_packet_t *data, packed_rs_packet_t *rsData, aes_data_t *aesData) {
     obc_error_code_t errCode;
     RETURN_IF_ERROR_CODE(ax25Recv(data, rsData));
-    RETURN_IF_ERROR_CODE(rsDecode(rsData, aesData->rawData, (uint8_t)RS_DECODED_SIZE));
+    RETURN_IF_ERROR_CODE(rsDecode(rsData, aesData->rawData));
     uint8_t decryptedData[RS_DECODED_SIZE-AES_IV_SIZE];
     RETURN_IF_ERROR_CODE(aes128Decrypt(aesData, decryptedData));
     RETURN_IF_ERROR_CODE(handleCommands(decryptedData));
--- comms/source/fec.c
+++ comms/source/fec.c
@@ -35,20 +35,16 @@ obc_error_code_t rsEncode(packed_telem_packet_t *telemData, packed_rs_packet_t *
  * 
  * @param rsData 255 byte array that has encoded reed solomon data
  * @param decodedData pointer to a union that includes a uint8_t array of size 223B
- * @param decodedDataLen length of the decoded data array
  * 
  * @return obc_error_code_t - whether or not the data was successfully decoded
 */
-obc_error_code_t rsDecode(packed_rs_packet_t *rsData, uint8_t *decodedData, uint8_t decodedDataLen){
+obc_error_code_t rsDecode(packed_rs_packet_t *rsData, uint8_t *decodedData){
     if (rsData == NULL)
         return OBC_ERR_CODE_INVALID_ARG;
 
     if (decodedData == NULL)
         return OBC_ERR_CODE_INVALID_ARG;
     
-    if (decodedDataLen != RS_DECODED_SIZE)
-        return OBC_ERR_CODE_INVALID_ARG;
-
     int8_t decodedLength = correct_reed_solomon_decode(rs, rsData->data, RS_ENCODED_SIZE, decodedData);
     
     if(decodedLength == -1)
