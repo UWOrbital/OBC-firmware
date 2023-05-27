3b1b5e225fc98febf54d51de9194f7e708a56136
shouza1712 <71526713+shouza1712@users.noreply.github.com>
2023-05-27 18:24:40 -0400

    created header file

--- cdh/include/temp_reader.h
+++ cdh/include/temp_reader.h
@@ -2,6 +2,21 @@
 #define CDH_INCLUDE_TEMP_READER_H_
 
 #include <stdint.h>
+#include <FreeRTOS.h>
+#include <os_timer.h>
+#include <sys_common.h>
+#include <obc_time.h>
 
+/**
+ * @brief Initialize the temp read handler handler task and associated FreeRTOS constructs (queues, timers, etc.)
+ * 
+ */
+void initTempReaderHandler(void);
+
+/**
+ * @brief Call back function for the freeRTOS timer created to read temperature periodically
+ * 
+ */
+void vTimerCallback( TimerHandle_t xTimer );
 
 #endif /* CDH_INCLUDE_TEMP_READER_H_ */
