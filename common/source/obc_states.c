9c24fff69371c0bd99fcac984c94c8a96739b41b
dgobalak <dgobalak@uwaterloo.ca>
2023-05-28 04:46:18 -0400

    Save state to fram whenever it's changed

--- common/source/obc_states.c
+++ common/source/obc_states.c
@@ -3,6 +3,7 @@
 #include "obc_logging.h"
 #include "obc_assert.h"
 #include "obc_time.h"
+#include "obc_persistent_store.h"
 #include "telemetry_manager.h"
 
 static obc_state_t currStateOBC;
@@ -13,7 +14,10 @@ obc_error_code_t changeStateOBC(obc_state_t newState) {
     
     currStateOBC = newState;
     telemetry_data_t telemData = {.id = TELEM_OBC_STATE, .timestamp = getCurrentUnixTime(), .obcState = currStateOBC};
-    
     RETURN_IF_ERROR_CODE(addTelemetryData(&telemData));
+
+    obc_state_persist_data_t persist = {.obcState = newState};
+    RETURN_IF_ERROR_CODE(setPersistentObcState(&persist));
+
     return OBC_ERR_CODE_SUCCESS;
 }
