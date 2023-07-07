7ef317bde1924642f5f62ee9c3e501181a6e8cfa
Daniel Gobalakrishnan <danielgobalakrishnan@gmail.com>
2023-07-07 13:05:24 -0400

    attempt 2

--- obc/cmake/toolchain_arm_none_eabi.cmake
+++ obc/cmake/toolchain_arm_none_eabi.cmake
@@ -4,7 +4,7 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 set(TOOLCHAIN_DIR ${CMAKE_BINARY_DIR}/toolchain)
 
 if(NOT EXISTS ${TOOLCHAIN_DIR})
-    include(obc/cmake/download_arm_toolchain.cmake)
+    include(${CMAKE_SOURCE_DIR}/obc/cmake/download_arm_toolchain.cmake)
 endif()
 
 # Target definition
