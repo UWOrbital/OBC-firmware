b951a3ac91212c06ddbce6cfc7f6b426469f17dc
Navtaj Hundal <n2hundal@uwaterloo.ca>
2023-07-11 23:58:14 -0400

    .

--- common/include/obc_board_config.h
+++ common/include/obc_board_config.h
@@ -41,6 +41,15 @@
 #define DS3232_INT_PORT gioPORTA
 #define DS3232_INT_PIN 0U
 
+// RFFM6404 pin numbers
+#define RFFM6404_TR_PIN_NUM 6U
+#define RFFM6404_EN_PIN_NUM 7U
+#define RFFM6404_BYP_PIN_NUM 2U
+
+// RFFM6404 pin ports
+#define RFFM6404_TR_PIN_PORT gioPORTA
+#define RFFM6404_EN_PIN_PORT gioPORTA
+
 #elif defined(OBC_REVISION_1)
 // Serial config
 #define UART_PRINT_REG sciREG
@@ -121,6 +130,15 @@
 #define CC1120_PKT_SYNC_RXTX_hetPORT1_PIN 28U  // cc1120 GIO2
 #define CC1120_TX_THR_PKT_hetPORT1_PIN 30U     // cc1120 GIO3
 #define CC1120_SYNC_EVENT_PIN CC1120_PKT_SYNC_RXTX_hetPORT1_PIN
+
+// RFFM6404 pin numbers
+#define RFFM6404_TR_PIN_NUM 6U
+#define RFFM6404_EN_PIN_NUM 7U
+#define RFFM6404_BYP_PIN_NUM 2U
+
+// RFFM6404 pin ports
+#define RFFM6404_TR_PIN_PORT gioPORTA
+#define RFFM6404_EN_PIN_PORT gioPORTA
 
 #else
 #error Board configuration not defined
