7df06bd2c5433aa09339371b922002cd24f3da76
Jac2125 <jace4188@gmail.com>
2023-07-15 15:40:35 -0400

    CAN bus is tested using canTransmit function

--- util/bringup/interface_tests/source/test_can.c
+++ util/bringup/interface_tests/source/test_can.c
@@ -1,4 +1,14 @@
 #include "test_can.h"
 #include "obc_sci_io.h"
+#include "can.h"
 
-void testCAN(void) { sciPrintf("Testing CAN...\r\n"); }
+void testCAN(void) {
+  sciPrintf("Testing CAN...\r\n");
+  canInit();
+  const uint8 txData = 0xFFU;
+  if (canTransmit(canREG1, 10, &txData) == 0U) {
+    sciPrintf("Tramsmission Failed.\r\n");
+  }
+
+  sciPrintf("Transmission Succeeded.\r\n");
+}
