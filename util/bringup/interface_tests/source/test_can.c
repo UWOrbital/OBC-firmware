41e0a6e2142e13aba2df5787b3da7e82bf73a171
Jac2125 <jace4188@gmail.com>
2023-07-17 19:15:18 -0400

    Macro for CAN bus mailbox number defined

--- util/bringup/interface_tests/source/test_can.c
+++ util/bringup/interface_tests/source/test_can.c
@@ -2,11 +2,14 @@
 #include "obc_sci_io.h"
 #include "can.h"
 
+// A number of Mailbox in CAN bus is in between 1 and 64
+#define boxNum 1
+
 void testCAN(void) {
   sciPrintf("Testing CAN...\r\n");
   canInit();
   const uint8 txData = 0xFFU;
-  if (canTransmit(canREG1, 10, &txData) == 0U) {
+  if (canTransmit(canREG1, boxNum, &txData) == 0U) {
     sciPrintf("Tramsmission Failed.\r\n");
   }
 
