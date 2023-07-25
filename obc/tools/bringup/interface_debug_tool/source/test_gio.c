4b876521265c39b2850d897586b9b67e6378723d
Jerry Hu <88156210+Jitao-Hu@users.noreply.github.com>
2023-07-25 00:23:50 -0400

    bring up utility GPIO (#83)
    # Purpose
    GIO Utility Bringup
    
    # New Changes
    Add gio test function to set all GPIO pin to high
    
    # Testing
    Source code can be made successfully
    
    # Outstanding Changes
    - If there are non-critical changes (i.e. additional features) that can
    be made to this feature in the future, indicate them here.
    
    ---------
    
    Co-authored-by: Jerry Hu <j376hu@uwaterloo.ca>
    Co-authored-by: Navtaj Hundal <n2hundal@uwaterloo.ca>

--- obc/tools/bringup/interface_debug_tool/source/test_gio.c
+++ obc/tools/bringup/interface_debug_tool/source/test_gio.c
@@ -1,4 +1,18 @@
 #include "test_gio.h"
 #include "obc_sci_io.h"
+#include "gio.h"
 
-void testGIO(void) { sciPrintf("Testing GIO...\r\n"); }
+void testGIO(void) {
+  sciPrintf("Testing GIO...\r\n");
+  /* GIOA has 8 pins */
+  for (int i = 0; i < 8; i++) {
+    gioSetBit(gioPORTA, i, 1);
+  }
+
+  /* GIOB has 4 pins */
+  for (int i = 0; i < 4; i++) {
+    gioSetBit(gioPORTB, i, 1);
+  }
+
+  sciPrintf("Set bit 0 to 7 on portA and bit 0 to 3 on portB to high\r\n");
+}
