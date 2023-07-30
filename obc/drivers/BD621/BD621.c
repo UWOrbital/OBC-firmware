37fece83b5ea790d5938a7b0f1ca56cfd92b4c58
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-07-29 20:15:02 -0400

    Addressed daniels comments, added additonal checks

--- obc/drivers/BD621/BD621.c
+++ obc/drivers/BD621/BD621.c
@@ -1,85 +1,97 @@
 #include "BD621.h"
 #include <stdlib.h>
+#include "het.h"
 
+static bool isValidPwm(uint32_t pwm);
+static bool isValidHetBase(hetRAMBASE_t* hetRam);
+static bool isValidMotorParameters(const DC_motor_t* motor);
 
-obc_error_code_t StartMotor(const DCMotor_t* motor) {
+obc_error_code_t startMotor(const DC_motor_t* motor) {
   if (motor == NULL) return OBC_ERR_CODE_INVALID_ARG;
-  pwmStart(motor->hetBase, motor->FinPWM);
-  pwmStart(motor->hetBase, motor->RinPWM);
+  if (!isValidMotorParameters(motor)) return OBC_ERR_CODE_INVALID_ARG;
+
+  pwmStart(motor->hetBase, motor->finPwm);
+  pwmStart(motor->hetBase, motor->rinPwm);
   return OBC_ERR_CODE_SUCCESS;
 }
 
-obc_error_code_t DriveMotorPWM(const DCMotor_t* motor, int32_t duty, float period) {
+obc_error_code_t driveMotorPWM(const DC_motor_t* motor, int32_t duty, float64 period) {
   if (motor == NULL) return OBC_ERR_CODE_INVALID_ARG;
+  if (!isValidMotorParameters(motor)) return OBC_ERR_CODE_INVALID_ARG;
+
+  float64 frequency = 1 / period;
+  if ((frequency < MOTOR_MIN_FREQUENCY) || (frequency > MOTOR_MAX_FREQUENCY)) return OBC_ERR_CODE_INVALID_ARG;
+
   if (abs(duty) > 100) duty = 100;
+  hetSIGNAL_t signal = {.duty = duty, .period = period};
 
-  hetSIGNAL_t signal = {
-    .duty = duty,
-    .period = period
-  };
-  
   if (duty >= 0) {
-    pwmSetSignal(motor->hetBase, motor->FinPWM, signal);
-    pwmSetDuty(motor->hetBase, motor->RinPWM, MOTOR_DRIVE_LOW);
+    pwmSetSignal(motor->hetBase, motor->finPwm, signal);
+    pwmSetDuty(motor->hetBase, motor->rinPwm, MOTOR_DRIVE_LOW);
   } else {
-    pwmSetDuty(motor->hetBase, motor->FinPWM, MOTOR_DRIVE_LOW);
-    pwmSetSignal(motor->hetBase, motor->RinPWM, signal);
+    pwmSetDuty(motor->hetBase, motor->finPwm, MOTOR_DRIVE_LOW);
+    pwmSetSignal(motor->hetBase, motor->rinPwm, signal);
   }
   return OBC_ERR_CODE_SUCCESS;
-
 }
 
-
-obc_error_code_t DriveMotorTorque(const DCMotor_t* motor, float speed, float period) {
+obc_error_code_t driveMotorTorque(const DC_motor_t* motor, float speed, float64 period) {
   if (motor == NULL) return OBC_ERR_CODE_INVALID_ARG;
+  if (!isValidMotorParameters(motor)) return OBC_ERR_CODE_INVALID_ARG;
+  if (motor->maxSpeed < MOTOR_SPEED_LOWER_BOUND) return OBC_ERR_CODE_INVALID_ARG;
 
-  uint32 duty = (uint32)  (abs(speed/motor->maxTorque) * 100);
+  float64 frequency = 1 / period;
+  if ((frequency < MOTOR_MIN_FREQUENCY) || (frequency > MOTOR_MAX_FREQUENCY)) return OBC_ERR_CODE_INVALID_ARG;
+
+  int32_t duty = (int32_t)(abs(speed / motor->maxSpeed) * 100);
   if (duty > 100) duty = 100;
+  if (speed <= 0) duty = -duty;
 
-  hetSIGNAL_t signal = {
-    .duty = duty,
-    .period = period
-  };
-  
-  if (speed >= 0) {
-    pwmSetSignal(motor->hetBase, motor->FinPWM, signal);
-    pwmSetDuty(motor->hetBase, motor->RinPWM, MOTOR_DRIVE_LOW);
-  } else {
-    pwmSetDuty(motor->hetBase, motor->FinPWM, MOTOR_DRIVE_LOW);
-    pwmSetSignal(motor->hetBase, motor->RinPWM, signal);
-  }
-  return OBC_ERR_CODE_SUCCESS;
+  return DriveMotorPWM(motor, duty, period);
 }
 
-obc_error_code_t ForwardMotor(const DCMotor_t* motor) {
+obc_error_code_t forwardMotor(const DC_motor_t* motor) {
   if (motor == NULL) return OBC_ERR_CODE_INVALID_ARG;
-  pwmSetDuty(motor->hetBase, motor->FinPWM, MOTOR_OPERATE_FR);
-  pwmSetDuty(motor->hetBase, motor->RinPWM, MOTOR_DRIVE_LOW);
+  if (!isValidMotorParameters(motor)) return OBC_ERR_CODE_INVALID_ARG;
+
+  pwmSetDuty(motor->hetBase, motor->finPwm, MOTOR_OPERATE_FR);
+  pwmSetDuty(motor->hetBase, motor->rinPwm, MOTOR_DRIVE_LOW);
   return OBC_ERR_CODE_SUCCESS;
 }
 
-
-obc_error_code_t ReverseMotor(const DCMotor_t* motor) {
+obc_error_code_t reverseMotor(const DC_motor_t* motor) {
   if (motor == NULL) return OBC_ERR_CODE_INVALID_ARG;
-  pwmSetDuty(motor->hetBase, motor->FinPWM, MOTOR_DRIVE_LOW);
-  pwmSetDuty(motor->hetBase, motor->RinPWM, MOTOR_OPERATE_FR);
+  if (!isValidMotorParameters(motor)) return OBC_ERR_CODE_INVALID_ARG;
+
+  pwmSetDuty(motor->hetBase, motor->finPwm, MOTOR_DRIVE_LOW);
+  pwmSetDuty(motor->hetBase, motor->rinPwm, MOTOR_OPERATE_FR);
   return OBC_ERR_CODE_SUCCESS;
 }
 
-
-obc_error_code_t BrakeMotor(const DCMotor_t* motor) {
+obc_error_code_t brakeMotor(const DC_motor_t* motor) {
   if (motor == NULL) return OBC_ERR_CODE_INVALID_ARG;
-  pwmSetDuty(motor->hetBase, motor->FinPWM, MOTOR_DRIVE_HIGH);
-  pwmSetDuty(motor->hetBase, motor->RinPWM, MOTOR_DRIVE_HIGH);
+  if (!isValidMotorParameters(motor)) return OBC_ERR_CODE_INVALID_ARG;
+
+  pwmSetDuty(motor->hetBase, motor->finPwm, MOTOR_DRIVE_HIGH);
+  pwmSetDuty(motor->hetBase, motor->rinPwm, MOTOR_DRIVE_HIGH);
   return OBC_ERR_CODE_SUCCESS;
 }
 
-
-obc_error_code_t IdleMotor(const DCMotor_t* motor) {
+obc_error_code_t idleMotor(const DC_motor_t* motor) {
   if (motor == NULL) return OBC_ERR_CODE_INVALID_ARG;
-  pwmStop(motor->hetBase, motor->FinPWM);
-  pwmStop(motor->hetBase, motor->RinPWM);
+  if (!isValidMotorParameters(motor)) return OBC_ERR_CODE_INVALID_ARG;
+
+  pwmStop(motor->hetBase, motor->finPwm);
+  pwmStop(motor->hetBase, motor->rinPwm);
   return OBC_ERR_CODE_SUCCESS;
 }
 
+static bool isValidPwm(uint32_t pwm) {
+  return ((pwm == pwm0) || (pwm == pwm1) || (pwm == pwm2) || (pwm == pwm3) || (pwm == pwm4) || (pwm == pwm5) ||
+          (pwm == pwm6) || (pwm == pwm7));
+}
+static bool isValidHetBase(hetRAMBASE_t* hetRam) { return (hetRam == hetRAM1 || hetRam == hetRAM2); }
 
+static bool isValidMotorParameters(const DC_motor_t* motor) {
+  return (isValidPwm(motor->finPwm) && isValidPwm(motor->rinPwm) && isValidHetBase(motor->hetBase));
+}
--- obc/drivers/BD621/BD621.h
+++ obc/drivers/BD621/BD621.h
@@ -6,16 +6,18 @@
 #define MOTOR_DRIVE_HIGH 100
 #define MOTOR_OPERATE_FR 50
 #define MOTOR_DRIVE_LOW 0
-
+#define MOTOR_SPEED_LOWER_BOUND 0.01
+#define MOTOR_MAX_FREQUENCY 100000
+#define MOTOR_MIN_FREQUENCY 20000
 
 /**
-* @struct DCMotor_t 
+* @struct DC_motor_t
 * @brief Struct used to hold the motor's pwm modules and which HET driver its running on
 * @param hetBase: pointer to HET RAM
     hetRAM1: HET1 RAM pointer
-    hetRAM2: HET2 RAM pointer 
-* @param FinPWM: pointer to pwm driver that controls FIN (Forward pin of motor driver) 
-* @param RinPWM: pointer to pwm driver that controls RIN (Reverse pin of motor driver)
+    hetRAM2: HET2 RAM pointer
+* @param finPwm: pointer to pwm driver that controls FIN (Forward pin of motor driver)
+* @param rinPwm: pointer to pwm driver that controls RIN (Reverse pin of motor driver)
     pwm0: Pwm 0
     pwm1: Pwm 1
     pwm2: Pwm 2
@@ -24,64 +26,64 @@
     pwm5: Pwm 5
     pwm6: Pwm 6
     pwm7: Pwm 7
-* @param  maxTorque: The torque produced at 100% duty cycle. 
+* @param  maxTorque: The torque produced at 100% duty cycle.
 **/
 typedef struct {
   hetRAMBASE_t* hetBase;
-  uint32 FinPWM, RinPWM;
-  const float maxTorque;  
-} DCMotor_t;
+  uint32_t finPwm, rinPwm;
+  const float maxSpeed;
+} DC_motor_t;
 
 /**
-* @brief Starts the PWM channels.
-* @param motor: pointer to motor struct
-* @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
-**/
-obc_error_code_t StartMotor(const DCMotor_t* motor);
+ * @brief Starts the PWM channels.
+ * @param motor: pointer to motor struct
+ * @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
+ **/
+obc_error_code_t startMotor(const DC_motor_t* motor);
 
 /**
-* @brief Drives the motor at the specified duty cycle and period.
-* @param motor: pointer to motor struct.
-* @param duty: The duty cycle give as a int32_t casted downt to uint32 with considerations for sign. 
-* @param period: The PWM period in us. 
-* @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
-**/
-obc_error_code_t DriveMotorPWM(const DCMotor_t* motor, int32_t speed, float period);
+ * @brief Drives the motor at the specified duty cycle and period.
+ * @param motor: pointer to motor struct.
+ * @param duty: The duty cycle give as a int32_t casted downt to uint32 with considerations for sign.
+ * @param period: The PWM period in us.
+ * @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
+ **/
+obc_error_code_t driveMotorPWM(const DC_motor_t* motor, int32_t speed, float64 period);
 
 /**
 * @brief Drives the motor at the specified speed and period.
 * @param motor: pointer to motor struct.
 * @param speed: The signed speed value at which the motor should be run. Set to maxiumum torque for the motor
-if it greater than that in magnitude. 
-* @param period: The PWM period in us. 
+if it greater than that in magnitude. Same units as maxSpeed.
+* @param period: The PWM period in us.
 * @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
 **/
-obc_error_code_t DriveMotorTorque(const DCMotor_t* motor, float speed, float period);
+obc_error_code_t driveMotorTorque(const DC_motor_t* motor, float speed, float64 period);
 
 /**
-* @brief Drives the motor clockwise at MOTOR_OPERATE_FR (%) duty cycle
-* @param motor: pointer to motor struct. 
-* @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
-*/
-obc_error_code_t ForwardMotor(const DCMotor_t* motor);
+ * @brief Drives the motor clockwise at MOTOR_OPERATE_FR (%) duty cycle
+ * @param motor: pointer to motor struct.
+ * @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
+ */
+obc_error_code_t forwardMotor(const DC_motor_t* motor);
 
 /**
-* @brief Drives the motor counterclockwise at MOTOR_OPERATE_FR (%) duty cycle
-* @param motor: pointer to motor struct.
-* @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL. 
-**/
-obc_error_code_t ReverseMotor(const DCMotor_t* motor);
-
-/** 
-* @brief Sets PWM signal to 100, setting the motor to HIGH until signal is changed.
-* @param DC_Motor: pointer to motor struct.
-* @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
-**/
-obc_error_code_t BrakeMotor(const DCMotor_t* motor);
+ * @brief Drives the motor counterclockwise at MOTOR_OPERATE_FR (%) duty cycle
+ * @param motor: pointer to motor struct.
+ * @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
+ **/
+obc_error_code_t reverseMotor(const DC_motor_t* motor);
 
 /**
-* @brief Sets the motor to idle mode.  
-* @param motor: pointer to motor struct. 
-* @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
-**/
-obc_error_code_t IdleMotor(const DCMotor_t* motor);
+ * @brief Sets PWM signal to 100, setting the motor to HIGH until signal is changed.
+ * @param DC_Motor: pointer to motor struct.
+ * @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
+ **/
+obc_error_code_t brakeMotor(const DC_motor_t* motor);
+
+/**
+ * @brief Sets the motor to idle mode.
+ * @param motor: pointer to motor struct.
+ * @return Returns OBC_ERR_CODE_SUCCESS if successful, OBC_ERR_CODE_INVALID_ARG if motor is a pointer to NULL.
+ **/
+obc_error_code_t idleMotor(const DC_motor_t* motor);
