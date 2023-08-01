1f75051736df049aa1413bfe4ee58367f20c8c45
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-08-01 19:48:25 -0400

    Added a seperate defs file

--- obc/drivers/max17320/max17320.h
+++ obc/drivers/max17320/max17320.h
@@ -1,5 +1,12 @@
 #pragma once
 
 #include "obc_errors.h"
+#include "max17320_defs.h"
 
 #include <stdint.h>
+
+obc_error_code_t writeAnalogRegister(bms_analog_register_t register, uint16_t data);
+obc_error_code_t readAnalogRegister(bms_analog_register_t register, uint16_t data);
+
+obc_error_code_t writeConfigRegister(bms_config_register_t register, uint16_t data);
+obc_error_code_t readConfigRegister(bms_config_register_t register, uint16_t data);
---
+++ obc/drivers/max17320/max17320_defs.h
@@ -0,0 +1,9 @@
+#pragma once
+
+typedef enum {
+
+} bms_analog_register_t;
+
+typedef enum {
+
+} bms_config_register_t;
