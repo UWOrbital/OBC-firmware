e28945f2e2870c126b1342212aa23f612d72a30f
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-08-01 17:31:21 -0400

    Added return if error code

--- obc/drivers/max17320/max17320.c
+++ obc/drivers/max17320/max17320.c
@@ -5,11 +5,22 @@
 
 #include <stdint.h>
 
+#define BMS_INTERNAL_MEM_LOWER_0 0x000
+#define BMS_INTERNAL_MEM_UPPER_0 0x0FF
+#define BMS_INTERNAL_MEM_LOWER_1 0x180
+#define BMS_INTERNAL_MEM_UPPER_1 0x1FF
+
+#define BMS_INTERNAL_MEM_MASK 0x00FF
+#define BMS_SLAVE_BIT_MASK (1 << 8)
+
+#define BMS_I2C_SLAVE_ADDR_0 15
+#define BMS_I2C_SLAVE_ADDR_1 20
+
 static obc_error_code_t isValidMemoryAddress(uint16_t* addr, uint16_t* slaveAddr);
 static inline obc_error_code_t memoryAddressMatchesSlaveAddr(uint16_t addr, uint16_t slaveAddr);
 static obc_error_code_t isValidTransaction(uint16_t* startAddr, uint16_t* slaveAddrPtr, uint16_t size);
 static obc_error_code_t writeToBmsBlockRegister(uint16_t startAddr, uint8_t* data, uint16_t size);
-static obc_error_code_t readFromBmsRegister(uint32_t addr, uint16_t size, uint8_t* buf);
+static obc_error_code_t readFromBmsBlockRegister(uint32_t addr, uint16_t size, uint8_t* buf);
 
 static obc_error_code_t isValidMemoryAddress(uint16_t* addr, uint16_t* slaveAddr) {
   bool isValid = (((addr >= BMS_INTERNAL_MEM_LOWER_0) && (addr <= BMS_INTERNAL_MEM_UPPER_0)) ||
@@ -33,45 +44,32 @@ static inline obc_error_code_t memoryAddressMatchesSlaveAddr(uint16_t addr, uint
   return OBC_ERR_CODE_SUCCESS;
 }
 
-static obc_error_code_t isValidTransaction(uint16_t* startAddr, uint16_t* slaveAddrPtr, uint16_t size) {
-  uint16_t slaveAddr = 0;
+static obc_error_code_t isValidTransaction(uint16_t* startAddr, uint16_t* slaveAddr, uint16_t size) {
   bool isValid = 0;
 
-  obc_error_code_t errCode = isValidMemoryAddress(startAddr, &slaveAddr);
-  if (errCode != OBC_ERR_CODE_SUCCESS) {
-    return errCode;
-  }
+  RETURN_IF_ERROR_CODE(isValidMemoryAddress(startAddr, slaveAddr));
 
   for (uint16_t off = 0; off < size; off++) {
     uint32_t addrTmp = startAddr + off;
-    if (memoryAddressMatchesSlaveAddr(addrTmp, slaveAddr) == OBC_ERR_CODE_BMS_MEM_SLAVE_ADDR_MISMATCH) {
+    if (memoryAddressMatchesSlaveAddr(addrTmp, *slaveAddr) == OBC_ERR_CODE_BMS_MEM_SLAVE_ADDR_MISMATCH) {
       return OBC_ERR_CODE_BMS_NEW_TRANSACTION;
     }
   }
-  *slaveAddrPtr = slaveAddr;
   return OBC_ERR_CODE_SUCCESS;
 }
 
 static obc_error_code_t writeToBmsBlockRegister(uint16_t startAddr, uint8_t* data, uint16_t size) {
   uint16_t slaveAddr = 0;
-  uint16_t startIntAddr = startAddr;
+  uint16_t startCommAddr = startAddr;
 
-  obc_error_code_t errCode = isValidTransaction(&startIntAddr, &slaveAddr, size);
-  if (errCode != OBC_ERR_CODE_SUCCESS) {
-    return errCode;
-  }
-
-  return (i2cWriteReg(slaveAddr, startIntAddr, data, size));
+  RETURN_IF_ERROR_CODE(isValidTransaction(&startCommAddr, &slaveAddr, size));
+  return (i2cWriteReg(slaveAddr, startCommAddr, data, size));
 }
 
-static obc_error_code_t readFromBmsRegister(uint32_t startAddr, uint16_t size, uint8_t* buf) {
+static obc_error_code_t readFromBmsBlockRegister(uint32_t startAddr, uint16_t size, uint8_t* buf) {
   uint16_t slaveAddr = 0;
-  uint16_t startIntAddr = startAddr;
+  uint16_t startCommAddr = startAddr;
 
-  obc_error_code_t errCode = isValidTransaction(&startIntAddr, &slaveAddr, size);
-  if (errCode != OBC_ERR_CODE_SUCCESS) {
-    return errCode;
-  }
-
-  return (i2cReadReg(slaveAddr, startAddr, buf, size));
+  RETURN_IF_ERROR_CODE(isValidTransaction(&startCommAddr, &slaveAddr, size));
+  return (i2cReadReg(slaveAddr, startCommAddr, buf, size));
 }
--- obc/drivers/max17320/max17320.h
+++ obc/drivers/max17320/max17320.h
@@ -3,14 +3,3 @@
 #include "obc_errors.h"
 
 #include <stdint.h>
-
-#define BMS_INTERNAL_MEM_LOWER_0 0x000
-#define BMS_INTERNAL_MEM_UPPER_0 0x0FF
-#define BMS_INTERNAL_MEM_LOWER_1 0x180
-#define BMS_INTERNAL_MEM_UPPER_1 0x1FF
-
-#define BMS_INTERNAL_MEM_MASK 0x00FF
-#define BMS_SLAVE_BIT_MASK (1 << 8)
-
-#define BMS_I2C_SLAVE_ADDR_0 15
-#define BMS_I2C_SLAVE_ADDR_1 20
