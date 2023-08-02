ca7410fc5756a4be7e67774730a9648369ba9d67
jac2125 <jace4188@gmail.com>
2023-08-01 23:08:26 -0400

    new CAN testing opened and now it lets the user choose the reg num

--- obc/tools/bringup/interface_debug_tool/source/test_can.c
+++ obc/tools/bringup/interface_debug_tool/source/test_can.c
@@ -1,4 +1,37 @@
 #include "test_can.h"
 #include "obc_sci_io.h"
+#include "can.h"
 
-void testCAN(void) { sciPrintf("Testing CAN...\r\n"); }
+#define REG_NUM_CHARS 1U
+
+// CAN Message Box Number Rule:
+//	TX: 1
+//	RX: 2
+#define TXMessageBox 1
+
+void testCAN(void) {
+  sciPrintf("Testing CAN...\r\n");
+  canInit();
+
+  uint16_t regNum;
+  unsigned char buffer[REG_NUM_CHARS] = {'\0'};
+  char *c;
+
+  while (1) {
+    sciPrintf("Enter a valid register address\n");
+    obc_error_code_t error = sciRead(buffer, REG_NUM_CHARS);
+    if (error == OBC_ERR_CODE_SUCCESS) {
+      regNum = strtol(buffer, &c, 10);
+      break;
+    }
+    sciPrintf("Error reading from SCI! - %d\r\n", (int)error);
+  }
+
+  uint32 canReg = canREG1->CTL + regNum;
+  const uint8 txData = 0xFFU;
+  if (canTransmit(&canReg, TXMessageBox, &txData) == 0U) {
+    sciPrintf("Tramsmission Failed.\r\n");
+    return;
+  }
+  sciPrintf("Transmission Succeeded.\r\n");
+}
