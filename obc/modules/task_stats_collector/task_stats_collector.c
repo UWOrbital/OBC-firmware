c53cd4d51483765dc590128b4471e21b836dafc3
Navtaj Hundal <n2hundal@uwaterloo.ca>
2023-08-10 23:21:23 -0400

    small change

--- obc/modules/task_stats_collector/task_stats_collector.c
+++ obc/modules/task_stats_collector/task_stats_collector.c
@@ -3,6 +3,7 @@
 #include "obc_task_config.h"
 #include "obc_sci_io.h"
 #include "obc_privilege.h"
+#include "obc_logging.h"
 
 #include <FreeRTOSConfig.h>
 #include <FreeRTOS.h>
@@ -30,13 +31,15 @@ void initTaskStatsCollector(void) {
 }
 
 static void vTaskStatsCollector(void *pvParameters) {
+  obc_error_code_t errCode;
   prvRaisePrivilege();
   while (1) {
     vTaskDelay(pdMS_TO_TICKS(15000));
     char taskStatsString[TASK_STATS_BUFFER_SIZE] = {0};
     vTaskList(taskStatsString);
-    sciPrintText((unsigned char *)taskTableHeaderStr, strlen(taskTableHeaderStr), UART_MUTEX_BLOCK_TIME);
-    sciPrintText((unsigned char *)taskStatsString, TASK_STATS_BUFFER_SIZE, UART_MUTEX_BLOCK_TIME);
+    LOG_IF_ERROR_CODE(
+        sciPrintText((unsigned char *)taskTableHeaderStr, strlen(taskTableHeaderStr), UART_MUTEX_BLOCK_TIME));
+    LOG_IF_ERROR_CODE(sciPrintText((unsigned char *)taskStatsString, TASK_STATS_BUFFER_SIZE, UART_MUTEX_BLOCK_TIME));
   }
 }
 #endif
