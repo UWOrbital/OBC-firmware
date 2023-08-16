300d38d88b1856cf3fcdb1142dd532d101c26549
Navtaj Hundal <n2hundal@uwaterloo.ca>
2023-08-15 21:44:09 -0400

    .

--- test/test_interfaces/unit/test_obc_gs_fec.cpp
+++ test/test_interfaces/unit/test_obc_gs_fec.cpp
@@ -22,7 +22,12 @@ TEST(TestFecEncodeDecode, EncodeDecodeZeroData) {
 TEST(TestFecEncodeDecode, EncodeDecodeNonZero) {
   packed_rs_packet_t encodedData = {0};
   uint8_t data[RS_DECODED_SIZE];
-  memset(data, 0xFF, RS_DECODED_SIZE);
+  uint32_t seed = 0x7A21C3;  // random number
+  for (uint16_t i = 0; i < RS_ENCODED_SIZE; ++i) {
+    // Pseudorandom generation using a simple algorithm
+    seed = (seed * 1103515245 + 12345) % (1 << 31);
+    data[i] = (uint8_t)(seed & 0xFF);
+  }
   initRs();
   ASSERT_EQ(rsEncode(data, &encodedData), 0);
   // flip some bits
