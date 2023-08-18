f35d2491c08087fc6f84b8f43da5d82996a4e89d
Adrian Ho <Adrianfpho@gmail.com>
2023-08-18 10:14:58 -0400

    this is the last one fr

--- obc/drivers/tpl5010/tpl5010.c
+++ obc/drivers/tpl5010/tpl5010.c
@@ -10,11 +10,7 @@
 #define DONE_SIGNAL_OFF 0
 #define TPL5010_GIO_PORT gioPORTB
 
-void tpl5010Init(void) {
-  gioEnableNotification(TPL5010_GIO_PORT, TPL5010_WAKE_PIN);
-
-  return OBC_ERR_CODE_SUCCESS;
-}
+void tpl5010Init(void) { gioEnableNotification(TPL5010_GIO_PORT, TPL5010_WAKE_PIN); }
 
 void feedHardwareWatchdog(void) {
   gioSetPort(TPL5010_GIO_PORT, DONE_SIGNAL_ON);  // pull high then low
