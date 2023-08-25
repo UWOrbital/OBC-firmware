8c9eb1d27c024e8dc36a29c1d1dbe708248f0a5f
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-08-25 18:46:32 -0400

    Added counter defs

--- obc/modules/runtimeTick_mgr/runtimeTick_mgr.c
+++ obc/modules/runtimeTick_mgr/runtimeTick_mgr.c
@@ -1,11 +1,24 @@
 #include "runtimeTick_mgr.h"
 
+#include <obc_errors.h>
+
+#include <rti.h>
 #include <stdint.h>
 
-void vConfigureRuntimeStats(void) {
+static uint32_t counter = 0;
+static uint32_t period = 0;
 
+obc_error_code_t setRtiCounter(uint32_t counterBlock, uint32_t period) {
+  if (counter > 1) return OBC_ERR_CODE_INVALID_ARG;
+  counter = counterBlock;
+}
+
+void vConfigureRuntimeStats(void) {
+  rtiInit();
+  rtiStartCounter(counter);
+  rtiSetPeriod(counter, period);
 }
 
 uint64_t vSystemTickGet(void) {
-
+  return rtiGetCurrentTick(counter);
 }
