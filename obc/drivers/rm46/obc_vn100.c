a86b7e8b33e10aaab2b352fe1af670e389e04849
Joel <joelmyleee@gmail.com>
2023-08-25 11:59:18 -0400

    Added packet types and enum of commands

--- obc/drivers/rm46/obc_vn100.c
+++ obc/drivers/rm46/obc_vn100.c
@@ -15,23 +15,48 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define VN100_BAUDRATE  1152000
+#define MAX_COMMAND_SIZE    256
+#define TICK_TIMEOUT        portMAX_DELAY
+
+/**
+ * @brief Generalized request function to handle mulitple different packet types
+ * @param cmd the input command to determine what packet to request
+ * 
+ *      VN_YPR      // Get yaw, pitch and roll measurements 
+ *      VN_MAG      // Get magnetic measurements
+ *      VN_ACC      // Get acceleration measurements
+ *      VN_GYR      // Get angular rate measurements
+ *      VN_YMR      // Get all of the above       
+ *
+ * @return OBC_ERR_CODE_SUCCESS on success, else an error code
+ */
+
+static obc_error_code_t requestCMD (vn_cmd_t cmd);
+
 void initVN100(void) {
-    
+    //TODO:
 }
 
-obc_error_code_t VN100SetBaudRate(int baudrate) {
-
+obc_error_code_t resetModule() {
+    char cmd [MAX_COMMAND_SIZE] = "$VNRST*4D";
+    return sciSendBytes(cmd, MAX_COMMAND_SIZE, TICK_TIMEOUT);
 }
 
-obc_error_code_t requestYPR(sciBase_t * sciREG) {
-    // use sciSendByte, to transmit a request packet to the VN-100
-    sciSendByte()
+obc_error_code_t VN100SetBaudrate(uint32_t baudrate) {
+    //TODO:
 }
 
-obc_error_code_t retrieveYPR(ypr_packet* buf) {
-    if (buf == NULL) {
+obc_error_code_t requestYPR() {
+    // use sciSendByte, to transmit a request command to the VN-100
+    char cmd [MAX_COMMAND_SIZE] = "$VNWRG,75,2,16,01,0029*XX\r\n";
+    return sciSendBytes(cmd, MAX_COMMAND_SIZE, TICK_TIMEOUT);
+}
+
+obc_error_code_t retrieveYPR(vn_ypr_packet_t * packet) {
+    if (packet == NULL) {
         return OBC_ERR_CODE_INVALID_ARG;
     }
-
+    
 }
 
