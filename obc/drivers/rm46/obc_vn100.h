05e9f190dc235e3dc1fd9f212c731c1434730a18
Joel <joelmyleee@gmail.com>
2023-08-25 11:59:18 -0400

    Added packet types and enum of commands

--- obc/drivers/rm46/obc_vn100.h
+++ obc/drivers/rm46/obc_vn100.h
@@ -3,25 +3,48 @@
 #include "obc_errors.h"
 #include <stdint.h>
 
-#define VN100_UART_REG  UART_PRINT_REG
-#define VN100_BAUDRATE  152000 
+typedef enum { 
+    VN_YPR,         // Get yaw, pitch and roll measurements 
+    VN_MAG,         // Get magnetic measurements
+    VN_ACC,         // Get acceleration measurements
+    VN_GYR,         // Get angular rate measurements
+    VN_YMR          // Get all of the above
+} vn_cmd_t;
 
-enum packetTypes { 
-    
-};
-
-struct ypr_packet {
+typedef struct {
     double yaw;
     double pitch;
     double roll;
-};
+} vn_ypr_packet_t; 
+
+typedef struct {
+    double magnet;
+} vn_mag_packet_t;
+
+typedef struct {
+    double accel;
+} vn_accel_packet_t;
+
+typedef struct {
+    double gyro;
+} vn_gyro_packet_t;
+
+typedef struct {
+    double yaw;
+    double pitch;
+    double roll;
+    double magnet;
+    double accel;
+    double gyro;
+} vn_all_packet_t;
 
 /**
- * @brief Initiates VN100 with the necessary
+ * @brief Initiates VN100 with the default parameters for baudrate, polling rate, etc
  *
- * @return OBC_ERR_CODE_SUCCESS on success, else an error code
  */
 void initVN100(void);
+
+obc_error_code_t resetModule();
 
 /**
  * @brief Set the baudrate for the VN-100 peripheral
@@ -29,7 +52,7 @@ void initVN100(void);
  *
  * @return OBC_ERR_CODE_SUCCESS on success, else an error code
  */
-obc_error_code_t VN100SetBaudRate(int baudrate);
+obc_error_code_t VN100SetBaudrate(uint32_t baudrate);
 
 /**
  * @brief Sends a request to the VN-100 via UART to return a packet containing Yaw, Pitch and Roll
@@ -44,4 +67,4 @@ obc_error_code_t requestYPR();
  * @param packet Packet to store the returned information in
  * @return OBC_ERR_CODE_SUCCESS on success, else an error code
  */
-obc_error_code_t retrieveYPR(ypr_packet* packet);
+obc_error_code_t retrieveYPR(vn_ypr_packet_t *packet);
