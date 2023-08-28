19bd5d39dbb39f62ff00a9984bbbd8bf2e76b7b5
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-08-28 00:37:33 -0400

    Added task stats

--- obc/modules/task_stats_collector/task_stats_collector.c
+++ obc/modules/task_stats_collector/task_stats_collector.c
@@ -36,13 +36,14 @@ static void vTaskStatsCollector(void *pvParameters) {
   while (1) {
     vTaskDelay(pdMS_TO_TICKS(15000));
     char taskStatsString[TASK_STATS_BUFFER_SIZE] = {0};
-    char taskStatsBuffer[400] = {0};
+    char taskStatsBuffer[200] = {0};
 
     vTaskList(taskStatsString);
     vTaskGetRunTimeStats(taskStatsBuffer);
     LOG_IF_ERROR_CODE(
         sciPrintText((unsigned char *)taskTableHeaderStr, strlen(taskTableHeaderStr), UART_MUTEX_BLOCK_TIME));
     LOG_IF_ERROR_CODE(sciPrintText((unsigned char *)taskStatsString, TASK_STATS_BUFFER_SIZE, UART_MUTEX_BLOCK_TIME));
+    LOG_IF_ERROR_CODE(sciPrintText((unsigned char *) taskStatsBuffer, 200, UART_MUTEX_BLOCK_TIME ));
   }
 }
 #endif
