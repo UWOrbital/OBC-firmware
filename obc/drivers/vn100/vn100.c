ce5a2d4baecfb3360a542ec616de393b9c0af781
Joel <joelmyleee@gmail.com>
2023-09-04 21:11:57 -0400

    Added some TODO notes and frameworks for functions

--- obc/drivers/vn100/vn100.c
+++ obc/drivers/vn100/vn100.c
@@ -2,7 +2,7 @@
 #include "obc_assert.h"
 #include "obc_board_config.h"
 #include "obc_sci_io.h"
-#include "obc_vn100.h"
+#include "vn100.h"
 
 #include <FreeRTOS.h>
 #include <FreeRTOSConfig.h>
@@ -15,87 +15,172 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define VN100_BAUDRATE      1152000
-#define MAX_COMMAND_SIZE    256U
-#define TICK_TIMEOUT        portMAX_DELAY
+#define VN100_BAUDRATE 1152000
+#define MAX_COMMAND_SIZE 256U
+#define TICK_TIMEOUT portMAX_DELAY
 
 /**
  * @brief Generalized request function to handle mulitple different packet types
  * @param cmd the input command to determine what packet to request
- * 
- *      VN_YPR      // Get yaw, pitch and roll measurements 
+ *
+ *      VN_YPR      // Get yaw, pitch and roll measurements
  *      VN_MAG      // Get magnetic measurements
  *      VN_ACC      // Get acceleration measurements
  *      VN_GYR      // Get angular rate measurements
- *      VN_YMR      // Get all of the above       
+ *      VN_YMR      // Get all of the above
  *
  * @return OBC_ERR_CODE_SUCCESS on success, else an error code
  */
 
-static obc_error_code_t requestCMD (vn_cmd_t cmd);
+static obc_error_code_t serialRequestCMD(vn_cmd_t cmd);
+
+static obc_error_code_t parsePacket(vn_cmd_t cmd, void* packet);
 
 void initVN100(void) {
-    /* TODO:
-       - Setup vn-100 mutex
-       - configure baudrate
-       - configure asyncronous outputs
-    */
+  sciSetBaudrate(UART_VN100_REG, VN100_BAUDRATE);
+  /* TODO:
+     - Setup vn-100 mutex
+     - setup the configuration register
+     - configure baudrate
+     - configure asyncronous outputs
+     - Add any offsets to gyro or acceleration or additional info about velocity compensation
+  */
 }
 
-obc_error_code_t requestCMD (vn_cmd_t cmd) {
-    unsigned char req [MAX_COMMAND_SIZE] = "";
-    switch (cmd)
-    {
+obc_error_code_t serialRequestCMD(vn_cmd_t cmd) {
+  /* TODO:
+     - Do some error checking in case the imu returns an error code
+     -
+  */
+  obc_error_code_t errCode;
+  
+  switch (cmd) {
     case VN_YPR:
-        break;
+      char req[MAX_COMMAND_SIZE] = "$VNRRG,8*XX\r\n";
+      errCode = sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
     case VN_MAG:
-        break;
+      char req[MAX_COMMAND_SIZE] = "$VNRRG,17*XX\r\n";
+      errCode = sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+      break;
     case VN_ACC:
-        break;
+      char req[MAX_COMMAND_SIZE] = "$VNRRG,18*XX\r\n";
+      errCode = sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+      break;
     case VN_GYR:
-        break;
+      char req[MAX_COMMAND_SIZE] = "$VNRRG,19*XX\r\n";
+      errCode = sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+      break;
     case VN_YMR:
-        break;
+      char req[MAX_COMMAND_SIZE] = "$VNRRG,27*XX\r\n";
+      errCode = sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+      break;
     default:
-        break;
-    }
-    sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+      errCode = OBC_ERR_CODE_INVALID_ARG;
+      break;
+  }
+  return errCode;
+
+}
+
+obc_error_code_t parsePacket(vn_cmd_t cmd, void* packet) {
+    /* TODO:
+        - Implement parsing for each of the packet types to extract the data 
+          and organize them into their respective packet types
+    */
+    
     return OBC_ERR_CODE_SUCCESS;
 }
 
 obc_error_code_t resetModule() {
-    unsigned char req [MAX_COMMAND_SIZE] = "$VNRST*4D";
-    return sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+  /* TODO:
+    - Confirm that the right response message is received from the module, otherwise throw an error
+  */
+  unsigned char req [MAX_COMMAND_SIZE] = "$VNRST*4D\r\n";
+  sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+  return OBC_ERR_CODE_SUCCESS;
 }
 
 obc_error_code_t VN100SetBaudrate(uint32_t baudrate) {
-    /*  TODO: 
-        - Set the baudrate on the VN_100 to the desired baudrate
-        - Set the local baudrate to the desired baudrate
+  /*  TODO:
+      - Set the baudrate on the VN_100 to the desired baudrate
+        - Figure out how to configure command to append the inputted baudrate
+      - Set the local baudrate to the desired baudrate
+      - Check if the inputted baudrate is acceptable
+  */
+
+  // Make the size of the string representation sufficiently large
+  unsigned char baud[12];
+  // Set to XX for now, means to ignore the checksum
+  unsigned char checksum[3] = "*XX\r\n"; 
+  unsigned char req [MAX_COMMAND_SIZE] = "$VNWRG,05,";
+  snprintf(baud, sizeof(baud), "%d", baudrate);
+  // Concatenate baudrate and checksum to the end of request command
+  strcat(req, baud);
+  strcat(req, checksum);
+
+  // Send the message via UART
+  sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+  sciSetBaudrate(UART_VN100_REG, baudrate);
+  return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t retrieveYPR(vn_ypr_packet_t* packet) {
+  /*  TODO:
+      - Call serialRequest for YPR
+      - Wait until flag has set and the response packet is ready to be retrieved
+  */
+  if (packet == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  serialRequestCMD(VN_YPR);
+  return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t retrieveMAG(vn_mag_packet_t* packet) {
+  if (packet == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+  serialRequestCMD(VN_MAG);
+  return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t retrieveACCEL(vn_accel_packet_t* packet) {
+  if (packet == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }  
+  serialRequestCMD(VN_ACC);
+  return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t retrieveGYRO(vn_gyro_packet_t* packet) {
+  if (packet == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }  
+  serialRequestCMD(VN_GYR);
+  return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t retrieveYMR(vn_ymr_packet_t* packet) {
+  if (packet == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }  
+  serialRequestCMD(VN_YMR);
+  return OBC_ERR_CODE_SUCCESS;
+}
+obc_error_code_t setASYNCOutputs(vn_cmd_t cmd) {
+    /* TODO:
+       - Let the user choose from the defined packet types to configure the asyncronous output register with a particular packet type
+       - See Example Case 1 in section 4.2.4 of the user manual
     */
-    unsigned char req [MAX_COMMAND_SIZE] = ""; 
-    sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG); 
-    
-    sciSetBaudrate(UART_VN100_REG, baudrate);
-    return OBC_ERR_CODE_SUCCESS;
-}
 
-obc_error_code_t requestYPR() {
-    // use sciSendByte, to transmit a request command to the VN-100
-    char req [MAX_COMMAND_SIZE] = "$VNWRG,75,2,16,01,0029*XX\r\n";
-    return sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+  switch (cmd) {
+    case VN_YPR:
+      char req[MAX_COMMAND_SIZE] = "$VNWRG,75,2,16,01,0009*XX\r\n";
+      sciSendBytes(&req, MAX_COMMAND_SIZE, TICK_TIMEOUT, UART_VN100_REG);
+      break;
+    default:
+      break;
+  }
+  return OBC_ERR_CODE_SUCCESS;
 }
-
-obc_error_code_t retrieveYPR(vn_ypr_packet_t * packet) {
-    /*  TODO:
-        - Maybe have this trigger when the packet is ready to come back
-        - Setup some kind of flag?
-        - Fill in parameters for sciReadBytes
-    */
-    if (packet == NULL) {
-        return OBC_ERR_CODE_INVALID_ARG;
-    }
-    
-    return OBC_ERR_CODE_SUCCESS;
-}
-
--- obc/drivers/vn100/vn100.h
+++ obc/drivers/vn100/vn100.h
@@ -3,43 +3,49 @@
 #include "obc_errors.h"
 #include <stdint.h>
 
-typedef enum { 
-    VN_YPR,         // Get yaw, pitch and roll measurements 
-    VN_MAG,         // Get magnetic measurements
-    VN_ACC,         // Get acceleration measurements
-    VN_GYR,         // Get angular rate measurements
-    VN_YMR          // Get all of the above
+typedef enum {
+  VN_YPR,  // Get yaw, pitch and roll measurements
+  VN_MAG,  // Get magnetic measurements
+  VN_ACC,  // Get acceleration measurements
+  VN_GYR,  // Get angular rate measurements
+  VN_YMR   // Get all of the above
 } vn_cmd_t;
 
 typedef struct {
-    double yaw;
-    double pitch;
-    double roll;
-} vn_ypr_packet_t; 
+  double yaw;
+  double pitch;
+  double roll;
+} vn_ypr_packet_t;
 
 typedef struct {
-    double magnet;
+  double magnet;
 } vn_mag_packet_t;
 
 typedef struct {
-    double accel;
+  double accel;
 } vn_accel_packet_t;
 
 typedef struct {
-    double gyro;
+  double gyro;
 } vn_gyro_packet_t;
 
 typedef struct {
-    double yaw;
-    double pitch;
-    double roll;
-    double magnet;
-    double accel;
-    double gyro;
-} vn_all_packet_t;
+  double yaw;
+  double pitch;
+  double roll;
+  double magnet;
+  double accel;
+  double gyro;
+} vn_ymr_packet_t;
 
 /**
  * @brief Initiates VN100 with the default parameters for baudrate, polling rate, etc
+ * Factory Defaults:
+ * User Tag: NULL
+ * Serial Baud Rate: 115200
+ * Async Data Output Frequency: 40 Hz
+ * Async Data Output Type: INS_LLA
+ * Syncronization control: 3,0,0,0,6,1,0,100000000,0
  *
  */
 void initVN100(void);
@@ -49,17 +55,11 @@ obc_error_code_t resetModule();
 /**
  * @brief Set the baudrate for the VN-100 peripheral
  * @param baudrate The desired baudrate to be set
+ * @note The default baudrate is 115200 Hz
  *
  * @return OBC_ERR_CODE_SUCCESS on success, else an error code
  */
 obc_error_code_t VN100SetBaudrate(uint32_t baudrate);
-
-/**
- * @brief Sends a request to the VN-100 via UART to return a packet containing Yaw, Pitch and Roll
- *
- * @return OBC_ERR_CODE_SUCCESS on success, else an error code
- */
-obc_error_code_t requestYPR();
 
 /**
  * @brief Retrieve the Yaw, Pitch and Roll from the VN-100
@@ -68,3 +68,44 @@ obc_error_code_t requestYPR();
  * @return OBC_ERR_CODE_SUCCESS on success, else an error code
  */
 obc_error_code_t retrieveYPR(vn_ypr_packet_t *packet);
+
+/**
+ * @brief Retrieve the Yaw, Pitch and Roll from the VN-100
+ *
+ * @param packet Packet to store the returned information in
+ * @return OBC_ERR_CODE_SUCCESS on success, else an error code
+ */
+obc_error_code_t retrieveMAG(vn_mag_packet_t *packet);
+
+/**
+ * @brief Retrieve the Yaw, Pitch and Roll from the VN-100
+ *
+ * @param packet Packet to store the returned information in
+ * @return OBC_ERR_CODE_SUCCESS on success, else an error code
+ */
+obc_error_code_t retrieveACCEL(vn_accel_packet_t *packet);
+
+/**
+ * @brief Retrieve the Yaw, Pitch and Roll from the VN-100
+ *
+ * @param packet Packet to store the returned information in
+ * @return OBC_ERR_CODE_SUCCESS on success, else an error code
+ */
+obc_error_code_t retrieveGYRO(vn_gyro_packet_t *packet);
+
+/**
+ * @brief Retrieve the Yaw, Pitch and Roll, Magnetometer, Acceleration, from the VN-100
+ *
+ * @param packet Packet to store the returned information in
+ * @return OBC_ERR_CODE_SUCCESS on success, else an error code
+ */
+obc_error_code_t retrieveYMR(vn_ymr_packet_t *packet);
+
+/**
+ * @brief Configure which packet type will be asyncronously outputted, note that this will also change the packet header
+ * type
+ *
+ * @param cmd Command to denote which packet type to use
+ * @return OBC_ERR_CODE_SUCCESS on success, else an error code
+ */
+obc_error_code_t setASYNCOutputs(vn_cmd_t cmd);
