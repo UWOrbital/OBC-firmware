56134fec7ddbf2c96bee8e4488cb7fc0f461d639
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-09-06 09:06:56 -0400

    Added files back

---
+++ obc/hal/obc_rev1/include/rti_weak.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <FreeRTOSConfig.h>
+
+#if (configGENERATE_RUN_TIME_STATS == 1)
+
+#include <stdint.h>
+
+/**
+ * @brief Returns the free counter tick for RTI 1
+ *
+ * @return A 32-bit tick value for the Free Counter RTI 1
+ */
+uint32_t rtiGetCounterTick();
+
+#endif
