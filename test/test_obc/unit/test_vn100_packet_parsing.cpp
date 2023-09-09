35bc6c931b2e736dc25a5b630b42233b91ea1b50
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-09-09 12:15:13 -0400

    Resolved bugs and added test

--- test/test_obc/unit/test_vn100_packet_parsing.cpp
+++ test/test_obc/unit/test_vn100_packet_parsing.cpp
@@ -1,6 +1,17 @@
 #include "vn100_parsing.h"
+#include "vn100.h"
+
 #include "obc_errors.h"
 
 #include <gtest/gtest.h>
 
-TEST(VN100_PARSING_SUITE, EXPECT_SERIAL_ERROR_CODE) {}
+TEST(VN100_PARSING_SUITE, EXPECT_SERIAL_ERROR_CODE) {
+  const char[] errorStringValid = "$VNERR,C";
+  const char[] errorStringInvalid = "$VNERR,E"
+
+      VN100_error_t error = 0;
+  ASSERT_EQ(parsePacket(VN_YMR, errorStringValid, &error), OBC_ERR_CODE_VN100_RESPONSE_ERROR);
+  ASSERT_EQ(error, INSUFFICIENT_BAUD_RATE);
+
+  ASSERT_EQ(parsePacket(VN_YMR, errorStringInvalid, &error), OBC_ERR_CODE_VN100_PARSE_ERROR);
+}
