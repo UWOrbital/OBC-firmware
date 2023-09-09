a6cf9b892b24319a69439a0f493bd1ad8b478c4b
Yarik Popov <yarikpopov03@gmail.com>
2023-09-08 21:25:23 -0400

    Corrected equal position false tests

--- test/test_obc/unit/test_sun_utils.cpp
+++ test/test_obc/unit/test_sun_utils.cpp
@@ -14,17 +14,17 @@ TEST(TestSunUtils, equalsPositionDataTrue) {
 TEST(TestSunUtils, equalsPositionDataFalseX) {
     position_data_t data1 = {0, 1, 2, 3};
     position_data_t data2 = {0, 2, 2, 3};
-    ASSERT_EQ(equalsDataPoint(data1, data2), 1);
+    ASSERT_EQ(equalsDataPoint(data1, data2), 0);
 }
 
 TEST(TestSunUtils, equalsPositionDataFalseY) {
     position_data_t data1 = {0, 1, 3, 3};  
     position_data_t data2 = {0, 1, 2, 3};
-    ASSERT_EQ(equalsDataPoint(data1, data2), 1);
+    ASSERT_EQ(equalsDataPoint(data1, data2), 0);
 }
 
 TEST(TestSunUtils, equalsPositionDataFalseZ) {
     position_data_t data1 = {0, 1, 2, 4};
     position_data_t data2 = {0, 1, 2, 3};
-    ASSERT_EQ(equalsDataPoint(data1, data2), 1);
+    ASSERT_EQ(equalsDataPoint(data1, data2), 0);
 }
