325a428e34532c9c284f3a76a1b552536bfe373a
Navtaj Hundal <n2hundal@uwaterloo.ca>
2023-09-10 00:32:15 -0400

    changed spi data formats

--- obc/hal/launchpad/source/spi.c
+++ obc/hal/launchpad/source/spi.c
@@ -306,7 +306,7 @@ void spiInit(void)
                   | (uint32)((uint32)0U << 21U)  /* wait on enable */
                   | (uint32)((uint32)0U << 20U)  /* shift direction */
                   | (uint32)((uint32)0U << 17U)  /* clock polarity */
-                  | (uint32)((uint32)1U << 16U)  /* clock phase */
+                  | (uint32)((uint32)0U << 16U)  /* clock phase */
                   | (uint32)((uint32)243U << 8U) /* baudrate prescale */
                   | (uint32)((uint32)8U << 0U);  /* data word length */
 
