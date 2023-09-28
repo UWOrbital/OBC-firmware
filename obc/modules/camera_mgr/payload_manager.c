5b9cf311448b83e255a7be4e5a8ecb0f52c8a00e
asmi-g <asmi.gujral@gmail.com>
2023-09-27 20:05:12 -0400

    SecondaryPayloadTask

--- obc/modules/camera_mgr/payload_manager.c
+++ obc/modules/camera_mgr/payload_manager.c
@@ -18,6 +18,18 @@ static QueueHandle_t payloadQueueHandle = NULL;
 static StaticQueue_t payloadQueue;
 static uint8_t payloadQueueStack[PAYLOAD_MANAGER_QUEUE_LENGTH * PAYLOAD_MANAGER_QUEUE_ITEM_SIZE];
 
+static TaskHandle_t secondaryPayloadTaskHandle = NULL;
+static StaticTask_t secondaryPayloadTaskBuffer;
+static StackType_t secondaryPayloadTaskStack[PAYLOAD_MANAGER_STACK_SIZE];
+
+static QueueHandle_t secondaryPayloadQueueHandle = NULL;
+static StaticQueue_t secondaryPayloadQueue;
+static uint8_t secondaryPayloadQueueStack[PAYLOAD_MANAGER_QUEUE_LENGTH * PAYLOAD_MANAGER_QUEUE_ITEM_SIZE];
+
+static SemaphoreHandle_t xPayloadMutex; 
+
+
+
 /**
  * @brief	Payload Manager task.
  * @param	pvParameters	Task parameters.
@@ -36,6 +48,21 @@ void initPayloadManager(void) {
     payloadQueueHandle = xQueueCreateStatic(PAYLOAD_MANAGER_QUEUE_LENGTH, PAYLOAD_MANAGER_QUEUE_ITEM_SIZE,
                                             payloadQueueStack, &payloadQueue);
   }
+
+  //Created a separate queue for the secondary payload
+  ASSERT((secondaryPayloadTaskStack != NULL) && (&secondaryPayloadTaskBuffer != NULL));
+  xPayloadMutex = xSemaphoreCreateMutex();
+  if (xPayloadMutex != NULL) {
+    secondaryPayloadTaskHandle = xTaskCreateStatic(secondaryPayloadManagerTask, PAYLOAD_MANAGER_NAME, PAYLOAD_MANAGER_STACK_SIZE, NULL,
+                                          PAYLOAD_MANAGER_PRIORITY, secondaryPayloadTaskStack, &secondaryPayloadTaskBuffer);
+  }
+
+  ASSERT((secondaryPayloadQueueStack != NULL) && (&secondaryPayloadQueue != NULL));
+  if (secondaryPayloadQueueHandle == NULL) {
+    secondaryPayloadQueueHandle = xQueueCreateStatic(PAYLOAD_MANAGER_QUEUE_LENGTH, PAYLOAD_MANAGER_QUEUE_ITEM_SIZE,
+                                            secondaryPayloadQueueStack, &secondaryPayloadQueue);
+  }
+  
 }
 
 obc_error_code_t sendToPayloadQueue(payload_event_t *event) {
@@ -61,5 +88,20 @@ static void vPayloadManagerTask(void *pvParameters) {
       case PAYLOAD_MANAGER_NULL_EVENT_ID:
         break;
     }
+  }
+}
+
+static void secondaryPayloadManagerTask(void *pvParameters){
+  ASSERT(secondaryPayloadQueueHandle != NULL);
+  
+  while(1){
+    if (xSemaphoreTake(xPayloadMutex, portMAX_DELAY) == pdTRUE){
+      // ADD SECONDARY PAYLOAD COMMAND HANDLER
+
+      //releases Payload mutex when secondary payload functionality has been executed
+      xSemaphoreGive(xPayloadMutex);
+    }
+    
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
