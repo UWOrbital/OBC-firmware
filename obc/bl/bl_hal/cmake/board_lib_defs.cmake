14f4a1ad481fb2432dd48e175587ff81cee15b2c
Daniel Gobalakrishnan <danielgobalakrishnan@gmail.com>
2023-10-04 00:37:15 -0700

    Created single app linker script for all boards; added other board hals to bl_hal

--- obc/bl/bl_hal/cmake/board_lib_defs.cmake
+++ obc/bl/bl_hal/cmake/board_lib_defs.cmake
@@ -1,10 +1,14 @@
 cmake_minimum_required(VERSION 3.15)
 
 if (${BOARD_TYPE} MATCHES RM46_LAUNCHPAD)
-    set(BL_HAL_LIB rm46-launchpad-bl-hal)
+    set(HAL_LIB rm46-launchpad-hal)
+elseif(${BOARD_TYPE} MATCHES OBC_REVISION_1)
+    set(HAL_LIB obc-rev1-hal)
+elseif(${BOARD_TYPE} MATCHES OBC_REVISION_2)
+    set(HAL_LIB obc-rev2-hal)
 else()
     message(FATAL_ERROR "Invalid board type: ${BOARD_TYPE}")
 endif()
 
-set(BL_HAL_LIB_ARCHIVE ${HAL_LIB}-archive)
-set(BL_HAL_LIB_NO_OPTIMIZE ${HAL_LIB}-no-optimize)
+set(HAL_LIB_OPTIMIZE ${HAL_LIB}-optimize)
+set(HAL_LIB_NO_OPTIMIZE ${HAL_LIB}-no-optimize)
