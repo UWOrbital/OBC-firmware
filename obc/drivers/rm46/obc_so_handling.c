58a30b03daafe3bcfb14e0ba84353b61b18e8b92
Guhan <guhan.shyam.iyer05@gmail.com>
2023-10-04 23:42:24 -0400

    Removed system reset

--- obc/drivers/rm46/obc_so_handling.c
+++ obc/drivers/rm46/obc_so_handling.c
@@ -1,9 +1,9 @@
-#include <FreeRTOS.h>
 #include "os_task.h"
 #include "obc_reset.h"
 #include "obc_logging.h"
 
+#include <FreeRTOS.h>
+
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
-  LOG_ERROR("Stack overflow detected: reseting RM46");
-  resetSystem(RESET_REASON_STACK_OVERFLOW_DETECTED);
+  LOG_ERROR("Stack overflow detected: reseting RM46.");
 }
--- obc/drivers/rm46/obc_so_handling.h
+++ obc/drivers/rm46/obc_so_handling.h
@@ -1,14 +1,15 @@
 #pragma once
 
-#include <FreeRTOS.h>
 #include "os_task.h"
 #include "obc_reset.h"
+
+#include <FreeRTOS.h>
 
 /**
  * @brief Stack overflow hook.
  * @param xTask Task reference handle.
  * @param pcTaskName Task name.
- *
+ * @returns Void.
  */
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName);
