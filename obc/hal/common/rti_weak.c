41b99910070b954882074b117fbbdfc1e0ac6a47
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-10-08 23:55:53 -0300

    edited rti base

--- obc/hal/common/rti_weak.c
+++ obc/hal/common/rti_weak.c
@@ -2,24 +2,18 @@
 #if (configGENERATE_RUN_TIME_STATS == 1)
 
 #include <rti.h>
+#include <reg_rti.h>
 
 
-#define RTI_BASE_MEMORY_ADDRESS 0xFFFFFC00u
-#define RTI_GLCTRL_ENABLE_COUNTER1_MASK 0x00000002u
+static volatile rtiBASE_t* const rtiBase = rtiREG1;
 
+#define RTI_GLCTRL_ENABLE_COUNTER1_MASK 2UL
 #define RTI_RTOS_TICK_COMPARE_CNT (configCPU_CLOCK_HZ / configTICK_RATE_HZ)
 #define RTI_RUNTIME_STATS_COMPARE_CNT (RTI_RTOS_TICK_COMPARE_CNT / 50)
 
-#define RTIFRC1 *((volatile uint32_t*)(RTI_BASE_MEMORY_ADDRESS + 0x00000030u))
-#define RTIUC1 *((volatile uint32_t*)(RTI_BASE_MEMORY_ADDRESS + 0x00000034u))
-#define RTICPUC1 *((volatile uint32_t*)(RTI_BASE_MEMORY_ADDRESS + 0x00000038u))
-
-#define RTI_GCTRL_REG *((volatile uint32_t*)0xFFFFFC00)    // Used to disable/enable the counter and timer
-#define RTI_SETINTENA_REG *((volatile uint32_t*)0xFFFFFC80)
-#define RTI_CLEARINTENA_REG *((volatile uint32_t*)0xFFFFFC84)
-#define RTI_INTFLAG_REG *((volatile uint32_t*)0xFFFFFC88)
-
-uint32_t rtiGetCounterTick() { return RTIFRC1; }
+uint32_t rtiGetCounterTick() { 
+  return rtiBase->CNT[1].FRCx; 
+}
 
 /**
  * @brief Resets all of RTI Counter 1. Disables the counter until enabled again 
@@ -31,18 +25,18 @@ __attribute__((weak)) uint32_t rtiResetCounter(uint32_t counter) {
   if (counter != rtiCOUNTER_BLOCK1) return 1;
 
   // Disable the timer Counter 1
-  RTI_GCTRL_REG &= ~(RTI_GLCTRL_ENABLE_COUNTER1_MASK);
+  rtiBase->GCTRL &= ~(RTI_GLCTRL_ENABLE_COUNTER1_MASK);
 
   // Preset Free Counting Register and Up Counter.
-  RTIFRC1 = 0x00000000;
-  RTIUC1 = 0x00000000;
-  RTICPUC1 = 0x00000000;
+  rtiBase->CNT[1].FRCx = 0x00000000;
+  rtiBase->CNT[1].UCx = 0x00000000;
+  rtiBase->CNT[1].CPUCx = 0x00000000;
 
   // Clear current Counter 1 overflow interrupts
-  RTI_INTFLAG_REG = (1UL << 18);
+  rtiBase->INTFLAG= (1UL << 18);
 
   // Disable future Counter 1 overflow interrupts
-  RTI_CLEARINTENA_REG = (1UL << 18);
+  rtiBase->CLEARINTENA = (1UL << 18);
   return 0;
 }
 
@@ -55,7 +49,7 @@ __attribute__((weak)) void rtiInit(void) {
   rtiResetCounter(rtiCOUNTER_BLOCK1);
 
   // Set the prescaler for Counter Block 1
-  RTICPUC1 = RTI_RUNTIME_STATS_COMPARE_CNT;
+  rtiBase->CNT[1].CPUCx = RTI_RUNTIME_STATS_COMPARE_CNT;
 }
 
 /**
@@ -67,7 +61,7 @@ __attribute__((weak)) void rtiStartCounter(uint32_t counter) {
   if (counter != rtiCOUNTER_BLOCK1) return;
 
   // Start the timer
-  RTI_GCTRL_REG |= RTI_GLCTRL_ENABLE_COUNTER1_MASK;
+  rtiBase->GCTRL |= RTI_GLCTRL_ENABLE_COUNTER1_MASK;
 }
 
 /**
@@ -79,7 +73,7 @@ __attribute__((weak)) void rtiStopCounter(uint32_t counter) {
   if (counter != rtiCOUNTER_BLOCK1) return;
 
   // Disable the timer
-  RTI_GCTRL_REG &= ~(RTI_GLCTRL_ENABLE_COUNTER1_MASK);
+  rtiBase->GCTRL &= ~(RTI_GLCTRL_ENABLE_COUNTER1_MASK);
 }
 
 #endif
