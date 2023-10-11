bf14d0b5b165a0299334580923c386815f1d2637
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2023-10-11 17:56:19 -0400

    Generalized get and set persistent sections

--- obc/sys/persistent/obc_persistent.c
+++ obc/sys/persistent/obc_persistent.c
@@ -11,30 +11,13 @@
 
 STATIC_ASSERT(sizeof(obc_persist_t) <= FRAM_MAX_ADDRESS, "obc_persist_t exceeds available FRAM space");
 
-/**
- * @brief Get a persistent section from FRAM and verify its header data
- *
- * @param sectionStartAddr Start address of the section in FRAM
- * @param sectionSize Size of the section in FRAM
- * @param buff Buffer to store the section
- * @param buffLen Length of the buffer (Must be at least sectionSize bytes long)
- * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code
- */
-static obc_error_code_t getPersistentSection(uint32_t sectionStartAddr, size_t sectionSize, uint8_t *buff,
-                                             size_t buffLen);
+/* Private function declarations */
 
 /**
- * @brief Set a persistent section in FRAM and write its header data
- *
- * @param sectionStartAddr Start address of the section in FRAM
- * @param sectionSize Size of the section in FRAM
- * @param buff Buffer containing the section data (Layout of buffer must match the section struct; leave space for
- * header data)
- * @param buffLen Length of the buffer (Must be at least sectionSize bytes long)
- * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code
+ * @brief Gets the config based on the section id
+ * @return obc_persist_config_t* Pointer to the config or NULL if sectionId is invalid
  */
-static obc_error_code_t setPersistentSection(uint32_t sectionStartAddr, size_t sectionSize, uint8_t *buff,
-                                             size_t buffLen);
+static obc_persist_config_t *getOBCPersistConfig(obc_persist_section_id_t sectionId);
 
 /**
  * @brief Overlay data in a section buffer. Data is placed after the header part of the section buffer.
@@ -60,43 +43,106 @@ static obc_error_code_t overlayDataInSectionBuff(uint8_t *data, size_t dataLen, 
 static obc_error_code_t extractDataFromSectionBuff(uint8_t *data, size_t dataLen, uint8_t *sectionBuff,
                                                    size_t sectionLen);
 
-obc_error_code_t getPersistentObcTime(obc_time_persist_data_t *data) {
+/* Public function definitions */
+
+obc_error_code_t getPersistentSection(obc_persist_section_id_t sectionId, uint8_t *buff, size_t buffLen) {
+  return getPersistentSectionBySubIndex(sectionId, 0, buff, buffLen);
+}
+
+obc_error_code_t setPersistentSection(obc_persist_section_id_t sectionId, const uint8_t *buff, size_t buffLen) {
+  return setPersistentSectionBySubIndex(sectionId, 0, buff, buffLen);
+}
+
+obc_error_code_t getPersistentSectionBySubIndex(obc_persist_section_id_t sectionId, size_t subIndex, uint8_t *buff,
+                                                size_t buffLen) {
+  obc_error_code_t errCode;
+  if (buff == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  // Validate against config
+  obc_persist_config_t *config = getOBCPersistConfig(sectionId);
+  if (config == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+  if (subIndex >= config->sectionCount) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  // Buffer will only hold the data not the header
+  if (buffLen < config->sectionSize - sizeof(obc_persist_section_header_t)) {
+    return OBC_ERR_CODE_BUFF_TOO_SMALL;
+  }
+  uint32_t sectionStartAddrBySubIndex = config->sectionStartAddr + subIndex * config->sectionSize;
+
+  // Reads header, assumes sectionSize holds the header as well
+  uint8_t headerBuffer[sizeof(obc_persist_section_header_t)] = {0};
+  RETURN_IF_ERROR_CODE(framRead(sectionStartAddrBySubIndex, headerBuffer, sizeof(obc_persist_section_header_t)));
+  obc_persist_section_header_t header = {0};
+  memcpy(&header, headerBuffer, sizeof(obc_persist_section_header_t));
+
+  if (header.len != config->sectionSize) {
+    return OBC_ERR_CODE_PERSISTENT_CORRUPTED;
+  }
+
+  // Compute CRC32 of data and check it
+  uint32_t crc32 = computeCrc32(0, buff, buffLen);
+  if (header.crc32 != crc32) {
+    return OBC_ERR_CODE_PERSISTENT_CORRUPTED;
+  }
+
+  RETURN_IF_ERROR_CODE(framRead(sectionStartAddrBySubIndex + sizeof(obc_persist_section_header_t), buff, buffLen));
+  return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t getPersistentSectionBySubIndex(obc_persist_section_id_t sectionId, size_t subIndex,
+                                                const uint8_t *buff, size_t buffLen) {
   obc_error_code_t errCode;
 
-  uint8_t sectionBuffer[sizeof(obc_time_persist_t)] = {0};
+  if (buff == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
 
-  // Read entire section (including header)
-  RETURN_IF_ERROR_CODE(
-      getPersistentSection(OBC_PERSIST_ADDR_OF(obcTime), sizeof(sectionBuffer), sectionBuffer, sizeof(sectionBuffer)));
+  // Validate against config
+  obc_persist_config_t *config = getOBCPersistConfig(sectionId);
+  if (config == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+  if (subIndex >= config->sectionCount) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
 
-  // Strip header from section buffer
-  RETURN_IF_ERROR_CODE(
-      extractDataFromSectionBuff((uint8_t *)data, sizeof(*data), sectionBuffer, sizeof(sectionBuffer)));
+  // Buffer will only hold the data not the header
+  if (buffLen < config->sectionSize - sizeof(obc_persist_section_header_t)) {
+    return OBC_ERR_CODE_BUFF_TOO_SMALL;
+  }
+
+  // Create header
+  obc_persist_section_header_t header = {0};
+  header.len = config->sectionSize;
+  header.crc32 = computeCrc32(0, buff, buffLen);
+
+  // Write header and data
+  uint32_t sectionStartAddrBySubIndex = config->sectionStartAddr + subIndex * config->sectionSize;
+  RETURN_IF_ERROR_CODE(framWrite(sectionStartAddrBySubIndex, &header, sizeof(obc_persist_section_header_t)));
+  RETURN_IF_ERROR_CODE(framWrite(sectionStartAddrBySubIndex + sizeof(obc_persist_section_header_t), buff, buffLen));
 
   return OBC_ERR_CODE_SUCCESS;
 }
 
-obc_error_code_t setPersistentObcTime(obc_time_persist_data_t *data) {
-  obc_error_code_t errCode;
+// TODO: Remove these functions
 
-  uint8_t sectionBuffer[sizeof(obc_time_persist_t)] = {0};
-
-  // Place in buffer with space for header
-  RETURN_IF_ERROR_CODE(overlayDataInSectionBuff((uint8_t *)data, sizeof(*data), sectionBuffer, sizeof(sectionBuffer)));
-
-  // Write entire section (including header)
-  RETURN_IF_ERROR_CODE(
-      setPersistentSection(OBC_PERSIST_ADDR_OF(obcTime), sizeof(sectionBuffer), sectionBuffer, sizeof(sectionBuffer)));
-
-  return OBC_ERR_CODE_SUCCESS;
-}
-
-/*---------------------------------------------------------------------*/
-/*------------------------- Private functions -------------------------*/
-/*---------------------------------------------------------------------*/
-
-static obc_error_code_t getPersistentSection(uint32_t sectionStartAddr, size_t sectionSize, uint8_t *buff,
-                                             size_t buffLen) {
+/**
+ * @brief Get a persistent section from FRAM and verify its header data
+ *
+ * @param sectionStartAddr Start address of the section in FRAM
+ * @param sectionSize Size of the section in FRAM
+ * @param buff Buffer to store the section
+ * @param buffLen Length of the buffer (Must be at least sectionSize bytes long)
+ * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code
+ */
+static obc_error_code_t getPersistentSectionOLD(uint32_t sectionStartAddr, size_t sectionSize, uint8_t *buff,
+                                                size_t buffLen) {
   obc_error_code_t errCode;
 
   if (buff == NULL) {
@@ -129,8 +175,18 @@ static obc_error_code_t getPersistentSection(uint32_t sectionStartAddr, size_t s
   return OBC_ERR_CODE_SUCCESS;
 }
 
-static obc_error_code_t setPersistentSection(uint32_t sectionStartAddr, size_t sectionSize, uint8_t *buff,
-                                             size_t buffLen) {
+/**
+ * @brief Set a persistent section in FRAM and write its header data
+ *
+ * @param sectionStartAddr Start address of the section in FRAM
+ * @param sectionSize Size of the section in FRAM
+ * @param buff Buffer containing the section data (Layout of buffer must match the section struct; leave space for
+ * header data)
+ * @param buffLen Length of the buffer (Must be at least sectionSize bytes long)
+ * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code
+ */
+static obc_error_code_t setPersistentSectionOLD(uint32_t sectionStartAddr, size_t sectionSize, uint8_t *buff,
+                                                size_t buffLen) {
   obc_error_code_t errCode;
 
   if (buff == NULL) {
@@ -156,6 +212,16 @@ static obc_error_code_t setPersistentSection(uint32_t sectionStartAddr, size_t s
   RETURN_IF_ERROR_CODE(framWrite(sectionStartAddr, buff, sectionSize));
 
   return OBC_ERR_CODE_SUCCESS;
+}
+
+/*---------------------------------------------------------------------*/
+/*------------------------- Private functions -------------------------*/
+/*---------------------------------------------------------------------*/
+
+static obc_persist_config_t *getOBCPersistConfig(obc_persist_section_id_t sectionId) {
+  if (sectionId >= OBC_PERSIST_SECTION_ID_COUNT) return NULL;
+
+  return &obcPersistConfig[sectionId];
 }
 
 static obc_error_code_t overlayDataInSectionBuff(uint8_t *data, size_t dataLen, uint8_t *sectionBuff,
--- obc/sys/persistent/obc_persistent.h
+++ obc/sys/persistent/obc_persistent.h
@@ -6,6 +6,7 @@
 
 #include "obc_errors.h"
 
+// TODO: Change instructions
 /*---------------------------------------------------------------------------*/
 /* GUIDE FOR ADDING A NEW PERSISTENT SECTION:
  * 1. Add a struct for the data to be stored in the section
@@ -53,6 +54,29 @@ typedef struct {
 } obc_time_persist_t;
 
 /*---------------------------------------------------------------------------*/
+/* CONFIG */
+
+typedef enum {
+  OBC_PERSIST_SECTION_ID_OBC_TIME = 0,
+  OBC_PERSIST_SECTION_ID_ALARM_MGR,
+
+  OBC_PERSIST_SECTION_ID_COUNT  // Must always be last
+} obc_persist_section_id_t;
+
+//? Might want to change the config related to include/excluding headers in the section size/addr
+typedef struct {
+  uint32_t sectionStartAddr;  // Includes the header
+  size_t sectionSize;         // Includes the header
+  size_t sectionCount;        // Put as 1 if only 1 section of this type is needed
+} obc_persist_config_t;
+
+/* Config */
+const obc_persist_config_t obcPersistConfig[] = {
+    [OBC_PERSIST_SECTION_ID_OBC_TIME] = {OBC_PERSIST_ADDR_OF(obcTime), sizeof(obc_time_persist_t), 1},
+    //  [OBC_PERSIST_SECTION_ID_ALARM_MGR] = {OBC_PERSIST_ADDR_OF(alarmMgr), sizeof(obc_alarm_mgr_persist_t), 1}
+};
+
+/*---------------------------------------------------------------------------*/
 
 /**
  * @brief Layout of the persistent storage
@@ -67,5 +91,55 @@ typedef struct {
 
 /* GETTERS AND SETTERS */
 
-obc_error_code_t getPersistentObcTime(obc_time_persist_data_t *buffer);
-obc_error_code_t setPersistentObcTime(obc_time_persist_data_t *data);
+/**
+ * @brief Get a persistent section from FRAM by the sectionId and verify its header data
+ *
+ * @param sectionId The sectionId of the section to get
+ * @param buff Buffer to store the section (header not included)
+ * @param buffLen Length of the buffer (Must be at least sectionSize - sizeof(obc_persist_section_header_t) bytes long)
+ *
+ * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code
+ */
+obc_error_code_t getPersistentSection(obc_persist_section_id_t sectionId, uint8_t *buff, size_t buffLen);
+
+/**
+ * @brief Set a persistent section in FRAM by the sectionId and write its header data
+ *
+ * @param sectionId The sectionId of the section to set
+ * @param buff Buffer containing the section data (header not included)
+ * @param buffLen Length of the buffer (Must be at least sectionSize - sizeof(obc_persist_section_header_t) bytes long)
+ *
+ * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code
+ */
+obc_error_code_t setPersistentSection(obc_persist_section_id_t sectionId, const uint8_t *buff, size_t buffLen);
+
+/**
+ * @brief Get a persistent section from FRAM by the sectionId and subIndex and verify its header data. This function is
+ * used when the section is an array of identical sections.
+ *
+ * @param sectionId The sectionId of the section to get
+ * @param subIndex The subIndex of the section to get
+ * @param buff Buffer to store the section data (header not included)
+ * @param buffLen Length of the buffer (Must be at least sectionSize - sizeof(obc_persist_section_header_t) bytes long)
+ *
+ * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code
+ */
+obc_error_code_t getPersistentSectionBySubIndex(obc_persist_section_id_t sectionId, size_t subIndex, uint8_t *buff,
+                                                size_t buffLen);
+
+/**
+ * @brief Set a persistent section in FRAM by the sectionId and subIndex and write its header data. This function is
+ * used when the section is an array of identical sections.
+ *
+ * @param sectionId The sectionId of the section to set
+ * @param subIndex The subIndex of the section to get
+ * @param buff Buffer containing the section data (header not included)
+ * @param buffLen Length of the buffer (Must be at least sectionSize - sizeof(obc_persist_section_header_t) bytes long)
+ *
+ * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code
+ */
+obc_error_code_t setPersistentSectionBySubIndex(obc_persist_section_id_t sectionId, size_t subIndex,
+                                                const uint8_t *buff, size_t buffLen);
+
+// obc_error_code_t getPersistentObcTime(obc_time_persist_data_t *buffer);
+// obc_error_code_t setPersistentObcTime(obc_time_persist_data_t *data);
