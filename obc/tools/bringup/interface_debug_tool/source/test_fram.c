dc9d22616ced207041cad8a468b87da0a80ef60f
Ekaums <ekaum.singh@gmail.com>
2023-10-16 18:16:16 -0400

    Completed FRAM test file

--- obc/tools/bringup/interface_debug_tool/source/test_fram.c
+++ obc/tools/bringup/interface_debug_tool/source/test_fram.c
@@ -10,17 +10,19 @@ void testFRAM(void){
     uint8_t write_data = 12345;
     uint8_t new_read_data;
     uint8_t id[9] = {0};
+    uint8_t test_id[9] = {0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0xC2, 0x25, 0x08};
 
-    errCode = framReadID(&id, sizeof(id));
+
+    errCode = framReadID(&id, sizeof(uint8_t) * 9);
 
     if (errCode != OBC_ERR_CODE_SUCCESS){
         sciPrintf("ERROR - FRAM Read ID unsuccessful. Error code: %d\r\n", errCode);
     }    
-    else if (id[0] != 0x7F || id[1] != 0x7F || id[2] != 0x7F || id[3] != 0x7F || id[4] != 0x7F || id[5] != 0x7F || id[6] != 0xC2 || id[7] != 0x25 || id[8] != 0x08){
-        sciPrintf("ERROR - Invalid ID read\r\n");
+    else if (memcmp(id, test_id, 9) == 0){
+        sciPrintf("SUCCESS: Queried ID matches expected ID");
     }
     else{
-        sciPrintf("SUCCESS: Queried ID matches expected ID");
+        sciPrintf("ERROR - Invalid ID read\r\n");
     }
     
     
@@ -36,7 +38,7 @@ void testFRAM(void){
 
     errCode = framRead(0x12345, new_read_data, sizeof(new_read_data));
     if (errCode != OBC_ERR_CODE_SUCCESS){
-        sciPrintf("ERROR - FRAM read operation unsuccessful. Error code: %d\r\n", errCode);
+        sciPrintf("ERROR - FRAM second read operation unsuccessful. Error code: %d\r\n", errCode);
     }
 
     if (new_read_data == write_data){
