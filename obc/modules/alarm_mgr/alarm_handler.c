94700b033ac0f1e31bb29fe2a31d370c058e3acd
danglevm <danglevuminh@gmail.com>
2023-10-16 11:00:50 -0400

    comment out static alert

--- obc/modules/alarm_mgr/alarm_handler.c
+++ obc/modules/alarm_mgr/alarm_handler.c
@@ -39,8 +39,8 @@ static obc_error_code_t peekEarliestAlarm(alarm_handler_alarm_info_t *alarm);
 
 static void datetimeToAlarmTime(rtc_date_time_t *datetime, rtc_alarm_time_t *alarmTime);
 
-STATIC_ASSERT((ALARM_QUEUE_SIZE <= OBC_PERSISTENT_MAX_ALARM_COUNT),
-              "queue size exceeds max alarms that can be stored in FRAM");
+// STATIC_ASSERT((ALARM_QUEUE_SIZE <= OBC_PERSISTENT_MAX_ALARM_COUNT),
+//               "queue size exceeds max alarms that can be stored in FRAM");
 
 void initAlarmHandler(void) {
   ASSERT((alarmHandlerTaskStack != NULL) && (&alarmHandlerTaskBuffer != NULL));
