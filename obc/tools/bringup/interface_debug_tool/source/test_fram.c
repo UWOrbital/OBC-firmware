1c930d4ede0b1b651a24295ea4eb2918a87dce7e
Ekaums <ekaum.singh@gmail.com>
2023-10-17 18:09:23 -0400

    Made changes

--- obc/tools/bringup/interface_debug_tool/source/test_fram.c
+++ obc/tools/bringup/interface_debug_tool/source/test_fram.c
@@ -6,19 +6,15 @@
 void testFRAM(void){
 
     obc_error_code_t errCode;
-    uint8_t read_data;
-    uint8_t write_data = 12345;
-    uint8_t new_read_data;
     uint8_t id[9] = {0};
-    uint8_t test_id[9] = {0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0xC2, 0x25, 0x08};
+    uint8_t testId[9] = {0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0xC2, 0x25, 0x08};
 
-
-    errCode = framReadID(&id, sizeof(uint8_t) * 9);
+    errCode = framReadID(&id, sizeof(id));
 
     if (errCode != OBC_ERR_CODE_SUCCESS){
         sciPrintf("ERROR - FRAM Read ID unsuccessful. Error code: %d\r\n", errCode);
     }    
-    else if (memcmp(id, test_id, 9) == 0){
+    else if (memcmp(id, testId, 9) == 0){
         sciPrintf("SUCCESS: Queried ID matches expected ID");
     }
     else{
@@ -26,25 +22,33 @@ void testFRAM(void){
     }
     
     
-    errCode = framRead(0x12345, read_data, sizeof(read_data));
+    uint8_t readData;
+
+    errCode = framRead(0x12345, readData, sizeof(readData));
     if (errCode != OBC_ERR_CODE_SUCCESS){
         sciPrintf("ERROR - FRAM read operation unsuccessful. Error code: %d\r\n", errCode);
     }
 
-    errCode = framWrite(0x12345, write_data, sizeof(write_data));
+
+    uint8_t writeData = 123;
+
+    errCode = framWrite(0x12345, writeData, sizeof(writeData));
     if (errCode != OBC_ERR_CODE_SUCCESS){
         sciPrintf("ERROR - FRAM write operation unsuccessful. Error code: %d\r\n", errCode);
     }
 
-    errCode = framRead(0x12345, new_read_data, sizeof(new_read_data));
+
+    uint8_t newReadData;
+
+    errCode = framRead(0x12345, newReadData, sizeof(newReadData));
     if (errCode != OBC_ERR_CODE_SUCCESS){
         sciPrintf("ERROR - FRAM second read operation unsuccessful. Error code: %d\r\n", errCode);
     }
 
-    if (new_read_data == write_data){
+    if (newReadData == writeData){
         sciPrintf("SUCCESS: Read data matches written data\r\n");
     }
-    else if (new_read_data == read_data){
+    else if (newReadData == readData){
         sciPrintf("ERROR: Data not updated after write operation\r\n");
     }
     else{
