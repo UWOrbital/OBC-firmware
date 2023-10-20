00948baede77cc631832b16bd3fc8e67f59bbe7f
nbrodbec <contact@nbrodbeck.com>
2023-10-20 15:37:53 -0400

    Fix outdated debug-tool code causing build errors

--- obc/tools/bringup/interface_debug_tool/source/main.c
+++ obc/tools/bringup/interface_debug_tool/source/main.c
@@ -1,3 +1,4 @@
+#include "obc_board_config.h"
 #include "obc_spi_io.h"
 #include "obc_sci_io.h"
 #include "obc_print.h"
@@ -41,7 +42,7 @@ void utilityCLI(void *pvParameters) {
   while (1) {
     unsigned char cmdChar;
     sciPrintf("Enter a command: ");
-    sciRead(&cmdChar, 1);
+    sciReadBytes(&cmdChar, 1, portMAX_DELAY, pdMS_TO_TICKS(1000), UART_READ_REG);
     sciPrintf("\r\n");
 
     if (cmdChar >= NUM_COMMANDS_MAX) {
