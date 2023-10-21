7da0d8732514356e11cb1b09d9d4b54e579917df
mariamelsahharr <54359917+mariamelsahharr@users.noreply.github.com>
2023-10-20 22:43:35 -0400

    Set reset reason

--- obc/sys/persistent/obc_persistent.h
+++ obc/sys/persistent/obc_persistent.h
@@ -18,15 +18,18 @@
  *        Ex: If you store an array in the section, you must read/write the
  *        entire array even if you only want to change 1 element.
  *      - The struct name should be <module>_persist_data_t
+ *
  * 2. Add a struct for the section itself (header + data)
  *      - The struct name should be <module>_persist_t
  *      - This is the layout of the section in persistent storage
+ *
  * 3. Add the section struct (header + data) to the obc_persist_t struct
  *      - The struct element name should be <module> (e.g. obcTime)
  *      - obc_persist_t is the layout of the entire persistent storage
  *      - You can declare a buffer of identical sections in this struct.
  *        This is useful for storing arrays that need to be individually
- *        accessible.
+ *        accessible
+ *
  * 4. Create getter/setter for the section
  *     - The getter/setter should be named getPersistent<Module> and
  *       setPersistent<Module> respectively
@@ -44,55 +47,67 @@ typedef struct {
 /*---------------------------------------------------------------------------*/
 
 /* SECTION AND DATA STRUCTS */
-
-// obc_time module
 typedef struct {
-  uint32_t unixTime;
-} obc_time_persist_data_t;
+  // obc_time module
+  typedef struct {
+    uint32_t unixTime;
+  } obc_time_persist_data_t;
 
-typedef struct {
-  obc_persist_section_header_t header;
-  obc_time_persist_data_t data;
-} obc_time_persist_t;
+  typedef struct {
+    obc_persist_section_header_t header;
+    obc_time_persist_data_t data;
+  } obc_time_persist_t;
 
-// alarm_mgr module
-typedef struct {
-  uint32_t unixTime;
-  alarm_type_t type;
-  alarm_handler_callback_def_t callbackDef;
-  union {
-    cmd_msg_t cmdMsg;
-  };
+  // alarm_mgr module
+  typedef struct {
+    uint32_t unixTime;
+    alarm_type_t type;
+    alarm_handler_callback_def_t callbackDef;
+    union {
+      cmd_msg_t cmdMsg;
+    };
+  } alarm_mgr_persist_data_t;
 
-} alarm_mgr_persist_data_t;
+  typedef struct {
+    obc_persist_section_header_t header;
+    alarm_mgr_persist_data_t data;
 
-typedef struct {
-  obc_persist_section_header_t header;
-  alarm_mgr_persist_data_t data;
+  } alarm_mgr_persist_t;
 
-} alarm_mgr_persist_t;
+  typedef struct {
+    obc_reset_reason_t reason;
+    // unsure ngl
+  } obc_reset_reason_persist_data_t;
 
-/*---------------------------------------------------------------------------*/
+  typedef struct {
+    obc_persist_section_header_t header;
+    obc_reset_reason_persist_data_t data;
+  } obc_reset_reason_persist_t;
 
-/**
- * @brief Layout of the persistent storage
- */
+  /*---------------------------------------------------------------------------*/
 
-typedef struct {
-  obc_time_persist_t obcTime;
+  /**
+   * @brief Layout of the persistent storage
+   */
 
-  alarm_mgr_persist_t alarmMgr[OBC_PERSISTENT_MAX_ALARM_COUNT];
+  typedef struct {
+    obc_time_persist_t obcTime;
+    alarm_mgr_persist_t alarmMgr[OBC_PERSISTENT_MAX_ALARM_COUNT];
+    obc_reset_reason_persist_t resetReason;
 
-} obc_persist_t;
+  } obc_persist_t;
 
 #define OBC_PERSIST_ADDR_OF(data) (0x0 + offsetof(obc_persist_t, data))
 
-/*---------------------------------------------------------------------------*/
+  /*---------------------------------------------------------------------------*/
 
-/* GETTERS AND SETTERS */
+  /* GETTERS AND SETTERS */
 
-obc_error_code_t getPersistentObcTime(obc_time_persist_data_t *buffer);
-obc_error_code_t setPersistentObcTime(obc_time_persist_data_t *data);
+  obc_error_code_t getPersistentObcTime(obc_time_persist_data_t *buffer);
+  obc_error_code_t setPersistentObcTime(obc_time_persist_data_t *data);
 
-obc_error_code_t getPersistentAlarmMgr(alarm_mgr_persist_data_t *buffer, size_t index);
-obc_error_code_t setPersistentAlarmMgr(alarm_mgr_persist_data_t *data, size_t index);
+  obc_error_code_t getPersistentAlarmMgr(alarm_mgr_persist_data_t *buffer, size_t index);
+  obc_error_code_t setPersistentAlarmMgr(alarm_mgr_persist_data_t *data, size_t index);
+
+  obc_error_code_t getPersistentResetReason(obc_reset_reason_persist_t *data);
+  obc_error_code_t setPersistentResetReason(obc_reset_reason_persist_t *data);
