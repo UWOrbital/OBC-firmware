6fe674e712d2313b4126f568cf0920d1fe55f1cb
Ekaums <ekaum.singh@gmail.com>
2023-10-31 19:11:55 -0400

    Created script

---
+++ obc/bl/bl_append_header.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+import struct
+import serial
+import argparse
+
+# Define header struct. Example dummy struct
+@dataclass
+class header_struct:
+    version: int
+    bin_size: int
+    data: int
+    
+def send_bin(input_file, input_version, header_data, com_port):
+        
+        with open(input_file, 'rb') as f:
+            bin_data = f.read()
+            
+        bin_len = len(bin_data)
+        header = header_struct(version=input_version, bin_size=bin_len, data=header_data)
+        
+        header_bytes = struct.pack('III', header.version, header.bin_size, header.data)
+        
+        output_file = input_file.replace('.bin', '_header.bin')
+        
+        with open(output_file, 'wb') as f: # Create output binary
+            f.write(header_bytes)
+            f.write(bin_data)
+
+        
+        OBC_UART_BAUD_RATE = 115200
+        
+        with serial.Serial(com_port, OBC_UART_BAUD_RATE, timeout=1) as ser:
+            with open(input_file, 'rb') as f:
+                data = f.read()
+                ser.write(data) # Write binary to device via UART
+            ser.close()
+
+        
+def main():
+    parser = argparse.ArgumentParser(description='Append custom data to .bin and send')
+    
+    # Add arguments
+    parser.add_argument('-i', '--input_file', required=True, help='Path to the input .bin file.')
+    parser.add_argument('-p', '--port', required=True, type=str, help='Serial port')
+    parser.add_argument('-d', '--data', required=True, type=int, help='Custom data to be sent')
+    parser.add_argument('-v', '--version', type=int, default=1, help='Version of the header. Default is 1.')
+
+    # Parse args
+    args = parser.parse_args()
+    send_bin(args.input_file, args.version, args.data, args.port)
+    
+if __name__ == "__main__":
+    main()
+
+    
