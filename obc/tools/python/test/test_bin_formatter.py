4df62795fa6f609438a2164b335ba48a2d95557f
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2023-11-09 10:10:38 -0500

    Add comment test_bin_formatter.py

--- obc/tools/python/test/test_bin_formatter.py
+++ obc/tools/python/test/test_bin_formatter.py
@@ -7,7 +7,7 @@ import os
 # Expected header is the header in bytes
 @mark.parametrize('input_name, bin_version, bin_data, expected_name, expected_header, expect_data', [
         ('test1.bin', 0, b'0123456789', 'test1_formatted.bin',
-        b'\x00\x00\x00\x00\n\x00\x00\x00', b'0123456789'),
+        b'\x00\x00\x00\x00\n\x00\x00\x00', b'0123456789'), # \n has ASCII value of 10 so struct.pack uses \n
         ('test2.bin', 2, b'10987', 'test2_formatted.bin',
         b'\x02\x00\x00\x00\x05\x00\x00\x00', b'10987'),
         ('test3.bin', 3, b'1', 'test3_formatted.bin',
