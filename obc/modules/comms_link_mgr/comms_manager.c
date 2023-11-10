6cb95548252f54c7ef7b742771157c0945bab34b
Navtaj Hundal <n2hundal@uwaterloo.ca>
2023-11-10 16:37:51 -0500

    revert testing changes

--- obc/modules/comms_link_mgr/comms_manager.c
+++ obc/modules/comms_link_mgr/comms_manager.c
@@ -14,8 +14,6 @@
 #include "telemetry_manager.h"
 #include "cc1120_txrx.h"
 #include "cc1120.h"
-#include "cc1120_defs.h"
-#include "cc1120_mcu.h"
 #include "rffm6404.h"
 #include "obc_privilege.h"
 
@@ -57,95 +55,6 @@ static uint8_t cc1120TransmitQueueStack[CC1120_TRANSMIT_QUEUE_LENGTH * CC1120_TR
 
 static const uint8_t TEMP_STATIC_KEY[AES_KEY_SIZE] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
                                                       0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
-
-uint8_t CC1120_REGS_DEFAULTS[CC1120_REGS_STD_SPACE_SIZE] = {
-    CC1120_DEFAULTS_IOCFG3,         CC1120_DEFAULTS_IOCFG2,
-    CC1120_DEFAULTS_IOCFG1,         CC1120_DEFAULTS_IOCFG0,
-    CC1120_DEFAULTS_SYNC3,          CC1120_DEFAULTS_SYNC2,
-    CC1120_DEFAULTS_SYNC1,          CC1120_DEFAULTS_SYNC0,
-    CC1120_DEFAULTS_SYNC_CFG1,      CC1120_DEFAULTS_SYNC_CFG0,
-    CC1120_DEFAULTS_DEVIATION_M,    CC1120_DEFAULTS_MODCFG_DEV_E,
-    CC1120_DEFAULTS_DCFILT_CFG,     CC1120_DEFAULTS_PREAMBLE_CFG1,
-    CC1120_DEFAULTS_PREAMBLE_CFG0,  CC1120_DEFAULTS_FREQ_IF_CFG,
-    CC1120_DEFAULTS_IQIC,           CC1120_DEFAULTS_CHAN_BW,
-    CC1120_DEFAULTS_MDMCFG1,        CC1120_DEFAULTS_MDMCFG0,
-    CC1120_DEFAULTS_SYMBOL_RATE2,   CC1120_DEFAULTS_SYMBOL_RATE1,
-    CC1120_DEFAULTS_SYMBOL_RATE0,   CC1120_DEFAULTS_AGC_REF,
-    CC1120_DEFAULTS_AGC_CS_THR,     CC1120_DEFAULTS_AGC_GAIN_ADJUST,
-    CC1120_DEFAULTS_AGC_CFG3,       CC1120_DEFAULTS_AGC_CFG2,
-    CC1120_DEFAULTS_AGC_CFG1,       CC1120_DEFAULTS_AGC_CFG0,
-    CC1120_DEFAULTS_FIFO_CFG,       CC1120_DEFAULTS_DEV_ADDR,
-    CC1120_DEFAULTS_SETTLING_CFG,   CC1120_DEFAULTS_FS_CFG,
-    CC1120_DEFAULTS_WOR_CFG1,       CC1120_DEFAULTS_WOR_CFG0,
-    CC1120_DEFAULTS_WOR_EVENT0_MSB, CC1120_DEFAULTS_WOR_EVENT0_LSB,
-    CC1120_DEFAULTS_PKT_CFG2,       CC1120_DEFAULTS_PKT_CFG1,
-    CC1120_DEFAULTS_PKT_CFG0,       CC1120_DEFAULTS_RFEND_CFG1,
-    CC1120_DEFAULTS_RFEND_CFG0,     CC1120_DEFAULTS_PA_CFG2,
-    CC1120_DEFAULTS_PA_CFG1,        CC1120_DEFAULTS_PA_CFG0,
-    CC1120_DEFAULTS_PKT_LEN};
-
-/**
- * @brief E2E test for SPI read function.
- * Reads through all registers up to the extended register space,
- * and compares values to default values.
- * Burst reads all the values and compares them to the default values.
- * Burst reads FREQ registers in extended address space and compares them to defaults.
- *
- * @return OBC_ERROR_CODE_SUCCESS - If all registers are read correctly and have the right value.
- * @return An error code - If any register does not have the expected value,
- *                 or status byte is invalid.
- */
-obc_error_code_t cc1120TestSpiRead(void) {
-  obc_error_code_t errCode;
-  uint8_t addr = 0x00U;
-  uint8_t data;
-  RETURN_IF_ERROR_CODE(cc1120StrobeSpi(CC1120_STROBE_SRES));
-  uint8_t burstData[CC1120_REGS_EXT_ADDR];
-  errCode = cc1120ReadSpi(addr, burstData, CC1120_REGS_EXT_ADDR);
-
-  RETURN_IF_ERROR_CODE(errCode);
-
-  if (memcmp(CC1120_REGS_DEFAULTS, burstData, CC1120_REGS_EXT_ADDR)) {
-    errCode = OBC_ERR_CODE_CC1120_TEST_FAILURE;
-    LOG_ERROR("CC1120 burst read test failed.\n");
-    return errCode;
-  }
-
-  if (errCode == OBC_ERR_CODE_SUCCESS) {
-    errCode = cc1120ReadExtAddrSpi(CC1120_REGS_EXT_MARCSTATE, &data, 1);
-
-    LOG_IF_ERROR_CODE(errCode);
-    RETURN_IF_ERROR_CODE(errCode);
-
-    if (data != 0x41U) {
-      errCode = OBC_ERR_CODE_CC1120_TEST_FAILURE;
-      LOG_ERROR_CODE(errCode);
-      return errCode;
-    }
-  }
-
-  if (errCode == OBC_ERR_CODE_SUCCESS) {
-    uint8_t extBurstData[3];
-    uint8_t expected[3] = {0x00U, 0x00U, 0x00U};
-    errCode = cc1120ReadExtAddrSpi(CC1120_REGS_EXT_FREQ2, extBurstData, 3);
-
-    if (errCode != OBC_ERR_CODE_SUCCESS) {
-      errCode = OBC_ERR_CODE_CC1120_TEST_FAILURE;
-      LOG_ERROR_CODE(errCode);
-      return errCode;
-    }
-
-    if (memcmp(extBurstData, expected, 3)) {
-      errCode = OBC_ERR_CODE_CC1120_TEST_FAILURE;
-      LOG_ERROR_CODE(errCode);
-      return errCode;
-    }
-  }
-
-  if (errCode == OBC_ERR_CODE_SUCCESS) LOG_INFO("CC1120 SPI read test passed.\n");
-
-  return errCode;
-}
 
 /**
  * @brief determines what the next Comms Manager state should be and sets it to that state
@@ -382,24 +291,41 @@ obc_error_code_t sendToFrontCommsManagerQueue(comms_event_t *event) {
 
 void obcTaskFunctionCommsMgr(void *pvParameters) {
   obc_error_code_t errCode;
-
   comms_state_t commsState = *((comms_state_t *)pvParameters);
-  gioSetBit(gioPORTA, 0, 0);
-  gioSetBit(gioPORTA, 0, 1);
+
   initAllCc1120TxRxSemaphores();
   LOG_IF_ERROR_CODE(cc1120Init());
 
-  char buf[300] = {0};
-  memset(buf, 0x55, sizeof(buf));
-
-  // LOG_IF_ERROR_CODE(cc1120TestSpiRead());
-  // LOG_IF_ERROR_CODE(cc1120TestSpiRead());
-  // uint8_t data = 0x4F;
-  // LOG_IF_ERROR_CODE(cc1120WriteSpi(CC1120_REGS_PA_CFG2, &data, 1));
-  // LOG_IF_ERROR_CODE(cc1120StrobeSpi(CC1120_STROBE_STX));
   while (1) {
-    errCode = cc1120Send(buf, sizeof(buf), pdMS_TO_TICKS(1000));
-    // vTaskDelay(pdMS_TO_TICKS(5000));
+    comms_event_t queueMsg;
+
+    if (xQueueReceive(commsQueueHandle, &queueMsg, COMMS_MANAGER_QUEUE_RX_WAIT_PERIOD) != pdPASS) {
+      continue;
+    }
+
+    LOG_IF_ERROR_CODE(getNextCommsState(queueMsg.eventID, &commsState));
+    if (errCode != OBC_ERR_CODE_SUCCESS) {
+      continue;
+    }
+
+    if (commsState >= sizeof(commsStateFns) / sizeof(comms_state_func_t)) {
+      LOG_ERROR_CODE(OBC_ERR_CODE_INVALID_STATE);
+      commsState = COMMS_STATE_DISCONNECTED;
+      continue;
+    }
+
+    if (commsStateFns[commsState] == NULL) {
+      LOG_ERROR_CODE(OBC_ERR_CODE_INVALID_STATE);
+      commsState = COMMS_STATE_DISCONNECTED;
+      continue;
+    }
+
+    LOG_IF_ERROR_CODE(commsStateFns[commsState]());
+    if (errCode != OBC_ERR_CODE_SUCCESS) {
+      rffm6404PowerOff();
+      comms_event_t event = {.eventID = COMMS_EVENT_ERROR};
+      sendToCommsManagerQueue(&event);
+    }
   }
 }
 
