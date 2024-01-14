510eebd594288342517aead9c3cb331b5fbf4fd5
JoelManYunLee <joelmyleee@gmail.com>
2024-01-14 18:06:37 -0500

    Removed Queue from GNC task

--- obc/modules/gnc_mgr/gnc_manager.c
+++ obc/modules/gnc_mgr/gnc_manager.c
@@ -13,32 +13,9 @@
 
 #define GNC_TASK_PERIOD_MS 50 /* 50ms period or 20Hz */
 
-static QueueHandle_t gncQueueHandle = NULL;
-static StaticQueue_t gncQueue;
-static uint8_t gncQueueStack[GNC_MANAGER_QUEUE_LENGTH * GNC_MANAGER_QUEUE_ITEM_SIZE];
-
-void obcTaskInitGncMgr(void) {
-  ASSERT((gncQueueStack != NULL) && (&gncQueue != NULL));
-  if (gncQueueHandle == NULL) {
-    gncQueueHandle =
-        xQueueCreateStatic(GNC_MANAGER_QUEUE_LENGTH, GNC_MANAGER_QUEUE_ITEM_SIZE, gncQueueStack, &gncQueue);
-  }
-}
-
-obc_error_code_t sendToGncQueue(gnc_event_t *event) {
-  ASSERT(gncQueueHandle != NULL);
-
-  if (event == NULL) return OBC_ERR_CODE_INVALID_ARG;
-
-  if (xQueueSend(gncQueueHandle, (void *)event, GNC_MANAGER_QUEUE_TX_WAIT_PERIOD) == pdPASS) {
-    return OBC_ERR_CODE_SUCCESS;
-  }
-  return OBC_ERR_CODE_QUEUE_FULL;
-}
+void obcTaskInitGncMgr(void) {}
 
 void obcTaskFunctionGncMgr(void *pvParameters) {
-  ASSERT(gncQueueHandle != NULL);
-
   TickType_t xLastWakeTime;
 
   // Initialize the last wake time to the current time
