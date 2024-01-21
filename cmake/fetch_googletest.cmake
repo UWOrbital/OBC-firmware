fde6bb4bd08d1b12c976bdfc9c2672fdf61cc1ab
danglevm <danglevuminh@gmail.com>
2024-01-21 10:16:05 -0500

    add 32 bit compile and link options - not working

--- cmake/fetch_googletest.cmake
+++ cmake/fetch_googletest.cmake
@@ -12,5 +12,11 @@ GIT_TAG        release-1.11.0
 )
 FetchContent_MakeAvailable(googletest)
 add_library(GTest::GTest INTERFACE IMPORTED)
+if(${CMAKE_BUILD_TYPE} MATCHES Test)
+target_compile_options(gtest INTERFACE -m32)
+target_compile_options(gtest_main INTERFACE -m32)
+target_compile_options(GTest::GTest INTERFACE -m32)
+target_compile_options(googletest INTERFACE -m32)
+endif()
 target_link_libraries(GTest::GTest INTERFACE gtest_main)
 # ---------------------------------------------------------------
