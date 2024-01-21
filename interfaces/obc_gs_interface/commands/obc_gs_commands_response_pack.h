d08115313749b52849f40443c9e3621251aa347e
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-01-21 01:18:55 -0500

    Forgot to wrap for Cpp

--- interfaces/obc_gs_interface/commands/obc_gs_commands_response_pack.h
+++ interfaces/obc_gs_interface/commands/obc_gs_commands_response_pack.h
@@ -8,4 +8,12 @@
 #include "obc_gs_errors.h"
 #include "obc_gs_commands_response.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 obc_gs_error_code_t packCommandResponse(cmd_unpacked_response_t response, uint8_t *buffer);
+
+#ifdef __cplusplus
+}
+#endif
--- interfaces/obc_gs_interface/commands/obc_gs_commands_response_unpack.h
+++ interfaces/obc_gs_interface/commands/obc_gs_commands_response_unpack.h
@@ -8,4 +8,12 @@
 #include "obc_gs_errors.h"
 #include "obc_gs_commands_response.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 obc_gs_error_code_t unpackCommandResponse(uint8_t* buffer, cmd_unpacked_response_t* response);
+
+#ifdef __cplusplus
+}
+#endif
