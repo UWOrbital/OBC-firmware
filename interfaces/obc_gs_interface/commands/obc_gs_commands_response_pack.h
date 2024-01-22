78794fb3bf356054bef0e6f5ce90836c3e03003a
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-01-21 19:19:02 -0500

    Added another param

--- interfaces/obc_gs_interface/commands/obc_gs_commands_response_pack.h
+++ interfaces/obc_gs_interface/commands/obc_gs_commands_response_pack.h
@@ -14,6 +14,7 @@ extern "C" {
 
 /**
  * @brief Pack a buffer of chars from a struct of type cmd_unpacked_response_t representing the response of a command.
+ * @param response The unpacked response which needs to be packed into bytes.
  * @param buffer A buffer of bytes to which to write the packed data
  * @return Error code indicating if the packing operation was successful. Error code OBC_GS_ERR_CODE_INVALID_ARG if the
  * buffer is NULL.
--- interfaces/obc_gs_interface/commands/obc_gs_commands_response_unpack.h
+++ interfaces/obc_gs_interface/commands/obc_gs_commands_response_unpack.h
@@ -15,6 +15,7 @@ extern "C" {
 /**
  * @brief Unpack a buffer of chars to a struct of type cmd_unpacked_response_t representing the response of a command.
  * @param buffer A buffer of bytes from which to read the packed data
+ * @param response Pointer to the response which needs to be set from values obtained by unpacking the buffer.
  * @return Error code indicating if the write was successful. OBC_GS_ERR_CODE_UNSUPPORTED_CMD if the unpacked cmd was
  * not valid.
  */
