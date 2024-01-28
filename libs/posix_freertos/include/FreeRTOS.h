bbb33b7c9e40da4036081246767a54d847037a0d
Thenuja Liyanage <Liyanage.Thenuja@hotmail.com>
2024-01-28 10:52:15 -0800

    Updated checking if current task is the mutex owner without needing to aquire mutex

--- libs/posix_freertos/include/FreeRTOS.h
+++ libs/posix_freertos/include/FreeRTOS.h
@@ -203,7 +203,7 @@ extern "C" {
 #endif
 
 #ifndef INCLUDE_xTaskGetCurrentTaskHandle
-#define INCLUDE_xTaskGetCurrentTaskHandle 0
+#define INCLUDE_xTaskGetCurrentTaskHandle 1
 #endif
 
 #if configUSE_CO_ROUTINES != 0
