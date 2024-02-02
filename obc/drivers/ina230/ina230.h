ed324ae51f8bdc8e65ac66ecec2c5abe4280ba77
Hadi Zaidi <38956768+0xVR@users.noreply.github.com>
2024-02-01 23:50:30 -0500

    Add pin enum for config struct to calculate device address

--- obc/drivers/ina230/ina230.h
+++ obc/drivers/ina230/ina230.h
@@ -10,6 +10,8 @@ typedef enum {
   SHUNT_AND_BUS_CTS
 } ina230_opmode_t;
 
+typedef enum { SDA, SCL, GND, VS } ina230_pin_t;
+
 /**
  * @struct Configuration struct for INA230 sensor
  *
@@ -17,10 +19,14 @@ typedef enum {
  * @param busVoltCT Conversion time for bus voltage measurement
  * @param shuntVoltCT Conversion time for shunt voltage measurement
  * @param opMode Operating mode for sensor
+ * @param a0 What the address pin A0 is connected to
+ * @param a1 What the address pin A1 is connected to
  */
 typedef struct {
   uint16_t sampleSize;
   float busVoltCT;
   float shuntVoltCT;
   ina230_opmode_t opMode;
+  ina230_pin_t a0;
+  ina230_pin_t a1;
 } ina230_config_t;
