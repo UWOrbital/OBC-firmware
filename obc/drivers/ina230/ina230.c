dcd3dad1016f4e57512ae553d354708dc1e08074
Hadi Zaidi <38956768+0xVR@users.noreply.github.com>
2024-02-02 14:07:24 -0500

    Add arg parsing to config write func

--- obc/drivers/ina230/ina230.c
+++ obc/drivers/ina230/ina230.c
@@ -26,7 +26,160 @@ obc_error_code_t ina230Init(ina230_config_t *config) {
   // return OBC_ERR_CODE_SUCCESS;
 }
 
-// TODO: write config func
+obc_error_code_t writeConfigINA230(uint16_t sampleSize, uint16_t busVoltCT, uint16_t shuntVoltCT,
+                                   ina230_opmode_t opMode, ina230_pin_t a0, ina230_pin_t a1) {
+  obc_error_code_t errCode;
+
+  uint8_t sampleSizeRegData;
+  switch (sampleSize) {
+    case 1:
+      sampleSizeRegData = 0x00;
+      break;
+    case 4:
+      sampleSizeRegData = 0x01;
+      break;
+    case 16:
+      sampleSizeRegData = 0x02;
+      break;
+    case 64:
+      sampleSizeRegData = 0x03;
+      break;
+    case 128:
+      sampleSizeRegData = 0x04;
+      break;
+    case 256:
+      sampleSizeRegData = 0x05;
+      break;
+    case 512:
+      sampleSizeRegData = 0x06;
+      break;
+    case 1024:
+      sampleSizeRegData = 0x07;
+      break;
+    default:
+      return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  uint8_t busVoltCTRegData;
+  switch (busVoltCT) {
+    case 140:
+      busVoltCTRegData = 0x00;
+      break;
+    case 204:
+      busVoltCTRegData = 0x01;
+      break;
+    case 332:
+      busVoltCTRegData = 0x02;
+      break;
+    case 588:
+      busVoltCTRegData = 0x03;
+      break;
+    case 1100:
+      busVoltCTRegData = 0x04;
+      break;
+    case 2116:
+      busVoltCTRegData = 0x05;
+      break;
+    case 4156:
+      busVoltCTRegData = 0x06;
+      break;
+    case 8244:
+      busVoltCTRegData = 0x07;
+      break;
+    default:
+      return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  uint8_t shuntVoltCTRegData;
+  switch (shuntVoltCT) {
+    case 140:
+      shuntVoltCTRegData = 0x00;
+      break;
+    case 204:
+      shuntVoltCTRegData = 0x01;
+      break;
+    case 332:
+      shuntVoltCTRegData = 0x02;
+      break;
+    case 588:
+      shuntVoltCTRegData = 0x03;
+      break;
+    case 1100:
+      shuntVoltCTRegData = 0x04;
+      break;
+    case 2116:
+      shuntVoltCTRegData = 0x05;
+      break;
+    case 4156:
+      shuntVoltCTRegData = 0x06;
+      break;
+    case 8244:
+      shuntVoltCTRegData = 0x07;
+      break;
+    default:
+      return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  uint8_t devAddr;
+  if (a1 == GND) {
+    if (a0 == GND) {
+      devAddr = 0x40;
+    } else if (a0 == VS) {
+      devAddr = 0x42;
+    } else if (a0 == SDA) {
+      devAddr = 0x44;
+    } else if (a0 == SCL) {
+      devAddr = 0x46;
+    } else {
+      return OBC_ERR_CODE_INVALID_ARG;
+    }
+  } else if (a1 == VS) {
+    if (a0 == GND) {
+      devAddr = 0x48;
+    } else if (a0 == VS) {
+      devAddr = 0x4A;
+    } else if (a0 == SDA) {
+      devAddr = 0x4C;
+    } else if (a0 == SCL) {
+      devAddr = 0x4E;
+    } else {
+      return OBC_ERR_CODE_INVALID_ARG;
+    }
+  } else if (a1 == SDA) {
+    if (a0 == GND) {
+      devAddr = 0x50;
+    } else if (a0 == VS) {
+      devAddr = 0x52;
+    } else if (a0 == SDA) {
+      devAddr = 0x54;
+    } else if (a0 == SCL) {
+      devAddr = 0x56;
+    } else {
+      return OBC_ERR_CODE_INVALID_ARG;
+    }
+  } else if (a1 == SCL) {
+    if (a0 == GND) {
+      devAddr = 0x58;
+    } else if (a0 == VS) {
+      devAddr = 0x5A;
+    } else if (a0 == SDA) {
+      devAddr = 0x5C;
+    } else if (a0 == SCL) {
+      devAddr = 0x5E;
+    } else {
+      return OBC_ERR_CODE_INVALID_ARG;
+    }
+  } else {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  uint8_t opModeRegData;
+  if (opMode > 7) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  } else {
+    opModeRegData = opMode;
+  }
+}
 
 // TODO: read config func
 
--- obc/drivers/ina230/ina230.h
+++ obc/drivers/ina230/ina230.h
@@ -5,7 +5,7 @@ typedef enum {
   SHUNT_VOLT_TRIG,
   BUS_VOLT_TRIG,
   SHUNT_AND_BUS_TRIG,
-  SHUNT_VOLT_CTS,
+  SHUNT_VOLT_CTS = 5,
   BUS_VOLT_CTS,
   SHUNT_AND_BUS_CTS
 } ina230_opmode_t;
@@ -16,16 +16,16 @@ typedef enum { SDA, SCL, GND, VS } ina230_pin_t;
  * @struct Configuration struct for INA230 sensor
  *
  * @param sampleSize Number of samples collected and averaged for calculations (1, 4, 16, 64, 128, 256, 512, or 1024)
- * @param busVoltCT Conversion time for bus voltage measurement
- * @param shuntVoltCT Conversion time for shunt voltage measurement
+ * @param busVoltCT Conversion time for bus voltage measurement in microseconds
+ * @param shuntVoltCT Conversion time for shunt voltage measurement in microseconds
  * @param opMode Operating mode for sensor
  * @param a0 What the address pin A0 is connected to
  * @param a1 What the address pin A1 is connected to
  */
 typedef struct {
   uint16_t sampleSize;
-  float busVoltCT;
-  float shuntVoltCT;
+  uint16_t busVoltCT;
+  uint16_t shuntVoltCT;
   ina230_opmode_t opMode;
   ina230_pin_t a0;
   ina230_pin_t a1;
