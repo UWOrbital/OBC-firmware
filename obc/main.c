ec6c86163b17abaf9dd4f023c866fbc005459caa
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2024-02-10 23:28:01 -0500

    Captured output

--- obc/main.c
+++ obc/main.c
@@ -1,3 +1,5 @@
+#include "comms_manager.h"
+#include "obc_errors.h"
 #include "obc_logging.h"
 #include "obc_sci_io.h"
 #include "obc_i2c_io.h"
@@ -17,6 +19,14 @@
 #include <spi.h>
 #include <can.h>
 #include <het.h>
+
+// Rm:
+#include <string.h>
+#include "obc_print.h"
+#include "cc1120_txrx.h"
+#include "cc1120.h"
+#include "obc_gs_aes128.h"
+#include "obc_gs_fec.h"
 
 // This is the stack canary. It should never be overwritten.
 // Ideally, it would be a random value, but we don't have a good source of entropy
@@ -41,8 +51,23 @@ int main(void) {
   initI2CMutex();
   initSpiMutex();
 
+  /*
+  obc_error_code_t errCode;
+  static const uint8_t TEMP_STATIC_KEY[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                                            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+  initializeAesCtx(TEMP_STATIC_KEY);
+  initRs();
+  initAllCc1120TxRxSemaphores();
+  LOG_IF_ERROR_CODE(cc1120Init());
+  uint8_t buff[300];
+  memset(buff, 0xA5, 300);
+  while (1) {
+    cc1120Send(buff, 300, 500);
+  }
+*/
+  static comms_state_t commsManagerState = COMMS_STATE_DISCONNECTED;
   obcSchedulerInitTask(OBC_SCHEDULER_CONFIG_ID_COMMS_MGR);
-  obcSchedulerCreateTask(OBC_SCHEDULER_CONFIG_ID_COMMS_MGR);
+  obcSchedulerCreateTaskWithArgs(OBC_SCHEDULER_CONFIG_ID_COMMS_MGR, &commsManagerState);
 
   // The state_mgr is the only task running initially.
   // obcSchedulerInitTask(OBC_SCHEDULER_CONFIG_ID_STATE_MGR);
