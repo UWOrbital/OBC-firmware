470461f82f5809f98af8b56d6d3c9f7070567719
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2024-02-17 20:10:11 -0500

    Test mypy pre-commit

--- obc/tools/python/bin_formatter.py
+++ obc/tools/python/bin_formatter.py
@@ -7,6 +7,7 @@ from typing import Final
 
 OBC_UART_BAUD_RATE: Final = 115200
 
+
 # Define header class. This is the header that will be appended to the .bin file
 @dataclass
 class BootloaderHeader:
@@ -14,8 +15,8 @@ class BootloaderHeader:
     bin_size: int
 
     def serialize(self) -> bytes:
-        """ Returns the serialized version of the object """
-        return struct.pack('<II', self.version, self.bin_size)
+        """Returns the serialized version of the object"""
+        return struct.pack("<II", self.version, self.bin_size)
 
 
 def create_bin(input_path: str, input_version: int) -> str:
@@ -31,10 +32,12 @@ def create_bin(input_path: str, input_version: int) -> str:
 
     input_bin_data = input_obj.read_bytes()
     bin_len = len(input_bin_data)
-    header = BootloaderHeader(version=input_version, bin_size=bin_len) # Create header struct
+    header = BootloaderHeader(
+        version=input_version, bin_size=bin_len
+    )  # Create header struct
     header_bytes = header.serialize()
 
-    output_path = input_path.replace('.bin', '_formatted.bin')
+    output_path = input_path.replace(".bin", "_formatted.bin")
     output_obj = Path(output_path)
     output_obj.write_bytes(header_bytes + input_bin_data)
 
@@ -52,23 +55,43 @@ def send_bin(file_path: str, com_port: str) -> None:
     file_obj = Path(file_path)
 
     # Open serial port and write binary to device via UART
-    with serial.Serial(com_port, baudrate=OBC_UART_BAUD_RATE, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_TWO, timeout=1) as ser:
-       data = file_obj.read_bytes()
-       ser.write(data)
+    with serial.Serial(
+        com_port,
+        baudrate=OBC_UART_BAUD_RATE,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_TWO,
+        timeout=1,
+    ) as ser:
+        data = file_obj.read_bytes()
+        ser.write(data)
 
 
 def arg_parse() -> ArgumentParser:
-    parser = ArgumentParser(description='Append custom data to .bin and send')
+    parser = ArgumentParser(description="Append custom data to .bin and send")
 
     # Add arguments
-    parser.add_argument('-i', required=True, dest='input_path', type=str, help='Path to the input .bin file.')
-    parser.add_argument('-p', required=True, dest='port', type=str, help='Serial port number')
-    parser.add_argument('-v', dest='version', type=int, default=0, help='Version of the application. Default is 0')
+    parser.add_argument(
+        "-i",
+        required=True,
+        dest="input_path",
+        type=str,
+        help="Path to the input .bin file.",
+    )
+    parser.add_argument(
+        "-p", required=True, dest="port", type=str, help="Serial port number"
+    )
+    parser.add_argument(
+        "-v",
+        dest="version",
+        type=int,
+        default=0,
+        help="Version of the application. Default is 0",
+    )
 
     return parser
 
 
-def main():
+def main() -> None:
     arg_parser = arg_parse()
     args = arg_parser.parse_args()
 
@@ -76,5 +99,5 @@ def main():
     send_bin(output_file, args.port)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
