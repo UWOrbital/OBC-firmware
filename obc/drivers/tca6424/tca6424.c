a94dffe6a1678cf8b1a255ee96e15a261511c51f
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-02-25 00:39:53 -0500

    Made changes to interface

--- obc/drivers/tca6424/tca6424.c
+++ obc/drivers/tca6424/tca6424.c
@@ -41,36 +41,34 @@ obc_error_code_t configureTCA6424APin(uint8_t pinLocation, TCA6424A_gpio_config_
   return OBC_ERR_CODE_SUCCESS;
 }
 
-obc_error_code_t driveTCA6424APinOutput(TCA6424A_pin_state_t pinState) {
-  uint8_t pinPort = (pinState.pinLocation & 0xF0) >> 4;
-  uint8_t pinIndex = pinState.pinLocation & 0x0F;
+obc_error_code_t driveTCA6424APinOutput(uint8_t pinLocation, uint8_t IOPortValue) {
+  uint8_t pinPort = (pinLocation & 0xF0) >> 4;
+  uint8_t pinIndex = pinLocation & 0x0F;
   if (pinIndex > MAX_PIN_COUNT || pinPort > MAX_PORT_COUNT) return OBC_ERR_CODE_INVALID_ARG;
 
   uint8_t outputPortAddress = TCA6424A_OUTPUT_PORT_ZERO_ADDR + pinPort;
   uint8_t outputPort = 0;
 
   obc_error_code_t errCode;
-  RETURN_IF_ERROR_CODE(configureTCA6424APin(pinState.pinLocation, TCA6424A_GPIO_CONFIG_OUTPUT));
+  RETURN_IF_ERROR_CODE(configureTCA6424APin(pinLocation, TCA6424A_GPIO_CONFIG_OUTPUT));
   RETURN_IF_ERROR_CODE(readTCA6424ARegister(outputPortAddress, &outputPort, 1));
 
-  outputPort |= ((pinState.IOPortValue & 0x01) << pinIndex);
+  outputPort |= ((IOPortValue & 0x01) << pinIndex);
   RETURN_IF_ERROR_CODE(writeTCA6424ARegister(outputPortAddress, &outputPort, 1));
   return OBC_ERR_CODE_SUCCESS;
 }
 
-obc_error_code_t readTCA6424APinInput(TCA6424A_pin_state_t* pinState) {
-  uint8_t pinPort = (pinState->pinLocation & 0xF0) >> 4;
-  uint8_t pinIndex = pinState->pinLocation & 0x0F;
+obc_error_code_t readTCA6424APinInput(uint8_t pinLocation, uint8_t* IOPortValue) {
+  uint8_t pinPort = (pinLocation & 0xF0) >> 4;
+  uint8_t pinIndex = pinLocation & 0x0F;
   if (pinIndex > MAX_PIN_COUNT || pinPort > MAX_PORT_COUNT) return OBC_ERR_CODE_INVALID_ARG;
-  if (pinState == NULL) return OBC_ERR_CODE_INVALID_ARG;
 
   obc_error_code_t errCode;
   uint8_t IOPortAddress = TCA6424A_INPUT_PORT_ZERO_ADDR + pinPort;
-  uint8_t IOPortValue = 0;
+  uint8_t IOPortRegister = 0;
 
-  RETURN_IF_ERROR_CODE(readTCA6424ARegister(IOPortAddress, &IOPortValue, 1));
-  IOPortValue = (IOPortValue & (0x01 << pinIndex)) ? TCA6424A_GPIO_HIGH : TCA6424A_GPIO_LOW;
-  pinState->IOPortValue = IOPortValue;
+  RETURN_IF_ERROR_CODE(readTCA6424ARegister(IOPortAddress, &IOPortRegister, 1));
+  *IOPortValue = (IOPortRegister & (0x01 << pinIndex)) ? TCA6424A_GPIO_HIGH : TCA6424A_GPIO_LOW;
   return OBC_ERR_CODE_SUCCESS;
 }
 
