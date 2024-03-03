88944d3474987dec21c82015a9ae9bee57dcdf87
nameerahr <nameerahrazi@gmail.com>
2024-03-02 22:38:13 -0500

    Added fclose on failure and removed duplicate logic

--- gs/logger/gs_logger.c
+++ gs/logger/gs_logger.c
@@ -6,6 +6,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define MAX_UINT32_STRING_SIZE 11U
+
 static const char *LEVEL_STRINGS[] = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
 static log_level_t logLevel;
 
@@ -14,17 +16,11 @@ void gsInitLogger(void) { logLevel = LOG_DEFAULT_LEVEL; }
 void logSetLevel(log_level_t newLogLevel) { logLevel = newLogLevel; }
 
 gs_error_code_t logErrorCode(log_level_t msgLevel, const char *file, uint32_t line, uint32_t errCode) {
-  if (msgLevel < logLevel) {
-    return GS_ERR_CODE_LOG_MSG_SILENCED;
-  }
+  // Convert error code to string
+  char errCodeStr[MAX_UINT32_STRING_SIZE] = {0};
+  snprintf(errCodeStr, MAX_UINT32_STRING_SIZE, "%lu", (unsigned long)errCode);
 
-  if (file == NULL) {
-    return GS_ERR_CODE_INVALID_ARG;
-  }
-
-  printf("%-5s -> %s:%lu - %lu\r\n", LEVEL_STRINGS[msgLevel], file, (unsigned long)line, (unsigned long)errCode);
-
-  return GS_ERR_CODE_SUCCESS;
+  return logMsg(msgLevel, file, line, errCodeStr);
 }
 
 gs_error_code_t logMsg(log_level_t msgLevel, const char *file, uint32_t line, const char *msg) {
--- interfaces/obc_gs_interface/logger/obc_gs_logger.c
+++ interfaces/obc_gs_interface/logger/obc_gs_logger.c
@@ -8,6 +8,7 @@
 
 #define LOG_FILE_NAME "obc_gs_log.log"
 
+#define MAX_UINT32_STRING_SIZE 11U
 #define MAX_MSG_SIZE 128U
 #define MAX_FNAME_LINENUM_SIZE 128U
 // Extra 10 for the small extra pieces in "%s - %s\r\n"
@@ -23,10 +24,10 @@ void logSetLevel(log_level_t newLogLevel) { logLevel = newLogLevel; }
 obc_gs_error_code_t writeToLogFile(char logBuf[], int logBufLen) {
   FILE *fpointer = fopen(LOG_FILE_NAME, "a");
   if (fpointer == NULL) {
-    printf("opening file\n");
     return OBC_GS_ERR_CODE_UNKNOWN;
   }
   if (fwrite(logBuf, sizeof(char), logBufLen, fpointer) != (size_t)logBufLen) {
+    fclose(fpointer);
     return OBC_GS_ERR_CODE_UNKNOWN;
   }
   if (fclose(fpointer) != 0) {
@@ -37,27 +38,11 @@ obc_gs_error_code_t writeToLogFile(char logBuf[], int logBufLen) {
 }
 
 obc_gs_error_code_t logErrorCode(log_level_t msgLevel, const char *file, uint32_t line, uint32_t errCode) {
-  if (msgLevel < logLevel) {
-    return OBC_GS_ERR_CODE_LOG_MSG_SILENCED;
-  }
+  // Convert error code to string
+  char errCodeStr[MAX_UINT32_STRING_SIZE] = {0};
+  snprintf(errCodeStr, MAX_UINT32_STRING_SIZE, "%lu", (unsigned long)errCode);
 
-  if (file == NULL) {
-    return OBC_GS_ERR_CODE_INVALID_ARG;
-  }
-
-  // Construct log entry
-  char logBuf[MAX_LOG_SIZE] = {0};
-  int logBufLen = 0;
-  logBufLen = snprintf(logBuf, MAX_LOG_SIZE, "%-5s -> %s:%lu - %lu\r\n", LEVEL_STRINGS[msgLevel], file,
-                       (unsigned long)line, (unsigned long)errCode);
-  if (logBufLen < 0) {
-    return OBC_GS_ERR_CODE_INVALID_ARG;
-  }
-  if ((uint32_t)logBufLen >= MAX_LOG_SIZE) {
-    return OBC_GS_ERR_CODE_BUFF_TOO_SMALL;
-  }
-
-  return writeToLogFile(logBuf, logBufLen);
+  return logMsg(msgLevel, file, line, errCodeStr);
 }
 
 obc_gs_error_code_t logMsg(log_level_t msgLevel, const char *file, uint32_t line, const char *msg) {
