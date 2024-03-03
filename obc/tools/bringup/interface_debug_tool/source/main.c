cabf762c5c56349228c072765ebc5bcbb330209c
Sardor Mirkomilov <89600047+sardormirk@users.noreply.github.com>
2024-03-02 20:42:36 -0500

    Sardor/temp sensor tests (#216)
    # Purpose
    
    Created a test for the temperature sensor to ensure it is working as
    intended
    
    # New Changes
    - Added temperature sensor test to tools/bringup/interface_debug_tool
    (test command, updated main to include temp test)
    
    # Testing
    - Will be flashed and tested next meeting
    
    # Outstanding Changes
    - Implement the rest of the tasks in debug tools, update makefile
    
    ---------
    
    Co-authored-by: Sardor Mirkomilov <89600047+voidblob@users.noreply.github.com>
    Co-authored-by: Navtaj Hundal <n2hundal@uwaterloo.ca>
    Co-authored-by: Navtajhundal <87790825+Navtajh04@users.noreply.github.com>

--- obc/tools/bringup/interface_debug_tool/source/main.c
+++ obc/tools/bringup/interface_debug_tool/source/main.c
@@ -13,7 +13,7 @@
 #include "test_can.h"
 #include "test_adc.h"
 #include "test_gio.h"
-
+#include "test_lm75bd.h"
 #include <FreeRTOS.h>
 #include <os_task.h>
 
@@ -34,15 +34,18 @@ typedef void (*testFunc_t)(void);
 #define NUM_COMMANDS_MAX 255  // Each command initiated by a single character
 
 static const testFunc_t testFuncs[NUM_COMMANDS_MAX] = {
-    [OP_CODE_SPI_TEST] = testSPI, [OP_CODE_SCI_TEST] = testSCI, [OP_CODE_I2C_TEST] = testI2C,
-    [OP_CODE_CAN_TEST] = testCAN, [OP_CODE_ADC_TEST] = testADC, [OP_CODE_GIO_TEST] = testGIO,
+    [OP_CODE_SPI_TEST] = testSPI,       [OP_CODE_SCI_TEST] = testSCI, [OP_CODE_I2C_TEST] = testI2C,
+    [OP_CODE_CAN_TEST] = testCAN,       [OP_CODE_ADC_TEST] = testADC, [OP_CODE_GIO_TEST] = testGIO,
+    [OP_CODE_LM75BD_TEST] = testLm75bd,
 };
 
 void utilityCLI(void *pvParameters) {
+  sciPrintf("Starting Bringup Utility...\r\n");
+
   while (1) {
     unsigned char cmdChar;
     sciPrintf("Enter a command: ");
-    sciReadBytes(&cmdChar, 1, portMAX_DELAY, pdMS_TO_TICKS(1000), UART_READ_REG);
+    sciReadBytes(&cmdChar, 1, portMAX_DELAY, portMAX_DELAY, UART_READ_REG);
     sciPrintf("\r\n");
 
     if (cmdChar >= NUM_COMMANDS_MAX) {
@@ -68,8 +71,6 @@ int main(void) {
   initSciPrint();
   initSpiMutex();
   initI2CMutex();
-
-  sciPrintf("Starting Bringup Utility...\r\n");
 
   xTaskCreateStatic(utilityCLI, "Bringup Utility", TASK_STACK_SIZE, NULL, 1, taskStack, &taskBuffer);
 
