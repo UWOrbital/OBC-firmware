dc9417ce164b5f24afc71666bcf480cc69480c7e
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-03-03 22:23:59 -0500

    Changes

--- obc/drivers/ina230/ina230.c
+++ obc/drivers/ina230/ina230.c
@@ -1,7 +1,6 @@
 #include "ina230.h"
 #include "obc_i2c_io.h"
 #include "tca6424.h"
-
 #include "obc_logging.h"
 
 #define INA230_I2C_ADDRESS_ONE 0b1000000U
@@ -27,10 +26,10 @@
 #define INA230_ALERT_LIMIT_REGISTER 0x07U
 
 #define INA230_EM_SOL_EVENT_EN_MASK (0x0001 << 15)
-#define INA230_EM_ALERT_LATCH_DISABLE_MASK (0x0000 << 0)
+#define INA230_EM_ALERT_LATCH_DISABLE_MASK (0x0000)
 
-#define INA230_CONFIG_MODE_MASK (0b111 << 0)
-#define INA2340_CONFIG_SHU_MASK (0b100 << 3)
+#define INA230_CONFIG_MODE_MASK (0b111)
+#define INA230_CONFIG_SHU_MASK (0b100 << 3)
 #define INA230_CONFIG_AVG_MASK (0b001 << 9)
 
 static obc_error_code_t writeINA230Register(uint8_t regAddress, uint8_t* data, uint8_t size, uint8_t i2cAddress);
@@ -40,7 +39,7 @@ obc_error_code_t initINA230Interface() {
   uint16_t maskEnableRegister = INA230_EM_ALERT_LATCH_DISABLE_MASK | INA230_EM_SOL_EVENT_EN_MASK;
   uint8_t maskEnRegisterUnpacked[] = {maskEnableRegister & 0xFF, (maskEnableRegister >> 8) & 0xFF};
 
-  uint16_t configRegister = INA230_CONFIG_MODE_MASK | INA2340_CONFIG_SHU_MASK | INA230_CONFIG_AVG_MASK;
+  uint16_t configRegister = INA230_CONFIG_MODE_MASK | INA230_CONFIG_SHU_MASK | INA230_CONFIG_AVG_MASK;
   uint8_t configRegisterUnpacked[] = {configRegister & 0xFF, (configRegister >> 8) & 0xFF};
 
   uint8_t shuntAlertINA1Unpacked[] = {MAX_VOLTAGE_LIMIT_INA1 & 0xFF, (MAX_VOLTAGE_LIMIT_INA1 >> 8) & 0xFF};
