673ec10dbb2bd382cef2f786451327dd9c2e5be0
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-03-12 22:19:26 -0400

    Added docs

--- interfaces/obc_gs_interface/commands/obc_gs_commands_response.h
+++ interfaces/obc_gs_interface/commands/obc_gs_commands_response.h
@@ -9,6 +9,20 @@
 #define CMD_RESPONSE_SUCCESS_MASK 0x01
 #define CMD_RESPONSE_MAX_PACKED_SIZE sizeof(cmd_unpacked_response_t)
 
+/*---------------------------------------------------------------------------*/
+/* GUIDE FOR ADDING A NEW COMMAND RESPONSE
+ * 1. Create new response struct
+ *     - Create a new struct such as obc_command_response_t below to define.
+ *        what the command response must look like. The command_response_error_code_t enum case
+ *        must be already defind for the error you are trying to implement.
+ *     - Add the command response struct to the union type of cmd_unpacked_response_t.
+ * 2. In both obc_gs_commands_response_unpack.c and obc_gs_commands_response_pack.c define the callback
+ *      - For each command callback you must define a callback fucntion that should be executed to pack the
+ *        struct that you declared in the union type of cmd_unpacked_response_t struct below.
+ *      - You must use the data_pack_unpack interface to avoid issues with endianess. The callback is executed after
+ *        the cmdId is passed from the cmd_unpacked_response_t struct.
+ *---------------------------------------------------------------------------*/
+
 typedef struct {
   float data1;
   uint32_t data2;
