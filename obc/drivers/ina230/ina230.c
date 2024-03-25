6be822cfda1e0292116f6608490cd1ae2b5c369c
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-03-24 23:46:36 -0400

    TODO: Implement disable

--- obc/drivers/ina230/ina230.c
+++ obc/drivers/ina230/ina230.c
@@ -17,62 +17,98 @@
 #define INA230_DISABLE_LOAD TCA6424A_GPIO_LOW
 #define INA230_ALERT_HIGH TCA6424A_GPIO_HIGH
 
-// ------------------  INA230 IC Related Configuration Values ------------- //
-#define MAX_VOLTAGE_LIMIT_INA1 0x7E000
-#define MAX_VOLTAGE_LIMIT_INA2 0x7E000
+// ------------------  INA230 IC Related General Configuration Addresses/Bitfields ------------- //
+#define INA230_CONFIG_REGISTER_ADDR 0x00U
+#define INA230_MASK_ENABLE_REGISTER_ADDR 0x06U
+#define INA230_ALERT_LIMIT_REGISTER_ADDR 0x07U
+#define INA230_CALIBRATION_REGISTER_ADDR 0x08U
 
-#define INA230_CONFIGURATION_REGISTER 0x00U
-#define INA230_MASK_ENABLE_REGISTER 0x06U
-#define INA230_ALERT_LIMIT_REGISTER 0x07U
+#define INA230_CONFIG_MODE_SHIFT 0U
+#define INA230_CONFIG_SHU_SHIFT 3U
+#define INA230_CONFIG_AVG_SHIFT 9U
+#define INA230_CONFIG_BUS_SHIFT 6U
 
-#define INA230_EM_SOL_EVENT_EN_MASK (0x0001 << 15)
-#define INA230_EM_ALERT_LATCH_DISABLE_MASK (0x0000)
+// ------------------  INA230 IC Configuration Masks/Flags ------------- //
+#define INA230_MASK_ENABLE_SHUNT_OVER_ALERT_MASK (0b1 << 15)
+#define INA230_MASK_ENABLE_SHUNT_UNDER_ALERT_MASK (0b1 << 14)
+#define INA230_MASK_ENABLE_BUS_OVER_ALERT_MASK (0b1 << 13)
+#define INA230_MASK_ENABLE_BUS_UNDER_ALERT_MASK (0b1 << 12)
+#define INA230_MASK_ENABLE_POWER_OVER_ALERT_MASK (0b1 << 11)
 
-#define INA230_CONFIG_MODE_MASK (0b111)
-#define INA230_CONFIG_SHU_MASK (0b100 << 3)
-#define INA230_CONFIG_AVG_MASK (0b001 << 9)
+#define INA230_MASK_ENABLE_TRANSPARENT_MODE_SET_MASK 1U
+
+// ------------------  INA230 Device Configuration ------------- //
+typedef struct {
+  uint8_t i2cDeviceAddress;
+
+  // TCA GPIO Expander ports
+  uint8_t tcaAlertPort;
+  uint8_t tcaEnablePort;
+
+  // Values for the configuration register
+  uint8_t configurationMode;
+  uint8_t configurationShunt;
+  uint8_t configurationAvg;
+  uint8_t configurationBus;
+
+  // Values for other registers
+  uint16_t maskEnableRegister;
+  uint16_t calibrationRegister;
+  uint16_t alertRegister;
+} ina230_config_t;
+
+static const ina230_config_t ina230Devices[] = {[INA230_DEVICE_ONE] = {.i2cDeviceAddress = INA230_I2C_ADDRESS_ONE,
+                                                                       .tcaAlertPort = INA230_ONE_ALERT_PIN,
+                                                                       .tcaEnablePort = INA230_ONE_ENABLE_PIN},
+
+                                                [INA230_DEVICE_TWO] = {.i2cDeviceAddress = INA230_I2C_ADDRESS_TWO,
+                                                                       .tcaAlertPort = INA230_TWO_ALERT_PIN,
+                                                                       .tcaEnablePort = INA230_TWO_ENABLE_PIN}};
 
 static obc_error_code_t writeINA230Register(uint8_t regAddress, uint8_t* data, uint8_t size, uint8_t i2cAddress);
 static obc_error_code_t initTca6424PinState();
 
-obc_error_code_t initINA230Interface() {
-  uint16_t maskEnableRegister = INA230_EM_ALERT_LATCH_DISABLE_MASK | INA230_EM_SOL_EVENT_EN_MASK;
-  uint8_t maskEnRegisterUnpacked[] = {maskEnableRegister & 0xFF, (maskEnableRegister >> 8) & 0xFF};
-
-  uint16_t configRegister = INA230_CONFIG_MODE_MASK | INA230_CONFIG_SHU_MASK | INA230_CONFIG_AVG_MASK;
-  uint8_t configRegisterUnpacked[] = {configRegister & 0xFF, (configRegister >> 8) & 0xFF};
-
-  uint8_t shuntAlertINA1Unpacked[] = {MAX_VOLTAGE_LIMIT_INA1 & 0xFF, (MAX_VOLTAGE_LIMIT_INA1 >> 8) & 0xFF};
-  uint8_t shuntAlertINA2Unpacked[] = {MAX_VOLTAGE_LIMIT_INA2 & 0xFF, (MAX_VOLTAGE_LIMIT_INA2 >> 8) & 0xFF};
-
+obc_error_code_t initINA230() {
   obc_error_code_t errCode;
-  RETURN_IF_ERROR_CODE(
-      writeINA230Register(INA230_MASK_ENABLE_REGISTER, &maskEnRegisterUnpacked[0], 2, INA230_I2C_ADDRESS_ONE));
-  RETURN_IF_ERROR_CODE(
-      writeINA230Register(INA230_MASK_ENABLE_REGISTER, &maskEnRegisterUnpacked[0], 2, INA230_I2C_ADDRESS_TWO));
+  for (uint8_t i = 0; i < INA230_DEVICE_COUNT; ++i) {
+    const ina230_config_t device = ina230Devices[i];
+    const uint16_t configurationRegister = (device.configurationMode << INA230_CONFIG_MODE_SHIFT) |
+                                           (device.configurationShunt << INA230_CONFIG_SHU_SHIFT) |
+                                           (device.configurationAvg << INA230_CONFIG_AVG_SHIFT) |
+                                           (device.configurationBus << INA230_CONFIG_BUS_SHIFT);
 
-  RETURN_IF_ERROR_CODE(
-      writeINA230Register(INA230_CONFIGURATION_REGISTER, &configRegisterUnpacked[0], 2, INA230_I2C_ADDRESS_ONE));
-  RETURN_IF_ERROR_CODE(
-      writeINA230Register(INA230_CONFIGURATION_REGISTER, &configRegisterUnpacked[0], 2, INA230_I2C_ADDRESS_TWO));
+    uint8_t configRegisterUnpacked[] = {configurationRegister & 0xFF, configurationRegister >> 8};
+    uint8_t maskEnRegisterUnpacked[] = {device.maskEnableRegister & 0xFF, device.maskEnableRegister >> 8};
+    uint8_t alertRegisterUnpacked[] = {device.alertRegister & 0xFF, device.alertRegister >> 8};
+    uint8_t calibrationRegisterUnpacked[] = {device.calibrationRegister & 0xFF, device.calibrationRegister >> 8};
 
-  RETURN_IF_ERROR_CODE(
-      writeINA230Register(INA230_ALERT_LIMIT_REGISTER, &shuntAlertINA1Unpacked[0], 2, INA230_I2C_ADDRESS_ONE));
-  RETURN_IF_ERROR_CODE(
-      writeINA230Register(INA230_ALERT_LIMIT_REGISTER, &shuntAlertINA2Unpacked[0], 2, INA230_I2C_ADDRESS_TWO));
+    uint8_t deviceAddress = device.i2cDeviceAddress;
+    RETURN_IF_ERROR_CODE(writeINA230Register(INA230_CONFIG_REGISTER_ADDR, configRegisterUnpacked,
+                                             sizeof(configRegisterUnpacked) / sizeof(configRegisterUnpacked[0]),
+                                             deviceAddress));
+    RETURN_IF_ERROR_CODE(writeINA230Register(INA230_MASK_ENABLE_REGISTER_ADDR, maskEnRegisterUnpacked,
+                                             sizeof(maskEnRegisterUnpacked) / sizeof(maskEnRegisterUnpacked[0]),
+                                             deviceAddress));
+    RETURN_IF_ERROR_CODE(writeINA230Register(INA230_ALERT_LIMIT_REGISTER_ADDR, alertRegisterUnpacked,
+                                             sizeof(alertRegisterUnpacked) / sizeof(alertRegisterUnpacked[0]),
+                                             deviceAddress));
+    RETURN_IF_ERROR_CODE(writeINA230Register(
+        INA230_CALIBRATION_REGISTER_ADDR, calibrationRegisterUnpacked,
+        sizeof(calibrationRegisterUnpacked) / sizeof(calibrationRegisterUnpacked[0]), deviceAddress));
+  }
 
   RETURN_IF_ERROR_CODE(initTca6424PinState());
   return OBC_ERR_CODE_SUCCESS;
 }
 
-obc_error_code_t readAndDisableIfAlert(ina230_device_t device) {
-  uint8_t pinLocation = (device == INA230_ONE) ? INA230_ONE_ALERT_PIN : INA230_TWO_ALERT_PIN;
+inline obc_error_code_t readAndDisableIfAlert(ina230_device_t device) {
+  uint8_t pinLocation = (device == INA230_DEVICE_ONE) ? INA230_ONE_ALERT_PIN : INA230_TWO_ALERT_PIN;
   uint8_t IOPortValue = 0;
   obc_error_code_t errCode;
   RETURN_IF_ERROR_CODE(readTCA6424APinInput(pinLocation, &IOPortValue));
 
   if (IOPortValue == INA230_ALERT_HIGH) {
-    pinLocation = (device == INA230_ONE) ? INA230_ONE_ENABLE_PIN : INA230_TWO_ENABLE_PIN,
+    pinLocation = ina230Devices[device].tcaEnablePort;
     IOPortValue = INA230_DISABLE_LOAD;
     RETURN_IF_ERROR_CODE(driveTCA6424APinOutput(pinLocation, IOPortValue));
   }
@@ -90,12 +126,11 @@ static obc_error_code_t writeINA230Register(uint8_t regAddress, uint8_t* data, u
 
 static obc_error_code_t initTca6424PinState() {
   obc_error_code_t errCode;
-  RETURN_IF_ERROR_CODE(configureTCA6424APin(INA230_ONE_ALERT_PIN, TCA6424A_GPIO_CONFIG_INPUT));
-  RETURN_IF_ERROR_CODE(configureTCA6424APin(INA230_TWO_ALERT_PIN, TCA6424A_GPIO_CONFIG_INPUT));
-  RETURN_IF_ERROR_CODE(configureTCA6424APin(INA230_ONE_ENABLE_PIN, TCA6424A_GPIO_CONFIG_OUTPUT));
-  RETURN_IF_ERROR_CODE(configureTCA6424APin(INA230_TWO_ENABLE_PIN, TCA6424A_GPIO_CONFIG_OUTPUT));
-
-  RETURN_IF_ERROR_CODE(driveTCA6424APinOutput(INA230_ONE_ENABLE_PIN, INA230_ENABLE_LOAD));
-  RETURN_IF_ERROR_CODE(driveTCA6424APinOutput(INA230_TWO_ENABLE_PIN, INA230_ENABLE_LOAD));
+  for (uint8_t i = 0; i < INA230_DEVICE_COUNT; ++i) {
+    ina230_config_t device = ina230Devices[i];
+    RETURN_IF_ERROR_CODE(configureTCA6424APin(device.tcaAlertPort, TCA6424A_GPIO_CONFIG_INPUT));
+    RETURN_IF_ERROR_CODE(configureTCA6424APin(device.tcaEnablePort, TCA6424A_GPIO_CONFIG_OUTPUT));
+    RETURN_IF_ERROR_CODE(driveTCA6424APinOutput(device.tcaEnablePort, INA230_ENABLE_LOAD));
+  }
   return OBC_ERR_CODE_SUCCESS;
 }
--- obc/drivers/ina230/ina230.h
+++ obc/drivers/ina230/ina230.h
@@ -2,7 +2,7 @@
 
 #include "obc_errors.h"
 
-typedef enum ina230_device { INA230_ONE, INA230_TWO } ina230_device_t;
+typedef enum { INA230_DEVICE_ONE = 0x00, INA230_DEVICE_TWO, INA230_DEVICE_COUNT } ina230_device_t;
 
-obc_error_code_t initINA230Interface();
+obc_error_code_t initINA230();
 obc_error_code_t readAndDisableIfAlert(ina230_device_t device);
