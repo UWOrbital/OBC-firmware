268df5a3160d73d6a1d6c0237a5b16b291dba535
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-03-30 16:08:15 -0400

    bl commit

--- obc/bl/source/bl_utils.c
+++ obc/bl/source/bl_utils.c
@@ -1,6 +1,27 @@
 #include "bl_utils.h"
 #include <stdint.h>
 
+#define DMA_COMPLETE 0xFF
+#define DMA_NOT_COMPLETE 0x00
+
+#define CRC_CONTROL_BASE_REGISTER 0xFE000000
+#define CRC_BUSY_FLAG_REGISTER (0xFE000000 | 0x38)
+#define CRC_PATTERN_COUNT_REGISTER (CRC_CONTROL_BASE_REGISTER | 0x40)
+#define CRC_SECTOR_COUNT_REGISTER (CRC_CONTROL_BASE_REGISTER | 0x44)
+
+#define CRC_SELECT_SIGNATURE_LOW_REGISTER (CRC_CONTROL_BASE_REGISTER | 0x70)
+#define CRC_SELECT_SIGNATURE_HIGH_REGISTER (CRC_CONTROL_BASE_REGISTER | 0x74)
+
+#define CRC_CRC_VALUE_LOW_REGISTER (CRC_CONTROL_BASE_REGISTER | 0x68)
+#define CRC_CRC_VALUE_HIGH_REGISTER (CRC_CONTROL_BASE_REGISTER | 0x6C)
+
+static volatile uint32_t* crcBusyFlagRegister = (void*)CRC_BUSY_FLAG_REGISTER;
+
+static uint8_t dmaCompleteFlag = DMA_NOT_COMPLETE;
+
+static inline void setDmaCompleteFlag();
+static inline void clearDmaCompleteFlag();
+
 uint32_t computeCRC32Checksum(const uint8_t* s, uint16_t n) {
   uint32_t crc = 0xFFFFFFFF;
 
@@ -16,3 +37,6 @@ uint32_t computeCRC32Checksum(const uint8_t* s, uint16_t n) {
 
   return ~crc;
 }
+
+static inline void setDmaCompleteFlag() { dmaCompleteFlag = DMA_COMPLETE; }
+static inline void clearDmaCompleteFlag() { dmaCompleteFlag = DMA_NOT_COMPLETE; }
