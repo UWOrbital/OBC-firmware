479fa2bf601eb4a39a1e2107638972351d2bfecd
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-04-13 18:33:27 -0400

    Modified bl_common

--- obc/bl/source/bl_common.c
+++ obc/bl/source/bl_common.c
@@ -6,7 +6,7 @@
 #include <stdint.h>
 
 bl_error_code_t verifyBinaryChecksum(app_header_t* header) {
-  crc_dma_request request = {0};
+  crc_dma_request_t request = {0};
   request.crcStartAddress = (uint32_t)APP_START_ADDRESS;
   request.crcElementCount = header->size;
   request.crcSeedValue = header->blChecksum.seed;
