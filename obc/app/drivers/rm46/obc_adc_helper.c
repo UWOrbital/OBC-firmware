aa194992d96d60ae80b45083ba157881d1602a44
kepler452b123 <adept5535@gmail.com>
2024-06-01 13:14:59 -0400

    .

--- obc/app/drivers/rm46/obc_adc_helper.c
+++ obc/app/drivers/rm46/obc_adc_helper.c
@@ -20,58 +20,6 @@ obc_error_code_t initADCMutex(void) {
   return OBC_ERR_CODE_SUCCESS;
 }
 
-obc_error_code_t adcGetSingleData(ADC_module_t adc, ADC_channel_t channel, ADC_group_t group, float *reading,
-                                  TickType_t blockTime) {
-  if (reading == NULL) {
-    return OBC_ERR_CODE_INVALID_ARG;
-  }
-
-  if (adcConversionMutex == NULL) {
-    return OBC_ERR_CODE_INVALID_STATE;
-  }
-
-  adcData_t adcData[MAXGROUPSIZE];
-
-  obc_error_code_t errCode;
-  RETURN_IF_ERROR_CODE(adcGetGroupReadings(adc, group, &adcData, blockTime));
-
-  uint32_t groupSize = adcGroupSize[(adc == ADC1) ? 0U : 1U][group];
-
-  for (uint32_t i = 0; i < groupSize; i++) {
-    if (adcData[i].id == channel) {
-      *reading =
-          (float)(adcData[i].value) * (REF_VOLTAGE_HIGH - REF_VOLTAGE_LOW) / (float)(1 << RESOLUTION) - REF_VOLTAGE_LOW;
-      return OBC_ERR_CODE_SUCCESS;
-    }
-  }
-
-  return OBC_ERR_CODE_ADC_INVALID_CHANNEL;
-}
-
-obc_error_code_t adcGetGroupData(ADC_module_t adc, ADC_group_t group, float *readings, TickType_t blockTime) {
-  if (readings == NULL) {
-    return OBC_ERR_CODE_INVALID_ARG;
-  }
-
-  if (adcConversionMutex == NULL) {
-    return OBC_ERR_CODE_INVALID_STATE;
-  }
-
-  adcData_t adcData[MAXGROUPSIZE];
-
-  obc_error_code_t errCode;
-  RETURN_IF_ERROR_CODE(adcGetGroupReadings(adc, group, &adcData, blockTime));
-
-  uint32_t groupSize = adcGroupSize[(adc == ADC1) ? 0U : 1U][group];
-
-  for (uint32_t i = 0; i < groupSize; i++) {
-    readings[i] =
-        (float)(adcData[i].value) * (REF_VOLTAGE_HIGH - REF_VOLTAGE_LOW) / (float)(1 << RESOLUTION) - REF_VOLTAGE_LOW;
-  }
-
-  return OBC_ERR_CODE_SUCCESS;
-}
-
 static obc_error_code_t adcGetGroupReadings(ADC_module_t adc, ADC_group_t group, adcData_t *data,
                                             TickType_t blockTime) {
   if (data == NULL) {
@@ -101,6 +49,58 @@ static obc_error_code_t adcGetGroupReadings(ADC_module_t adc, ADC_group_t group,
   adcGetData(adcReg, group, data);
 
   xSemaphoreGive(adcConversionMutex);
+
+  return OBC_ERR_CODE_SUCCESS;
+}
+
+obc_error_code_t adcGetSingleData(ADC_module_t adc, ADC_channel_t channel, ADC_group_t group, float *reading,
+                                  TickType_t blockTime) {
+  if (reading == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  if (adcConversionMutex == NULL) {
+    return OBC_ERR_CODE_INVALID_STATE;
+  }
+
+  adcData_t adcData[MAXGROUPSIZE];
+
+  obc_error_code_t errCode;
+  RETURN_IF_ERROR_CODE(adcGetGroupReadings(adc, group, adcData, blockTime));
+
+  uint32_t groupSize = adcGroupSize[(adc == ADC1) ? 0U : 1U][group];
+
+  for (uint32_t i = 0; i < groupSize; i++) {
+    if (adcData[i].id == channel) {
+      *reading =
+          (float)(adcData[i].value) * (REF_VOLTAGE_HIGH - REF_VOLTAGE_LOW) / (float)(1 << RESOLUTION) - REF_VOLTAGE_LOW;
+      return OBC_ERR_CODE_SUCCESS;
+    }
+  }
+
+  return OBC_ERR_CODE_ADC_INVALID_CHANNEL;
+}
+
+obc_error_code_t adcGetGroupData(ADC_module_t adc, ADC_group_t group, float *readings, TickType_t blockTime) {
+  if (readings == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
+  }
+
+  if (adcConversionMutex == NULL) {
+    return OBC_ERR_CODE_INVALID_STATE;
+  }
+
+  adcData_t adcData[MAXGROUPSIZE];
+
+  obc_error_code_t errCode;
+  RETURN_IF_ERROR_CODE(adcGetGroupReadings(adc, group, adcData, blockTime));
+
+  uint32_t groupSize = adcGroupSize[(adc == ADC1) ? 0U : 1U][group];
+
+  for (uint32_t i = 0; i < groupSize; i++) {
+    readings[i] =
+        (float)(adcData[i].value) * (REF_VOLTAGE_HIGH - REF_VOLTAGE_LOW) / (float)(1 << RESOLUTION) - REF_VOLTAGE_LOW;
+  }
 
   return OBC_ERR_CODE_SUCCESS;
 }
