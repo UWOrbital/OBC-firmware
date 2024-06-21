0030692a5a532a95e397191c94523a9e20dfc0da
Riya Patel <riyap7521@gmail.com>
2024-06-21 20:20:21 +0000

    fixes

--- interfaces/obc_gs_interface/aes128/obc_gs_aes128.c
+++ interfaces/obc_gs_interface/aes128/obc_gs_aes128.c
@@ -4,44 +4,14 @@
 #include <aes.h>
 
 #include "mbedtls/gcm.h"
-// docs:
+// library docs:
 // https://mbed-tls.readthedocs.io/projects/api/en/development/api/file/gcm_8h/#gcm_8h_1af264b64b26c4720188b530cfccddb4ef
 
 #include <stdint.h>
 #include <string.h>
 
 // Store the AES context
-// static struct AES_ctx ctx;
-static mbedtls_gcm_context gcm_ctx;
-
-// /**
-//  * @brief Decrypts the AES blocks
-//  *
-//  * @param aesData Pointer to an aes_data_t struct that includes a struct of the IV and data
-//  * @param output array to store the decrypted data
-//  * @param outputBufferLen length of the buffer to store the decrypted data
-//  *
-//  * @return obc_gs_error_code_t - whether or not the data was successfully decrypted
-//  */
-// obc_gs_error_code_t aes128Decrypt(aes_data_t *aesData, uint8_t *output, uint8_t outputBufferLen) {
-//   if (aesData == NULL) {
-//     return OBC_GS_ERR_CODE_INVALID_ARG;
-//   }
-
-//   if (output == NULL) {
-//     return OBC_GS_ERR_CODE_INVALID_ARG;
-//   }
-
-//   if (outputBufferLen != aesData->ciphertextLen) {
-//     return OBC_GS_ERR_CODE_INVALID_ARG;
-//   }
-
-//   memcpy(output, aesData->ciphertext, aesData->ciphertextLen);
-//   AES_ctx_set_iv(&ctx, aesData->iv);
-//   AES_CTR_xcrypt_buffer(&ctx, output, aesData->ciphertextLen);
-
-//   return OBC_GS_ERR_CODE_SUCCESS;
-// }
+static const mbedtls_gcm_context gcm_ctx;
 
 /**
  * @brief Performs authenticated decryption using AES in GCM mode.
@@ -61,51 +31,31 @@ static mbedtls_gcm_context gcm_ctx;
  * parameters.
  */
 
-// Authenticated decryption using GCM
-// authentication tag ensures data hasnt been tampered with
+// If the tag does not match the expected value (i.e., the value that would be computed from the ciphertext and the
+// key), the function will return an error code indicating that authentication failed
 obc_gs_error_code_t gcmDecrypt(aes_data_t *aesData, uint8_t *output, uint8_t outputBufferLen, const uint8_t *tag,
                                size_t tag_len) {
   if (aesData == NULL || output == NULL || tag == NULL) {
     return OBC_GS_ERR_CODE_INVALID_ARG;
   }
 
-  // if (outputBufferLen != aesData->ciphertextLen) {
-  // return OBC_GS_ERR_CODE_INVALID_ARG;
-  // }
+  if (outputBufferLen < aesData->ciphertextLen) {
+    return OBC_GS_ERR_CODE_INVALID_ARG;
+  }
 
-  int result = mbedtls_gcm_auth_decrypt(&gcm_ctx, aesData->ciphertextLen, aesData->iv, AES_IV_SIZE, NULL, 0, tag,
-                                        tag_len, aesData->ciphertext, output);
+  uint8_t result = mbedtls_gcm_auth_decrypt(&gcm_ctx, aesData->ciphertextLen, aesData->iv, AES_IV_SIZE, NULL, 0, tag,
+                                            tag_len, aesData->ciphertext, output);
   switch (result) {
-    // no errors
     case 0:
       return OBC_GS_ERR_CODE_SUCCESS;
-    // tag doesn't match
     case MBEDTLS_ERR_GCM_AUTH_FAILED:
       return OBC_GS_ERR_CODE_AUTH_FAILED;
-    // if the lengths or pointers are not valid or a cipher-specific error code if the decryption failed
     case MBEDTLS_ERR_GCM_BAD_INPUT:
       return OBC_GS_ERR_CODE_INVALID_ARG;
-    // add other errror cases if needed
     default:
       return OBC_GS_ERR_CODE_DEC_FAIL;
   }
 }
-
-// /**
-//  * @brief Initializes the AES context
-//  *
-//  * @param key - The key to decrypt the AES blocks with
-//  * @return obc_gs_error_code_t - whether or not the context was successfully initialized
-//  */
-// obc_gs_error_code_t initializeAesCtx(const uint8_t *key) {
-//   if (key == NULL) {
-//     return OBC_GS_ERR_CODE_INVALID_ARG;
-//   }
-
-//   AES_init_ctx(&ctx, key);
-
-//   return OBC_GS_ERR_CODE_SUCCESS;
-// }
 
 /**
  * @brief Initializes the GCM context with a given encryption key.
@@ -114,8 +64,7 @@ obc_gs_error_code_t gcmDecrypt(aes_data_t *aesData, uint8_t *output, uint8_t out
  * * @param key - The key to decrypt the AES blocks with (128 bit?)
  */
 
-// Initialize GCM context
 void initializeGcmCtx(const uint8_t *key) {
   mbedtls_gcm_init(&gcm_ctx);
-  mbedtls_gcm_setkey(&gcm_ctx, MBEDTLS_CIPHER_ID_AES, key, 128);  // Assuming 128-bit keys
+  mbedtls_gcm_setkey(&gcm_ctx, MBEDTLS_CIPHER_ID_AES, key, AES_KEY_BITS);
 }
