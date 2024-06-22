110385cc5a88ab2b14079bf7d5454074d0350189
kepler452b123 <adept5535@gmail.com>
2024-06-22 07:33:06 -0400

    Modified HAL so its same as origin/main

--- obc/shared/hal/launchpad/source/notification.c
+++ obc/shared/hal/launchpad/source/notification.c
@@ -50,7 +50,6 @@
 
 #include "esm.h"
 #include "sys_selftest.h"
-#include "adc.h"
 #include "can.h"
 #include "gio.h"
 #include "sci.h"
@@ -96,15 +95,6 @@ void memoryPort1TestFailNotification(uint32 groupSelect, uint32 dataSelect, uint
 }
 
 /* USER CODE BEGIN (8) */
-/* USER CODE END */
-void adcNotification(adcBASE_t *adc, uint32 group)
-{
-/*  enter user code between the USER CODE BEGIN and USER CODE END. */
-/* USER CODE BEGIN (11) */
-/* USER CODE END */
-}
-
-/* USER CODE BEGIN (12) */
 /* USER CODE END */
 void canErrorNotification(canBASE_t *node, uint32 notification)
 {
