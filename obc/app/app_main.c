81d12867ffa56b587bb30b2cb63120f0b70528fa
cathouwu <iamcatherinewho@gmail.com>
2024-06-23 00:38:26 -0400

    cleaned up stack overflow testing function

--- obc/app/app_main.c
+++ obc/app/app_main.c
@@ -51,18 +51,6 @@ static void __attribute__((no_stack_protector)) __construct_stk_chk_guard() {
   }
 }
 
-void stack_overflows_here();
-
-const char *buffer_long = "This is a long long string";
-
-void stack_overflows_here() {
-  char buffer_short[20];
-
-  strcpy(buffer_short, buffer_long);
-
-  printf("Overflow case run.");
-}
-
 int main(void) {
   // Run hardware initialization code
   gioInit();
