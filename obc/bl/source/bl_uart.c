9bcd32831a8753e097e24a2e30fe9e620f840d33
cathouwu <iamcatherinewho@gmail.com>
2024-06-28 18:32:22 -0400

    adressed comments

--- obc/bl/source/bl_uart.c
+++ obc/bl/source/bl_uart.c
@@ -1,14 +1,11 @@
 #include "bl_uart.h"
 
 #include <sci.h>
-
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 
 /* DEFINES */
-#define BL_UART_SCIREG_1_BAUD 115200U
-#define BL_UART_SCIREG_2_BAUD 115200U
 #define BL_UART_SCIREG_BAUD 115200U
 
 /* TYPEDEFS */
@@ -19,9 +16,7 @@ typedef struct {
 
 /* PRIVATE VARIABLES */
 static const bl_uart_reg_config_t bl_uart_reg_config[] = {
-    [BL_UART_SCIREG_1] = {BL_UART_SCIREG_1_BAUD, sciREG},
-    [BL_UART_SCIREG_2] = {BL_UART_SCIREG_2_BAUD, scilinREG},
-    [BL_UART_SCIREG] = {BL_UART_SCIREG_BAUD, sciREG},
+    [BL_UART_SCIREG] = {BL_UART_SCIREG_BAUD, UART_BL_REG},
 };
 
 /* PUBLIC FUNCTIONS */
