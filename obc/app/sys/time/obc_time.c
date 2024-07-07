74660128c07c2e9137ec1c4067453d5442940a83
Navtaj Hundal <n2hundal@uwaterloo.ca>
2024-07-06 21:58:08 -0700

    init

--- obc/app/sys/time/obc_time.c
+++ obc/app/sys/time/obc_time.c
@@ -15,6 +15,23 @@
 
 // Global Unix time
 static volatile uint32_t currTime;
+static volatile rtc_date_time_t currDataTime = {0};
+
+/**
+ * @brief Set the current unix time.
+ *
+ * @param unixTime The unix time to set.
+ * @warning This function should not be called from an ISR.
+ */
+static void setCurrentUnixTime(uint32_t unixTime);
+
+/**
+ * @brief Set the current data time.
+ *
+ * @param datetime The data time to set.
+ * @warning This function should not be called from an ISR.
+ */
+static void setCurrentDateTime(rtc_date_time_t datetime);
 
 obc_error_code_t initTime(void) {
   obc_error_code_t errCode;
@@ -59,9 +76,43 @@ uint32_t getCurrentUnixTime(void) {
   return time;
 }
 
+uint32_t getCurrentUnixTimeInISR(void) {
+  uint32_t time;
+
+  UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
+  time = currTime;
+  taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
+
+  return time;
+}
+
+rtc_date_time_t getCurrentDateTime(void) {
+  rtc_date_time_t dateTime = {0};
+  vPortEnterCritical();
+  dateTime = currDataTime;
+  vPortExitCritical();
+
+  return dateTime;
+}
+
+rtc_date_time_t getCurrentDateTimeinISR(void) {
+  rtc_date_time_t dateTime = {0};
+  UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
+  dateTime = currDataTime;
+  taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
+
+  return dateTime;
+}
+
 void setCurrentUnixTime(uint32_t unixTime) {
   vPortEnterCritical();
   currTime = unixTime;
+  vPortExitCritical();
+}
+
+void setCurrentDateTime(rtc_date_time_t datetime) {
+  vPortEnterCritical();
+  currDataTime = datetime;
   vPortExitCritical();
 }
 
@@ -78,6 +129,7 @@ obc_error_code_t syncUnixTime(void) {
   RETURN_IF_ERROR_CODE(datetimeToUnix(&datetime, &unixTime));
 
   setCurrentUnixTime(unixTime);
+  setCurrentDateTime(datetime);
 
   return OBC_ERR_CODE_SUCCESS;
 }
