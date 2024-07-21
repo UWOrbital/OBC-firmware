8b434ffc43ebd890f124c4405b4186114ad68a71
cathouwu <iamcatherinewho@gmail.com>
2024-07-21 14:36:54 -0400

    changed error code to common

--- obc/app/sys/obc_errors.h
+++ obc/app/sys/obc_errors.h
@@ -23,6 +23,7 @@ typedef enum {
   OBC_ERR_CODE_FAILED_UNPACK = 15,
   OBC_ERR_CODE_FAILED_PACK = 16,
   OBC_ERR_CODE_INVALID_STATE_TRANSITION = 17,
+  OBC_ERR_CODE_FAILED_STACK_CANARY = 18,
 
   /* Driver Errors 100 - 199*/
   OBC_ERR_CODE_SPI_FAILURE = 100,
@@ -188,6 +189,4 @@ typedef enum {
   /** Operation is not supported. */
   OBC_ERR_CODE_RED_ENOTSUPP = 1524,
 
-  /** Random stack canary failed to be created. */
-  OBC_ERR_CODE_FAILED_STACK_CANARY = 1525,
 } obc_error_code_t;
