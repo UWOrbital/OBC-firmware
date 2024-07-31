2a1db01c13f4a1fbb57617655f84919a4da244ca
Ameen Durani <uni.ameend@gmail.com>
2024-07-31 16:35:47 -0400

    Fixes: pre-commit

--- hil/examples/LogSink_Example.cpp
+++ hil/examples/LogSink_Example.cpp
@@ -11,65 +11,67 @@
 #define timeperiod 10
 
 TEST(LogIntegrationTest, Generated_Logs) {
-    // Did it Read?
-    std::ifstream read;
-    read.open(logFile);
-    ASSERT_TRUE(read.is_open());
-    char temp = '_';
-    read>>temp;
-    ASSERT_TRUE(temp != ' ' && temp != '\n');
-    read.close();
+  // Did it Read?
+  std::ifstream read;
+  read.open(logFile);
+  ASSERT_TRUE(read.is_open());
+  char temp = '_';
+  read >> temp;
+  ASSERT_TRUE(temp != ' ' && temp != '\n');
+  read.close();
 }
 
 TEST(LogIntegrationTest, Number_Logs) {
-    // Did it read expected times?
-    std::ifstream read;
-    read.open(logFile);
-    char buff[256];
-    int count;
-    while(read.getline(buff,256)){
-        count++;
-    }
-    ASSERT_TRUE(count>minLogsAquired) << "Required: "<<minLogsAquired<<" Iterated: "<<count;
-    read.close();
+  // Did it read expected times?
+  std::ifstream read;
+  read.open(logFile);
+  char buff[256];
+  int count;
+  while (read.getline(buff, 256)) {
+    count++;
+  }
+  ASSERT_TRUE(count > minLogsAquired) << "Required: " << minLogsAquired << " Iterated: " << count;
+  read.close();
 }
 
 TEST(LogIntegrationTest, Speific_Logs) {
-    // Did it read a specific log?
+  // Did it read a specific log?
 
-    std::ifstream read;
-    read.open(logFile);
-    char buffarr[256];
-    std::string buffstr;
-    int count=0;
-    bool found = false;
-    while(read.getline(buffarr,256)){
-        buffstr = buffarr;
+  std::ifstream read;
+  read.open(logFile);
+  char buffarr[256];
+  std::string buffstr;
+  int count = 0;
+  bool found = false;
+  while (read.getline(buffarr, 256)) {
+    buffstr = buffarr;
 
-        if(buffstr.find(specificLog) != std::string::npos){
-            found = true;
-            break;
-        }
-        count++;
+    if (buffstr.find(specificLog) != std::string::npos) {
+      found = true;
+      break;
     }
-    ASSERT_TRUE(found) <<"Iterated "<<count<<" times!";
-    read.close();
+    count++;
+  }
+  ASSERT_TRUE(found) << "Iterated " << count << " times!";
+  read.close();
 }
 
-TEST(LogIntegrationTest, TimeFrame_Logs){
-    //Did it read all logs within a specific time frame of eachother?
+TEST(LogIntegrationTest, TimeFrame_Logs) {
+  // Did it read all logs within a specific time frame of eachother?
 
-    std::ifstream read;
-    read.open(logFile);
-    char buffarr[256];
-    std::string buffstr, buffcurrent;
-    bool ok = true;
-    int index = 0, prevtime = 100;
-    while(read.getline(buffarr, 256)){
-        buffstr = buffarr;
-        buffcurrent = buffstr.substr(7, 2);
-        ASSERT_TRUE(std::stoi(buffcurrent) - prevtime < timeperiod) << "buffcurrent is "<<buffcurrent<<" and "<< std::stoi(buffcurrent)<<" prevtime is "<<prevtime<<" "<<std::stoi(buffcurrent) - prevtime<<" is >= than 10";
-        prevtime = stoi(buffcurrent);
-    }
-    read.close();
+  std::ifstream read;
+  read.open(logFile);
+  char buffarr[256];
+  std::string buffstr, buffcurrent;
+  bool ok = true;
+  int index = 0, prevtime = 100;
+  while (read.getline(buffarr, 256)) {
+    buffstr = buffarr;
+    buffcurrent = buffstr.substr(7, 2);
+    ASSERT_TRUE(std::stoi(buffcurrent) - prevtime < timeperiod)
+        << "buffcurrent is " << buffcurrent << " and " << std::stoi(buffcurrent) << " prevtime is " << prevtime << " "
+        << std::stoi(buffcurrent) - prevtime << " is >= than 10";
+    prevtime = stoi(buffcurrent);
+  }
+  read.close();
 }
