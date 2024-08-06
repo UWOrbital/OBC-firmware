e403b84c1d7460654857bec67165c470fa6b8b87
Ameen Durani <uni.ameend@gmail.com>
2024-08-06 08:55:24 -0400

    Added SnakeCase to globally defined constants.

--- hil/examples/LogSink_Example.cpp
+++ hil/examples/LogSink_Example.cpp
@@ -5,15 +5,15 @@
 #include <fstream>
 #include <string>
 
-#define minLogsAquired 7
-#define specificLog "Firmware/OBC-firmware/obc/app/drivers/sdcard/sdc_bdev.c:416 - 108"
-#define logFile "logsink.txt"
-#define timeperiod 10
+#define MIN_LOGS_AQUIRED 7
+#define SPECIFIC_LOG "Firmware/OBC-firmware/obc/app/drivers/sdcard/sdc_bdev.c:416 - 108"
+#define LOG_FILE "logsink.txt"
+#define TIME_PERIOD 10
 
 TEST(LogIntegrationTest, Generated_Logs) {
   // Did it Read?
   std::ifstream read;
-  read.open(logFile);
+  read.open(LOG_FILE);
   ASSERT_TRUE(read.is_open());
   char temp = '_';
   read >> temp;
@@ -24,13 +24,13 @@ TEST(LogIntegrationTest, Generated_Logs) {
 TEST(LogIntegrationTest, Number_Logs) {
   // Did it read expected times?
   std::ifstream read;
-  read.open(logFile);
+  read.open(LOG_FILE);
   char buff[256];
   int count;
   while (read.getline(buff, 256)) {
     count++;
   }
-  ASSERT_TRUE(count > minLogsAquired) << "Required: " << minLogsAquired << " Iterated: " << count;
+  ASSERT_TRUE(count > MIN_LOGS_AQUIRED) << "Required: " << MIN_LOGS_AQUIRED << " Iterated: " << count;
   read.close();
 }
 
@@ -38,7 +38,7 @@ TEST(LogIntegrationTest, Speific_Logs) {
   // Did it read a specific log?
 
   std::ifstream read;
-  read.open(logFile);
+  read.open(LOG_FILE);
   char buffarr[256];
   std::string buffstr;
   int count = 0;
@@ -46,7 +46,7 @@ TEST(LogIntegrationTest, Speific_Logs) {
   while (read.getline(buffarr, 256)) {
     buffstr = buffarr;
 
-    if (buffstr.find(specificLog) != std::string::npos) {
+    if (buffstr.find(SPECIFIC_LOG) != std::string::npos) {
       found = true;
       break;
     }
@@ -60,7 +60,7 @@ TEST(LogIntegrationTest, TimeFrame_Logs) {
   // Did it read all logs within a specific time frame of eachother?
 
   std::ifstream read;
-  read.open(logFile);
+  read.open(LOG_FILE);
   char buffarr[256];
   std::string buffstr, buffcurrent;
   bool ok = true;
@@ -68,7 +68,7 @@ TEST(LogIntegrationTest, TimeFrame_Logs) {
   while (read.getline(buffarr, 256)) {
     buffstr = buffarr;
     buffcurrent = buffstr.substr(7, 2);
-    ASSERT_TRUE(std::stoi(buffcurrent) - prevtime < timeperiod)
+    ASSERT_TRUE(std::stoi(buffcurrent) - prevtime < TIME_PERIOD)
         << "buffcurrent is " << buffcurrent << " and " << std::stoi(buffcurrent) << " prevtime is " << prevtime << " "
         << std::stoi(buffcurrent) - prevtime << " is >= than 10";
     prevtime = stoi(buffcurrent);
