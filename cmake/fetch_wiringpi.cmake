b920245649b9ec6e6975a35cbc7f97b9d79c6053
Ameen Durani <uni.ameend@gmail.com>
2024-08-12 09:31:00 -0400

    Modified workflow changes to custom fetch_wiringpi.cmake file

--- cmake/fetch_wiringpi.cmake
+++ cmake/fetch_wiringpi.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.15)
 
 # ---------------------------------------------------------------
-# Fetch GTest from github
+# Fetch WiringPi from github
 # ---------------------------------------------------------------
 include(FetchContent)
 
@@ -11,6 +11,17 @@ GIT_REPOSITORY https://github.com/WiringPi/WiringPi.git
 GIT_TAG        release-3.6
 )
 FetchContent_MakeAvailable(wiringpi)
-add_library(wiringpi INTERFACE IMPORTED)
-target_link_libraries(wiringpi INTERFACE IMPORTED)
+
+# ---------------------------------------------------------------
+# Manually building wiringPi
+# ---------------------------------------------------------------
+add_custom_target(
+    wiringpi-build
+    COMMAND ${CMAKE_COMMAND} -E chdir ${wiringpi_BINARY_DIR} make
+    WORKING_DIRECTORY ${wiringpi_SOURCE_DIR}
+)
+
+add_library(wiringpi INTERFACE)
+target_include_directories(wiringpi INTERFACE ${wiringpi_SOURCE_DIR})
+target_link_libraries(wiringpi INTERFACE ${wiringpi_BINARY_DIR}/wiringPi)
 # ---------------------------------------------------------------
