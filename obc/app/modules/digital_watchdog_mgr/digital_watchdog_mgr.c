171ab4ac54f7d9f9d9fbe1777bb2f87a2427cecd
Navtaj Hundal <n2hundal@uwaterloo.ca>
2024-08-23 23:38:26 -0700

    address comments

--- obc/app/modules/digital_watchdog_mgr/digital_watchdog_mgr.c
+++ obc/app/modules/digital_watchdog_mgr/digital_watchdog_mgr.c
@@ -10,6 +10,7 @@
 
 // Must feed the watchdog before the timeout period expires
 // This value should provide some margin
+// Should be atleast twice as fast as the fastest timeout
 #define FEEDING_PERIOD pdMS_TO_TICKS(50)
 
 /* Task timeouts for watchdog */
@@ -108,7 +109,7 @@ void obcTaskFunctionSwWatchdog(void *params) {
 
   // initialize all tasks as checked in
   for (uint8_t i = 0; i < OBC_SCHEDULER_TASK_COUNT; i++) {
-    watchdogTaskArray[i].taskLastCheckInTick = xTaskGetTickCount();
+    digitalWatchdogTaskCheckIn(i);
   }
 
   while (1) {
