22b1d402596ba0604f1ccb5aef85fd7534f7e5df
nameerahr <nameerahrazi@gmail.com>
2024-08-28 17:11:50 -0400

    Update include of new file from merge

--- obc/app/drivers/rm46/obc_exception_handlers.c
+++ obc/app/drivers/rm46/obc_exception_handlers.c
@@ -3,7 +3,7 @@
 
 #include "os_task.h"
 #include "obc_reset.h"
-#include "obc_logging.h"
+#include "logging.h"
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
   LOG_FATAL_FROM_ISR("***********************STACK OVERFLOW DETECTED!!!!!!***********************");
