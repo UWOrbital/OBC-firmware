bf8eb4b6a8cceeb32b1aac29f840b42af5904a4d
Navtajhundal <87790825+Navtajh04@users.noreply.github.com>
2024-09-07 11:17:13 -0700

    Add Timestamping to Logs (#299)
    # Purpose
    Explain the purpose of the PR here, including references to any existing
    Notion tasks.
    Add timestamps to logs so that when looking at the logs a while after
    they have been reported you can see when they were reported
    # New Changes
    - Explain new changes
    
    # Testing
    - Explain tests that you ran to verify code functionality.
    - Any functions that can be unit-tested should include a unit test in
    the PR. Otherwise, explain why it cannot be unit-tested.
    - Regression tested on launchpad, need someone on campus to test on rev1
    or some board with RTC
    # Outstanding Changes
    - If there are non-critical changes (i.e. additional features) that can
    be made to this feature in the future, indicate them here.
    - Current precision is 1s which is fine since our telem is only
    collected every min anyways. Once (if?) we have high freq logging we can
    look into ms precision if needed
    - GS tool to convert Unix time to datetime for logs received

--- obc/app/sys/time/obc_time.h
+++ obc/app/sys/time/obc_time.h
@@ -14,22 +14,43 @@ obc_error_code_t initTime(void);
  * @brief Get the current unix time.
  *
  * @return uint32_t The current unix time.
+ * @warning Do not call in an ISR
  */
 uint32_t getCurrentUnixTime(void);
 
 /**
- * @brief Set the current unix time.
+ * @brief Get the current unix time while in ISR.
  *
- * @param unixTime The unix time to set.
- * @warning This function should not be called from an ISR.
+ * @return uint32_t The current unix time.
  */
-void setCurrentUnixTime(uint32_t unixTime);
+uint32_t getCurrentUnixTimeInISR(void);
+
+/**
+ * @brief Get the current data time.
+ *
+ * @return rtc_date_time_t The current data time.
+ * @warning Do not call in an ISR
+ */
+rtc_date_time_t getCurrentDateTime(void);
+
+/**
+ * @brief Get the current data time while in ISR.
+ *
+ * @return rtc_date_time_t The current data time.
+ */
+rtc_date_time_t getCurrentDateTimeinISR(void);
 
 /**
  * @brief Increment the current unix time by 1 second.
  * @warning This function should not be called from an ISR.
  */
 void incrementCurrentUnixTime(void);
+
+/**
+ * @brief Increment the current date time by 1 second.
+ * @warning This function should not be called from an ISR.
+ */
+void incrementCurrentDateTime(void);
 
 /**
  * @brief Sync the unix time with the RTC.
