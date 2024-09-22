033d0655c4afc828da8d26b116a0255cd960d4e4
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2024-09-22 14:23:20 -0400

    Change the home page

--- gs/frontend/aro/src/home/Home.tsx
+++ gs/frontend/aro/src/home/Home.tsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+import "./Home.css";
+import HomeStatus from "./HomeStatus";
+
 function Home() {
 	return (
-		<>
-			<h1>Home</h1>
-		</>
+		<main className="home">
+			<h1>Welcome to the ARO Website by UWOrbital</h1>
+			<HomeStatus />
+			<div className="selection-buttons">
+				{/*TODO: Switch to using buttons and have animation or something nice*/}
+				<Link to={"new-request"} className="create-button">
+					Create New Request
+				</Link>
+				<Link to={"requests"} className="view-button">
+					View All Requests
+				</Link>
+			</div>
+		</main>
 	);
 }
 
