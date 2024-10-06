79f8a14c7a155b6b7296525d726286dd8dea9245
Kashif Bhuiyan <adept5535@gmail.com>
2024-10-05 22:53:58 -0400

    Revert "Create adc helper functions (#246)"
    This reverts commit 18e2ac280b634b728e83cf18d00539f1d7d89714.

--- obc/examples/test_app_adc/README.md
+++
@@ -1,7 +0,0 @@
-To test:
-
-Copy main.c into app/app_main.c
-Build firmware and flash to MCU
-Attach voltage to ADIN1 and ADIN2 (please note that ADIN1 and ADIN2 are the only active pins right now for testing, and both are in Group 1)
-Connect to serial port
-It should read the voltage and display on the console
--- obc/shared/hal/launchpad/include/adc.h
+++ obc/shared/hal/launchpad/include/adc.h
@@ -229,53 +229,7 @@ typedef struct adc_config_reg
     uint32 CONFIG_PARCR;
 }adc_config_reg_t;
 
-#define ADC1_OPMODECR_CONFIGVALUE		0x81140001U
-#define ADC1_CLOCKCR_CONFIGVALUE		(6U)
 
-#define ADC1_G0MODECR_CONFIGVALUE		((uint32)ADC_12_BIT | (uint32)0x00000000U | (uint32)0x00000000U)
-#define ADC1_G1MODECR_CONFIGVALUE		((uint32)ADC_12_BIT | (uint32)0x00000020U | (uint32)0x00000000U | (uint32)0x00000000U)
-#define ADC1_G2MODECR_CONFIGVALUE		((uint32)ADC_12_BIT | (uint32)0x00000020U | (uint32)0x00000000U | (uint32)0x00000000U)
-
-#define ADC1_G0SRC_CONFIGVALUE 			((uint32)0x00000000U | (uint32)ADC1_EVENT)
-#define ADC1_G1SRC_CONFIGVALUE 			((uint32)0x00000000U | (uint32)ADC1_EVENT)
-#define ADC1_G2SRC_CONFIGVALUE 			((uint32)0x00000000U | (uint32)ADC1_EVENT)
-
-#define ADC1_BNDCR_CONFIGVALUE			((uint32)((uint32)8U << 16U)|(8U + 8U))
-#define ADC1_BNDEND_CONFIGVALUE			(2U)
-
-#define ADC1_G0SAMP_CONFIGVALUE			(2U)
-#define ADC1_G1SAMP_CONFIGVALUE			(2U)
-#define ADC1_G2SAMP_CONFIGVALUE			(2U)
-
-#define ADC1_G0SAMPDISEN_CONFIGVALUE	((uint32)((uint32)0U << 8U) | 0x00000000U)
-#define ADC1_G1SAMPDISEN_CONFIGVALUE	((uint32)((uint32)0U << 8U) | 0x00000000U)
-#define ADC1_G2SAMPDISEN_CONFIGVALUE	((uint32)((uint32)0U << 8U) | 0x00000000U)
-
-#define ADC1_PARCR_CONFIGVALUE			(0x00000005U)
-
-#define ADC2_OPMODECR_CONFIGVALUE		0x81140001U
-#define ADC2_CLOCKCR_CONFIGVALUE		(6U)
-
-#define ADC2_G0MODECR_CONFIGVALUE		((uint32)ADC_12_BIT | (uint32)0x00000000U | (uint32)0x00000000U)
-#define ADC2_G1MODECR_CONFIGVALUE		((uint32)ADC_12_BIT | (uint32)0x00000020U | (uint32)0x00000000U | (uint32)0x00000000U)
-#define ADC2_G2MODECR_CONFIGVALUE		((uint32)ADC_12_BIT | (uint32)0x00000020U | (uint32)0x00000000U | (uint32)0x00000000U)
-
-#define ADC2_G0SRC_CONFIGVALUE 			((uint32)0x00000000U | (uint32)ADC2_EVENT)
-#define ADC2_G1SRC_CONFIGVALUE 			((uint32)0x00000000U | (uint32)ADC2_EVENT)
-#define ADC2_G2SRC_CONFIGVALUE 			((uint32)0x00000000U | (uint32)ADC2_EVENT)
-
-#define ADC2_BNDCR_CONFIGVALUE			((uint32)((uint32)8U << 16U)|(8U + 8U))
-#define ADC2_BNDEND_CONFIGVALUE			(2U)
-
-#define ADC2_G0SAMP_CONFIGVALUE			(2U)
-#define ADC2_G1SAMP_CONFIGVALUE			(2U)
-#define ADC2_G2SAMP_CONFIGVALUE			(2U)
-
-#define ADC2_G0SAMPDISEN_CONFIGVALUE	((uint32)((uint32)0U << 8U) | 0x00000000U)
-#define ADC2_G1SAMPDISEN_CONFIGVALUE	((uint32)((uint32)0U << 8U) | 0x00000000U)
-#define ADC2_G2SAMPDISEN_CONFIGVALUE	((uint32)((uint32)0U << 8U) | 0x00000000U)
-
-#define ADC2_PARCR_CONFIGVALUE			(0x00000005U)
 
 /** 
  *  @defgroup ADC ADC
@@ -307,8 +261,6 @@ uint32 adcMidPointCalibration(adcBASE_t *adc);
 void adcSetEVTPin(adcBASE_t *adc, uint32 value);
 uint32 adcGetEVTPin(adcBASE_t *adc);
 
-void adc1GetConfigValue(adc_config_reg_t *config_reg, config_value_type_t type);
-void adc2GetConfigValue(adc_config_reg_t *config_reg, config_value_type_t type);
 
 /** @fn void adcNotification(adcBASE_t *adc, uint32 group)
 *   @brief Group notification
