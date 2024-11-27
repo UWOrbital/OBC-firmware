2814b18568cdf410224deda19d1c1b47c849a8be
Kashif Bhuiyan <adept5535@gmail.com>
2024-11-27 02:07:59 -0500

    switched to memcpy

--- obc/app/sys/metadata/obc_metadata.c
+++ obc/app/sys/metadata/obc_metadata.c
@@ -14,13 +14,7 @@ obc_error_code_t readAppMetadata(app_metadata_t* metadata) {
 #if ENABLE_BL_BYPASS
   return OBC_ERR_CODE_NO_METADATA;
 #else
-  volatile app_metadata_t* flash_metadata = (volatile app_metadata_t*)(&__metadata_start__);
-
-  metadata->vers = flash_metadata->vers;
-  metadata->binSize = flash_metadata->binSize;
-  metadata->boardType = flash_metadata->boardType;
-  // If buffers are replaced with actual data, add more here
-
+  memcpy(metadata, &__metadata_start__, sizeof(app_metadata_t));
   return OBC_ERR_CODE_SUCCESS;
 #endif
 }
