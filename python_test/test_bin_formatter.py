aaa9f3e9703128fe6fa3d4fa216345e35dc8fe58
Kashif Bhuiyan <adept5535@gmail.com>
2025-01-07 16:13:46 -0500

    updated bin formatter test

--- python_test/test_bin_formatter.py
+++ python_test/test_bin_formatter.py
@@ -9,33 +9,33 @@ from obc.tools.python.bin_formatter import create_bin
     "input_name, metadata_input_name, bin_version, bin_data, cmake_data, expected_name, expected_header, expect_data",
     [
         (
-            "test1.bin",
+            "OBC-firmware.bin",
             "OBC-metadata.bin",
             0,
             b"0123456789",
-            # Simulate what the contents of the metadata.bin file would be as generated from top level CMake file
+            # Simulate what the contents of the metadata bin file would be as generated from top level CMake file
             b"1\n",
-            "test1_formatted.bin",
+            "OBC-firmware_formatted.bin",
             b"\x00\x00\x00\x00\n\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
             b"0123456789",
         ),  # \n has ASCII value of 10 so struct.pack uses \n
         (
-            "test2.bin",
+            "OBC-firmware.bin",
             "OBC-metadata.bin",
             2,
             b"10987",
             b"2\n",
-            "test2_formatted.bin",
+            "OBC-firmware_formatted.bin",
             b"\x02\x00\x00\x00\x05\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
             b"10987",
         ),
         (
-            "test3.bin",
+            "OBC-firmware.bin",
             "OBC-metadata.bin",
             3,
             b"1",
             b"0\n",
-            "test3_formatted.bin",
+            "OBC-firmware_formatted.bin",
             b"\x03\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
             b"1",
         ),
