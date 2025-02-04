911b56fbc4507a9b18cc85cde1496e127d1bc715
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2025-02-03 19:58:25 -0500

    Change to vitejs/plugin-react

--- gs/frontend/mcc/vite.config.ts
+++ gs/frontend/mcc/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import deno from "@deno/vite-plugin";
 
 // https://vitejs.dev/config/
