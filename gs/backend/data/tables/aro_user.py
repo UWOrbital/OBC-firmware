3d6979c1d04d15406bad6d8ed6472fbece63b974
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2025-02-12 20:33:15 -0500

    Fix MIN_EMAIL_LENGTH use

--- gs/backend/data/tables/aro_user.py
+++ gs/backend/data/tables/aro_user.py
@@ -4,7 +4,7 @@ from uuid import UUID, uuid4
 from pydantic import EmailStr
 from sqlmodel import Field
 
-from gs.backend.data.constants import CALL_SIGN_MAX_LENGTH, CALL_SIGN_MIN_LENGTH, DEFAULT_MAX_LENGTH, MIN_EMAIL_LENGTH
+from gs.backend.data.constants import CALL_SIGN_MAX_LENGTH, CALL_SIGN_MIN_LENGTH, DEFAULT_MAX_LENGTH, EMAIL_MIN_LENGTH
 from gs.backend.data.tables.base_model import BaseSQLModel
 
 ARO_USER_TABLE_NAME: Final[str] = "aro_users"
@@ -17,7 +17,7 @@ class AROUsers(BaseSQLModel, table=True):
 
     id: UUID = Field(default_factory=uuid4, primary_key=True, index=True)
     call_sign: str = Field(min_length=CALL_SIGN_MIN_LENGTH, max_length=CALL_SIGN_MAX_LENGTH)
-    email: EmailStr = Field(min_length=MIN_EMAIL_LENGTH, max_length=DEFAULT_MAX_LENGTH, unique=True)
+    email: EmailStr = Field(min_length=EMAIL_MIN_LENGTH, max_length=DEFAULT_MAX_LENGTH, unique=True)
     first_name: str = Field(max_length=DEFAULT_MAX_LENGTH)
     last_name: str | None = Field(max_length=DEFAULT_MAX_LENGTH, nullable=True)
     phone_number: str
