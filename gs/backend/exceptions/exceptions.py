7e8f237b85762afa531f4c257c49daba11ea08ae
FelixYang <116753515+FxJYg@users.noreply.github.com>
2025-02-23 12:37:45 -0500

    added @attribute for all exception classes

--- gs/backend/exceptions/exceptions.py
+++ gs/backend/exceptions/exceptions.py
@@ -12,6 +12,7 @@ class BaseOrbitalError(Exception):
 class ServiceError(BaseOrbitalError):
     """
     @brief Exception related to general service errors
+    @attribute message (str) - Service exception message
     """
 
     def __init__(self, message: str = "Service exception") -> None:
@@ -21,6 +22,7 @@ class ServiceError(BaseOrbitalError):
 class NotFoundError(ServiceError):
     """
     @brief Exception raised when an item is not found
+    @attribute message (str) - Item not found message
     """
 
     def __init__(self, message: str = "Item not found") -> None:
@@ -30,6 +32,7 @@ class NotFoundError(ServiceError):
 class InvalidArgumentError(BaseOrbitalError):
     """
     @brief Exception raised when an invalid argument is provided
+    @attribute message (str) - Invalid argument message
     """
 
     def __init__(self, message: str = "The argument provided was invalid") -> None:
@@ -39,6 +42,7 @@ class InvalidArgumentError(BaseOrbitalError):
 class InvalidStateError(BaseOrbitalError):
     """
     @brief Exception raised when an operation is performed in an invalid state
+    @attribute message (str) - Invalid state message
     """
 
     def __init__(self, message: str = "Invalid state") -> None:
@@ -48,6 +52,7 @@ class InvalidStateError(BaseOrbitalError):
 class DatabaseError(BaseOrbitalError):
     """
     @brief Exception raised for database-related errors
+    @attribute message (str) - Database error message
     """
 
     def __init__(self, message: str = "There was a database error") -> None:
@@ -57,6 +62,7 @@ class DatabaseError(BaseOrbitalError):
 class UnauthorizedError(BaseOrbitalError):
     """
     @brief Exception raised when a user is not authorized to perform an action
+    @attribute message (str) - Unauthorized error message
     """
 
     def __init__(self, message: str = "The current user is not authorized to perform this action") -> None:
@@ -66,6 +72,7 @@ class UnauthorizedError(BaseOrbitalError):
 class UnknownError(BaseOrbitalError):
     """
     @brief Exception raised for unexpected errors
+    @attribute message (str) - Unknown error message
     """
 
     def __init__(self, message: str = "An unexpected error occurred") -> None:
@@ -75,6 +82,7 @@ class UnknownError(BaseOrbitalError):
 class SunPositionError(BaseOrbitalError):
     """
     @brief Exception raised when an error occurs in sun position data generation
+    @attribute message (str) - Sun position error message
     """
 
     def __init__(self, message: str = "There was an error when trying to generate the sun position data") -> None:
