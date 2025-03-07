7e498b568cb43847dc61bdf06afad042696b07d3
Jiatao Geng <jiataogeng7@gmail.com>
2025-03-06 20:58:11 -0500

    make requested changes

--- gs/frontend/mcc/src/common/header.tsx
+++ gs/frontend/mcc/src/common/header.tsx
@@ -1,19 +1,18 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import "./header.css";
 
 function Header() {
   return (
-    <div>
-      <div className="nav">
-        <Link to="/">
-          <img src={logo} alt="orbital logo"></img>
-        </Link>
-        <div className="navmenu">
-          <Link to="/">Dashboard</Link>
-          <Link to="/mission-commands">Mission Commands</Link>
-          <Link to="/telemetry-data">Telemetry Data</Link>
-          <Link to="/aro-requests">ARO Requests</Link>
-        </div>
+    <div className="nav">
+      <Link to="/">
+        <img src={logo} alt="orbital logo"></img>
+      </Link>
+      <div className="navmenu">
+        <Link to="/">Dashboard</Link>
+        <Link to="/mission-commands">Mission Commands</Link>
+        <Link to="/telemetry-data">Telemetry Data</Link>
+        <Link to="/aro-requests">ARO Requests</Link>
       </div>
     </div>
   );
