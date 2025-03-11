88ea3d5665982c7e05d17d29e13d47877d56a8fa
Cameron Lee <cameronkmlee10@gmail.com>
2025-03-10 20:10:10 -0400

    Implement scheduler config generation

---
+++ obc/app/rtos/gen_scheduler_config.py
@@ -0,0 +1,50 @@
+from jinja2 import Environment, FileSystemLoader
+from toml import load
+
+
+class Task:
+    def __init__(self, stem: str, stack_size: str, priority: str):
+        self.stem = stem
+        self.stack_size = stack_size
+        self.priority = priority
+        self.camel_case = self.stem_to_camel(self.stem)
+
+        self.name = "\"" + stem + "\""
+        self.init = "obcTaskInit" + self.camel_case
+        self.function = "obcTaskFunction" + self.camel_case
+        self.stack = "obcTaskStack" + self.camel_case
+        self.buffer = "obcTaskBuffer" + self.camel_case
+        self.config_id = "OBC_SCHEDULER_CONFIG_ID_" + self.stem.upper()
+
+    def stem_to_camel(self, stem: str) -> str:
+        lst = stem.split("_")
+        return "".join(word.capitalize() for word in lst)
+
+
+task_list = []
+
+template_filename = "config_skeleton.c.jinja"
+config_filename = "scheduler_config.toml"
+output_filename = "obc_scheduler_config.c"
+
+env = Environment(loader=FileSystemLoader(""), trim_blocks=True, lstrip_blocks=True)
+template = env.get_template(template_filename)
+context = {
+    "tasks": task_list,
+}
+
+
+def main() -> None:
+    with open(config_filename, "r") as file:
+        toml_data = load(file)
+
+    for t in toml_data["tasks"]:
+        task_list.append(Task(t["stem"], t["stack_size"], t["priority"])) 
+    
+    with open(output_filename, mode="w") as output:
+        output.write(template.render(context))
+        print("Generated", output_filename)
+
+
+if __name__ == "__main__":
+    main()
