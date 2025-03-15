14fce99d4775650378fe36927c61197c1aa557d7
Jiatao Geng <jiataogeng7@gmail.com>
2025-03-15 18:08:16 -0400

    add map to aro website

--- gs/frontend/aro/src/new_request/new_request_form.tsx
+++ gs/frontend/aro/src/new_request/new_request_form.tsx
@@ -1,17 +1,34 @@
 import "./new_request_form.css";
-import { type ChangeEvent, useState } from "react";
+import { type ChangeEvent, useCallback, useEffect, useState } from "react";
+import {
+  APIProvider,
+  Map,
+  MapCameraChangedEvent,
+} from "@vis.gl/react-google-maps";
+
+const DEFAULT_ZOOM = 15;
 
 const NewRequestForm = () => {
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
+  const [mapLatitude, setMapLatitude] = useState(0);
+  const [mapLongitude, setMapLongitude] = useState(0);
+  const [zoom, setZoom] = useState(DEFAULT_ZOOM);
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-    // TODO: Show a map centered at latitude / longitude.
-  });
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      setMapLatitude(position.coords.latitude);
+      setMapLongitude(position.coords.longitude);
+      // TODO: Show a map centered at latitude / longitude.
+    });
+  }, []);
 
-  const handleSubmit = (_: unknown) => {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    updateMap();
+
     // TODO: Use the proper type for this
     const submission = {
       latitude,
@@ -21,6 +38,12 @@ const NewRequestForm = () => {
     console.log(submission);
 
     alert("Thanks for submitting!");
+  };
+
+  const updateMap = () => {
+    setMapLatitude(latitude);
+    setMapLongitude(longitude);
+    setZoom(DEFAULT_ZOOM);
   };
 
   const handleLatitudeChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -33,34 +56,60 @@ const NewRequestForm = () => {
     setLongitude(value);
   };
 
+  const handleCameraChange = useCallback((ev: MapCameraChangedEvent) => {
+    setMapLatitude(ev.detail.center.lat);
+    setMapLongitude(ev.detail.center.lng);
+    setZoom(ev.detail.zoom);
+  });
+
   // TODO: Add better error handling and switch to using react-hook-form
   return (
-    <form className="input-form" onSubmit={handleSubmit} id="main-form">
-      <div>
-        <label>Latitude:</label>
-        <input
-          required
-          type="number"
-          placeholder="Enter your coordinates"
-          value={latitude}
-          onChange={handleLatitudeChange}
-        />
+    <div className="new-request-page">
+      <form className="input-form" onSubmit={handleSubmit} id="main-form">
+        <div>
+          <label>Latitude:</label>
+          <input
+            required
+            type="number"
+            placeholder="Enter your coordinates"
+            value={latitude}
+            onChange={handleLatitudeChange}
+          />
+        </div>
+        <div>
+          <label>Longitude:</label>
+          <input
+            required
+            type="number"
+            placeholder="Enter your coordinates"
+            value={longitude}
+            onChange={handleLongitudeChange}
+          />
+        </div>
+        <div>
+          <input className="button" type="submit" />
+          <input
+            className="button"
+            type="button"
+            value="Update Map"
+            onClick={updateMap}
+          />
+        </div>
+      </form>
+      <div className="map">
+        <APIProvider
+          apiKey={"API_KEY"}
+          onLoad={() => console.log("Maps API has loaded.")}
+        >
+          <Map
+            zoom={zoom}
+            center={{ lat: mapLatitude, lng: mapLongitude }}
+            onCameraChanged={handleCameraChange}
+          >
+          </Map>
+        </APIProvider>
       </div>
-      <div>
-        <label>Longitude:</label>
-        <input
-          required
-          type="number"
-          placeholder="Enter your coordinates"
-          value={longitude}
-          onChange={handleLongitudeChange}
-        />
-      </div>
-      <div>
-        <input className="button" type="submit" />
-        <input className="button" type="submit" value="Update Map" />
-      </div>
-    </form>
+    </div>
   );
 };
 
