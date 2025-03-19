93a91e76de9a8ea85e8ba2de6ba093d9b66b84db
Suhas Unni <suhasunni6@gmail.com>
2025-03-19 17:19:06 -0400

    use pref_counter() and add uuid

--- gs/backend/api/middleware/logger_middleware.py
+++ gs/backend/api/middleware/logger_middleware.py
@@ -1,7 +1,8 @@
 from collections.abc import Awaitable, Callable, Sequence
 from datetime import datetime
 from sys import getsizeof
-from time import time
+from time import perf_counter
+from uuid import uuid4
 
 from fastapi import FastAPI, Request, Response
 from loguru import logger
@@ -22,9 +23,9 @@ class LoggerMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
         """Logs the request and response"""
         request_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        start_time = time()
+        start_time = perf_counter()
         response = await call_next(request)
-        process_time = time() - start_time
+        process_time = perf_counter() - start_time
 
         if request.url.path in self.excluded_endpoints:
             return response
@@ -34,16 +35,18 @@ class LoggerMiddleware(BaseHTTPMiddleware):
         # TODO: update this based on userID header name
         request_user_id = request.headers.get("user_id", "Anonymous")
         request_params = dict(request.query_params)
+        request_id = str(uuid4())
 
         logger.info(
             " | ".join(
                 [
                     f"REQUEST | Method: {request.method}",
+                    f"Request ID: {request_id}",
                     f"URL: {request.url.path}",
                     f"User id: {request_user_id}",
                     f"Params: {request_params}",
                     f"Time: {request_time}",
-                    f"Size: {request_size} bytes.",
+                    f"Bytes: {request_size}.",
                 ]
             )
         )
@@ -55,6 +58,7 @@ class LoggerMiddleware(BaseHTTPMiddleware):
         else:
             logger_severity = logger.info
 
+        # not all responses have a body_iterator attribute
         has_body_iterator = hasattr(response, "body_iterator")
 
         if has_body_iterator:
@@ -69,9 +73,10 @@ class LoggerMiddleware(BaseHTTPMiddleware):
             " | ".join(
                 [
                     f"RESPONSE | Status: {response.status_code}",
+                    f"Request ID: {request_id}",
                     f"Response: {response_body}",
-                    f"Size (bytes): {response_size}",
-                    f"Time Elasped: {process_time:.3f} seconds.",
+                    f"Bytes: {response_size}",
+                    f"Seconds Elasped: {process_time:.3f}.",
                 ]
             )
         )
@@ -83,5 +88,4 @@ class LoggerMiddleware(BaseHTTPMiddleware):
                 headers=dict(response.headers),
                 media_type=response.media_type,
             )
-        else:
-            return response
+        return response
