f929218eb5089fb352077843870c58a5468a630a
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2025-03-29 11:55:00 -0400

    Change stack size to ints and other minor changes

--- obc/app/rtos/gen_scheduler_config.py
+++ obc/app/rtos/gen_scheduler_config.py
@@ -1,9 +1,9 @@
 from re import match
+from typing import Final
 
 from jinja2 import Environment, FileSystemLoader
-from pydantic import BaseModel, NonNegativeInt, PositiveInt, ValidationError, field_validator
+from pydantic import BaseModel, PositiveInt, ValidationError, field_validator
 from toml import load
-from typing import Optional
 
 
 class Task(BaseModel):
@@ -18,70 +18,78 @@ class Task(BaseModel):
     @attribute conditional_enable (Optional[str]) - conditional macro; SNAKE_CASE
     @attribute task_init (bool) - whether task has init function
     """
+
     task_name: str
-    stack_size: str
+    stack_size: PositiveInt
     priority: str
     function_stem: str
     config_id_stem: str
-    conditional_enable: Optional[str] = None
+    conditional_enable: str | None = None
     task_init: bool = True
 
-    @field_validator('task_name', mode='after')
+    @field_validator("task_name", mode="after")
     @classmethod
     def is_snake_case(cls, name: str) -> str:
+        """
+        Checks if the string name is a snake_case string
+        """
         if match(r"^[a-z]+(_[a-z]+)*$", name):
             return name
         raise ValueError(f"{name} must be lower snake_case")
 
-    @field_validator('function_stem', mode='after')
+    @field_validator("function_stem", mode="after")
     @classmethod
     def is_camel_case(cls, name: str) -> str:
+        """
+        Checks if the string name is a camelCase string
+        """
         if match(r"^([A-Z][a-z]*)+$", name):
             return name
         raise ValueError(f"{name} must be upper CamelCase")
 
-    @field_validator('config_id_stem', 'conditional_enable', mode='after')
+    @field_validator("config_id_stem", "conditional_enable", mode="after")
     @classmethod
     def is_upper_snake_case(cls, name: str) -> str:
+        """
+        Checks if the string name is a SNAKE_CASE string
+        """
         if match(r"[A-Z]+(_[A-Z]+)*$", name):
             return name
         raise ValueError(f"{name} must be upper SNAKE_CASE")
 
 
-TASK_LIST: list[Task] = []
-
-C_TEMPLATE_FILENAME = "config_skeleton.c.jinja"
-H_TEMPLATE_FILENAME = "header_skeleton.h.jinja"
-CONFIG_FILENAME = "scheduler_config.toml"
-C_FILENAME = "obc_scheduler_config.c"
-H_FILENAME = "obc_scheduler_config.h"
+C_TEMPLATE_FILENAME: Final[str] = "config_skeleton.c.jinja"
+H_TEMPLATE_FILENAME: Final[str] = "header_skeleton.h.jinja"
+CONFIG_FILENAME: Final[str] = "scheduler_config.toml"
+C_FILENAME: Final[str] = "obc_scheduler_config.c"
+H_FILENAME: Final[str] = "obc_scheduler_config.h"
 
 
 def main() -> None:
     """Entry point to script"""
-    env = Environment(loader=FileSystemLoader(""), trim_blocks=True,
-                      lstrip_blocks=True)
+    env = Environment(loader=FileSystemLoader(""), trim_blocks=True, lstrip_blocks=True)
 
     c_template = env.get_template(C_TEMPLATE_FILENAME)
     h_template = env.get_template(H_TEMPLATE_FILENAME)
+    tasks = []
 
     context = {
-        "tasks": TASK_LIST,
+        "tasks": tasks,
     }
 
-    with open(CONFIG_FILENAME, "r") as config:
+    with open(CONFIG_FILENAME) as config:
         toml_data = load(config)
 
     for t in toml_data["tasks"]:
         try:
-            TASK_LIST.append(Task(**t))
+            tasks.append(Task(**t))
         except ValidationError as e:
             print(e)
-    
+
     with open(C_FILENAME, "w") as c_output:
         c_output.write(c_template.render(context))
         print(f"Generated new scheduler config at ./{C_FILENAME}")
-    
+
     with open(H_FILENAME, "w") as h_output:
         h_output.write(h_template.render(context))
         print(f"Generated new header file at ./{H_FILENAME}")
