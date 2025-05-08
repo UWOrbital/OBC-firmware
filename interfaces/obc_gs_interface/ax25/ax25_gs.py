cc1063020ace608d23c7ec2047ba5add3ecb40ad
Adityya Kaushal <adityyakaushal@gmail.com>
2025-05-08 16:17:09 -0400

    Updated code as per review

--- interfaces/obc_gs_interface/ax25/ax25_gs.py
+++ interfaces/obc_gs_interface/ax25/ax25_gs.py
@@ -1,5 +1,7 @@
-import ax25
-import binascii
+from binascii import crc_hqx
+
+from ax25 import Address, Control, Frame, FrameType
+
 
 class AX25:
     """
@@ -12,8 +14,15 @@ class AX25:
 
     def __init__(self) -> None:
         pass
-   
-    def ax25EncodeFrame(self, data_to_send: str, frame_type: ax25.FrameType, src_callsign: str = GROUND_STATION_CALLSIGN, dst_callsign: str = CUBE_SAT_CALLSIGN, ns: int = 0) -> bytes:
+
+    def encode_frame(
+        self,
+        data_to_send: str,
+        frame_type: FrameType,
+        src_callsign: str = GROUND_STATION_CALLSIGN,
+        dst_callsign: str = CUBE_SAT_CALLSIGN,
+        ns: int = 0,
+    ) -> bytes:
         """
         Encodes and Information Frame with the requested data using the ax25 library
 
@@ -23,15 +32,24 @@ class AX25:
         :param ns: Send Sequence Number
         :return: Generated Frame
         """
-        
+
         # Generate Frame Object as per Library Specfications
-        control_block = ax25.Control(frame_type, poll_final=False, send_seqno=ns)
-        src_address = ax25.Address(call=src_callsign, ssid=self._DEFAULT_SSID)
-        dst_address = ax25.Address(call=dst_callsign, ssid=self._DEFAULT_SSID)
-        frame_bytes = bytearray(ax25.Frame(dst=dst_address, src=src_address, via=None, control=control_block, pid=0, data=data_to_send.encode('utf-8')).pack())
-        
+        control_block = Control(frame_type, poll_final=False, send_seqno=ns)
+        src_address = Address(call=src_callsign, ssid=self._DEFAULT_SSID)
+        dst_address = Address(call=dst_callsign, ssid=self._DEFAULT_SSID)
+        frame_bytes = bytearray(
+            Frame(
+                dst=dst_address,
+                src=src_address,
+                via=None,
+                control=control_block,
+                pid=0,
+                data=data_to_send.encode("utf-8"),
+            ).pack()
+        )
+
         # Calculate fcs using CRC 16
-        fcs = bytearray(binascii.crc_hqx(frame_bytes, 0).to_bytes(2, 'big'))
+        fcs = bytearray(crc_hqx(frame_bytes, 0).to_bytes(2, "big"))
         # Define the flags
         flag = bytearray(bytes.fromhex("7E"))
         # Use the mutability of bytearrays to append everything into a huge bytearray that contains what we want to send
@@ -39,9 +57,9 @@ class AX25:
         # Convert the bytearray to bytes
         return_frame = bytes(frame_bytes)
 
-        return return_frame;
-    
-    def ax25DecodeFrame(self, data: bytes) -> ax25.Frame:
+        return return_frame
+
+    def decode_frame(self, data: bytes) -> Frame:
         """
         Encodes and Information Frame with the requested data using the ax25 library
 
@@ -52,23 +70,23 @@ class AX25:
         :return: True if the frame is successfully made otherwise False
         """
         # Extract the original 2 fcs bytes (16 bits)
-        fcs_original = int.from_bytes(data[-3:-1], byteorder='big', signed=True)
+        fcs_original = int.from_bytes(data[-3:-1], byteorder="big", signed=True)
 
         # Use splicing to get rid of the start and end byte and 2 fcs bytes
-        data  = data[1:-3]
+        data = data[1:-3]
 
         # Calculate fcs of recieved frame
-        fcs_data = binascii.crc_hqx(data, 0)
+        fcs_data = crc_hqx(data, 0)
 
-        # TODO: Error handle this properly
-        if(fcs_original != fcs_data):
-            print("Oh no")
+        if fcs_original != fcs_data:
+            raise Exception("The data has been corrupted (FCS sequences do not match)")
 
-        return ax25.Frame.unpack(data) 
+        return Frame.unpack(data)
 
 
 # Example Usage
-coder = AX25()
-frame = coder.ax25EncodeFrame("A", ax25.FrameType.I, AX25.CUBE_SAT_CALLSIGN, AX25.GROUND_STATION_CALLSIGN, 0)
-print(frame)
-print(coder.ax25DecodeFrame(frame).dst)
+if __name__ == "__main__":
+    coder = AX25()
+    frame = coder.encode_frame("A", FrameType.I, AX25.CUBE_SAT_CALLSIGN, AX25.GROUND_STATION_CALLSIGN, 0)
+    print(frame)
+    print(coder.decode_frame(frame).dst)
