601fb5b302258577a42dc826a6ac2817c21fc56f
Adityya Kaushal <adityyakaushal@gmail.com>
2025-05-08 11:41:33 -0400

    Implemented Encode and Decode for I frame with FCS as well as start and end flags

--- interfaces/obc_gs_interface/ax25/ax25_gs.py
+++ interfaces/obc_gs_interface/ax25/ax25_gs.py
@@ -1,5 +1,5 @@
 import ax25
-import sys
+import binascii
 
 class AX25:
     """
@@ -23,13 +23,23 @@ class AX25:
         :param ns: Send Sequence Number
         :return: True if the frame is successfully made otherwise False
         """
-
+        
+        # Generate Frame Object as per Library Specfications
         control_block = ax25.Control(frame_type=ax25.FrameType.I, poll_final=False, send_seqno=ns)
         src_address = ax25.Address(call=src_callsign, ssid=self._DEFAULT_SSID)
         dst_address = ax25.Address(call=dst_callsign, ssid=self._DEFAULT_SSID)
-        frame_bytes = ax25.Frame(dst=dst_address, src=src_address, via=None, control=control_block, pid=0, data=data_to_send.encode('utf-8')).pack()
+        frame_bytes = bytearray(ax25.Frame(dst=dst_address, src=src_address, via=None, control=control_block, pid=0, data=data_to_send.encode('utf-8')).pack())
         
-        return frame_bytes;
+        # Calculate fcs using CRC 16
+        fcs = bytearray(binascii.crc_hqx(frame_bytes, 0).to_bytes(2, 'big'))
+        # Define the flags
+        flag = bytearray(bytes.fromhex("7E"))
+        # Use the mutability of bytearrays to append everything into a huge bytearray that contains what we want to send
+        frame_bytes = flag + frame_bytes + fcs + flag
+        # Convert the bytearray to bytes
+        return_frame = bytes(frame_bytes)
+
+        return return_frame;
     
     def ax25DecodeIFrame(self, data: bytes) -> ax25.Frame:
         """
@@ -41,12 +51,24 @@ class AX25:
         :param ns: Send Sequence Number
         :return: True if the frame is successfully made otherwise False
         """
-        
-        return ax25.Frame.unpack(data)
+        # Extract the original 2 fcs bytes (16 bits)
+        fcs_original = int.from_bytes(data[-3:-1], byteorder='big', signed=True)
+
+        # Use splicing to get rid of the start and end byte and 2 fcs bytes
+        data  = data[1:-3]
+
+        # Calculate fcs of recieved frame
+        fcs_data = binascii.crc_hqx(data, 0)
+
+        # TODO: Error handle this properly
+        if(fcs_original != fcs_data):
+            print("Oh no")
+
+        return ax25.Frame.unpack(data)   
 
 
-
+# Example Usage
 coder = AX25()
-frame = coder.ax25EncodeIFrame("Hallo", AX25.CUBE_SAT_CALLSIGN, AX25.GROUND_STATION_CALLSIGN, 0)
+frame = coder.ax25EncodeIFrame("A", AX25.CUBE_SAT_CALLSIGN, AX25.GROUND_STATION_CALLSIGN, 0)
 print(frame)
 print(coder.ax25DecodeIFrame(frame).dst)
