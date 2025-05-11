023150d9b46f389fbd15f0ea5971ddd4962a9230
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2025-05-10 20:07:23 -0400

    fix encode seg faulting

--- interfaces/obc_gs_interface/fec/fec.py
+++ interfaces/obc_gs_interface/fec/fec.py
@@ -12,6 +12,10 @@ class PackedRsPacket(ctypes.Structure):
     _fields_ = [("data", ctypes.c_uint8 * 255)]
 
 
+fec.initRs.argtypes = ()
+fec.initRs.restype = None
+fec.initRs()  # TODO: Move this inside of a class that wraps fec
+
 # Let's define the functions from our fec file that we want to use here...
 # Arguments for rsEncode
 fec.rsEncode.argtypes = [ctypes.POINTER(ctypes.c_uint8), ctypes.POINTER(PackedRsPacket)]
@@ -24,7 +28,7 @@ class Vars:
 
     def __init__(self) -> None:
         self._struct_pointer = ctypes.pointer(PackedRsPacket((ctypes.c_uint8 * 255)()))
-        self._int_pointer = ctypes.pointer(ctypes.c_uint8(2))
+        self._int_pointer = ctypes.pointer(ctypes.c_uint8(255))
 
     def encode(self) -> None:
         """ """
@@ -33,4 +37,8 @@ class Vars:
 
 if __name__ == "__main__":
     variables = Vars()
+    for i in range(len(variables._struct_pointer.contents.data)):
+        print(variables._struct_pointer.contents.data[i], end=" ")
     print(variables.encode())
+    for i in range(len(variables._struct_pointer.contents.data)):
+        print(variables._struct_pointer.contents.data[i], end=" ")
