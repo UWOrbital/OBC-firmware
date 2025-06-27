c6855c2355c87dab3ed35a7a07edbf7ebbab1430
Andrew Wang <andrew_wang@AndrewZenbook14.>
2025-06-27 06:21:45 -0400

    format

--- obc/app/modules/gnc_mgr/gnc_manager.c
+++ obc/app/modules/gnc_mgr/gnc_manager.c
@@ -23,16 +23,16 @@ void obcTaskFunctionGncMgr(void *pvParameters) {
   /* Initialize the last wake time to the current time */
   xLastWakeTime = xTaskGetTickCount();
 
-  // Spam logs at startup
-  for (int i = 0; i < 6; i++) {
-    // LOG_DEBUG("BLOCK TEST LOG");
-    LOG_ERROR_CODE(1000 + i);
-    // printf("Finished log %d\n", i);
-  }
-
   /* Run GNC tasks periodically at 20 Hz */
   while (1) {
     digitalWatchdogTaskCheckIn(OBC_SCHEDULER_CONFIG_ID_GNC_MGR);
+
+    // Spam logs at startup
+    for (int i = 0; i < 6; i++) {
+      // LOG_DEBUG("BLOCK TEST LOG");
+      LOG_ERROR_CODE(1000 + i);
+      // printf("Finished log %d\n", i);
+    }
 
     /* This will automatically update the xLastWakeTime variable to be the last unblocked time */
     vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(GNC_TASK_PERIOD_MS));
