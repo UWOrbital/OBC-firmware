424c615ceab0f1ed17a3d8d7d1e38596377b845f
Adityya Kaushal <52387864+Adityya-K@users.noreply.github.com>
2025-07-09 15:46:04 -0400

    Improve App Transfer Protocol (#510)
    # Purpose
    This PR aims to approve the app_transfer protocol, integrating it with
    our existing command infrastructure. See #398 for more information.
    
    # New Changes
    - Created new command files in the `shared/` directory to process and
    handle time tagged as well as non time tagged commands
    - Moved the `command_callbacks` files into the `shared/` directory as
    well
    - Updated CMakeFiles to get the project to build properly for test and
    obc
    - Integrated bl_logging from #388 
    - Defined a new rtiGetCurrentTick() function (NOTE: This does not use
    compares)
    - Added a timeout to blUartReadBytes()
    - Changing main loop in `bl_main.c` to be command oriented
    - Adding new command callbacks
    
    # Testing
    - Ran exisiting testing and ensured every test was passing
    - Verified `CMD_SET_PROGRAMMING_SESSION` via a python script and UART
    - Verified `CMD_DOWNLOAD_DATA` via a python script and Uniflash's read
    memory function
    - Verified `CMD_ERASE_APP` via a python script and Uniflash's read
    memory function (memory was not readable after erase)
    - Added test functions for implemented commands:
    `CMD_SET_PROGRAMMING_SESSION`, `CMD_DOWNLOAD_DATA` and `CMD_ERASE_APP`
    in `test_command_pack_unpack.cpp`
    - Verified bootloader jumps to program after 5 seconds (which is the
    initial timeout that is currently set)
    - Verified that when you reset the board, you can reflash the program
    - Tested the time that it takes to flash via the script and it is 40
    seconds
    
    # Outstanding Changes
    - Try and increase baudrate to increase speed during flash

--- obc/bl/source/bl_uart.c
+++ obc/bl/source/bl_uart.c
@@ -1,6 +1,7 @@
 #include "bl_uart.h"
+#include "bl_time.h"
+#include "obc_errors.h"
 
-#include <sci.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -21,10 +22,26 @@ void blUartInit(void) {
   sciSetBaudrate(UART_BL_REG, BL_UART_SCIREG_BAUD);
 }
 
-void blUartReadBytes(uint8_t *buf, uint32_t numBytes) {
-  for (uint32_t i = 0U; i < numBytes; i++) {
-    buf[i] = (uint8_t)sciReceiveByte(UART_BL_REG);
+obc_error_code_t blUartReadBytes(uint8_t *buf, uint32_t numBytes, uint32_t timeout_ms) {
+  if (buf == NULL) {
+    return OBC_ERR_CODE_INVALID_ARG;
   }
+
+  uint32_t initTime = blGetCurrentTick();
+  do {
+    if (sciIsRxReady(UART_BL_REG) == SCI_RX_INT) {
+      for (uint32_t i = 0U; i < numBytes; i++) {
+        buf[i] = (uint8_t)sciReceiveByte(UART_BL_REG);
+        // TODO: Figure out why the board sometimes receives 0x00 as the first byte
+        if (i == 0 && buf[i] == 0) {
+          i -= 1;
+        }
+      }
+      return OBC_ERR_CODE_SUCCESS;
+    }
+  } while ((blGetCurrentTick() - initTime) < timeout_ms || blGetCurrentTick() < initTime);
+
+  return OBC_ERR_CODE_UART_FAILURE;
 }
 
 void blUartWriteBytes(uint32_t numBytes, uint8_t *buf) { sciSend(UART_BL_REG, numBytes, buf); }
