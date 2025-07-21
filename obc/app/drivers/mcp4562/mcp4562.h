9e1e9828ca3c3658cc2d79602800923cdfb0ed09
PencilKnot <wingchang.ca@gmail.com>
2025-07-20 22:47:55 -0400

    Made logic and stylistic fixes

--- obc/app/drivers/mcp4562/mcp4562.h
+++ obc/app/drivers/mcp4562/mcp4562.h
@@ -2,51 +2,75 @@
 
 #include "obc_errors.h"
 
+typedef enum {
+    VOLATILE_WIPER_0 = 0x0,
+    VOLATILE_WIPER_1 = 0x1
+} mcp4562_wiper_t;
+
 /**
- * @brief Write a 9-bit value to a specified register address
+ * @brief Allows mcp4562 to write data
  *
- * @return obc_error_code_t - whether or not the write was successful
+ * @param address   4 bit device memory address
+ * @param data   9 bit data value to write
+ *
+ * @return obc_error_code_t (success or failure)
  */
 obc_error_code_t mcp4562WriteData(uint8_t address, uint16_t data);
 
 /**
- * @brief Write multiple 9-bit values to the same register continuously
+ * @brief Allows mcp4562 to write data continuously
  *
- * @return obc_error_code_t - whether or not the continuous write was successful
+ * @param address   4 bit device memory address
+ * @param data   16 bit data array (every 2 bits contain 9 bits)
+ * @param length    length of data array
+ *
+ * @return obc_error_code_t (success or failure)
  */
 obc_error_code_t mcp4562WriteContData(uint8_t address, uint16_t data[], size_t length);
 
 /**
- * @brief Read the most recently addressed register
+ * @brief Allows mcp4562 to read data
  *
- * @return obc_error_code_t - whether or not the read was successful
+ * @param data   Pointer to 16 bit variable to store 9 bit data
+ *
+ * @return obc_error_code_t (success or failure)
  */
 obc_error_code_t mcp4562ReadData(uint16_t *data);
 
 /**
- * @brief Random read from a specific register
+ * @brief Allows mcp4562 to randomly read data sent
  *
- * @return obc_error_code_t - whether or not the random read was successful
+ * @param address   4 bit device memory address
+ * @param data   Pointer to 16 bit variable to store 9 bit data
+ *
+ * @return obc_error_code_t (success or failure)
  */
 obc_error_code_t mcp4562ReadRandomData(uint8_t address, uint16_t *data);
 
 /**
- * @brief Continuously read multiple 9-bit values from sequential memory locations
+ * @brief Allows mcp4562 to read data continuously
  *
- * @return obc_error_code_t - whether or not the continuous read was successful
+ * @param data   16 bit data array (every entry contain 9 bits)
+ * @param length    length of data array
+ *
+ * @return obc_error_code_t (success or failure)
  */
 obc_error_code_t mcp4562ReadContData(uint16_t data[], size_t length);
 
 /**
- * @brief Increment the wiper register value by 1
- * 
- * @return obc_error_code_t - whether or not the increment was successful
+ * @brief allows mcp4562 to increment wiper from n to n+1 (change resistance)
+ *
+ * @param address   4 bit device memory address
+ *
+ * @return obc_error_code_t (success or failure)
  */
 obc_error_code_t mcp4562IncrementWiper(uint8_t address);
 
 /**
- * @brief Decrement the wiper register value by 1
+ * @brief allows mcp4562 to decrement wiper from n to n-1 (change resistance)
  *
- * @return obc_error_code_t - whether or not the decrement was successful
+ * @param address   4 bit device memory address
+ *
+ * @return obc_error_code_t (success or failure)
  */
 obc_error_code_t mcp4562DecrementWiper(uint8_t address);
