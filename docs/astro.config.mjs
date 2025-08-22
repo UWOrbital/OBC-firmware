5f8fdc24f45202e67180ec8e798d86ff11ddf0bf
Adityya Kaushal <adityyakaushal@gmail.com>
2025-08-22 12:58:21 -0400

    Updated astro config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://uworbital.github.io',
-  base: '/docs',
 	integrations: [
 		starlight({
 			title: 'UW Orbital',
