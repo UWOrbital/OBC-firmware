e05aba48e77fc5c01f75a1d1e0137989fb898d1a
Adityya Kaushal <adityyakaushal@gmail.com>
2025-08-22 12:41:39 -0400

    Started docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'UW Orbital',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/UWOrbital/OBC-firmware' }],
+			sidebar: [
+				{
+					label: 'Getting Started',
+					autogenerate: { directory: 'getting-started' },
+				},
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+			],
+		}),
+	],
+});
