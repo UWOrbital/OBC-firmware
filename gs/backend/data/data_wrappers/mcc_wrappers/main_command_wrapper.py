d89a58ad4fab59fc92e24f755c739fc07283bfe4
Edison Wang <ew040607@gmail.com>
2025-09-11 19:56:01 -0400

    created POST and DELETE data wrappers for MainCommand

--- gs/backend/data/data_wrappers/mcc_wrappers/main_command_wrapper.py
+++ gs/backend/data/data_wrappers/mcc_wrappers/main_command_wrapper.py
@@ -6,8 +6,33 @@ from gs.backend.data.tables.main_tables import MainCommand
 
 def get_all_commands() -> list:
     """
-    @brief data wrapper for main_commands
+    @brief get all data wrapper for MainCommands
     """
     with get_db_session() as session:
         commands = session.exec(select(MainCommand)).all()
         return commands
+
+
+def create_command(command_data: dict) -> MainCommand:
+    """
+    @brief post data wrapper for MainCommands
+    """
+    with get_db_session() as session:
+        command = MainCommand(**command_data)
+        session.add(command)
+        session.commit()
+        session.refresh(command)
+        return command
+
+
+def delete_command_by_id(command_id: int) -> bool:
+    """
+    @brief delete data wrapper for MainCommands
+    """
+    with get_db_session() as session:
+        command = session.get(MainCommand, command_id)
+        if not command:
+            return False
+        session.delete(command)
+        session.commit()
+        return True
