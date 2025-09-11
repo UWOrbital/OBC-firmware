eb3da553d08ecb9f6353f3c917ae4ca9ccacabda
sunray4 <sunray4@users.noreply.github.com>
2025-09-11 19:34:01 -0400

    aro frontend migrated to nodejs

--- gs/frontend/aro/src/app.tsx
+++ gs/frontend/aro/src/app.tsx
@@ -1,10 +1,29 @@
-// import { useState } from "react";
-import "./App.css";
+import "./app.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home/home.tsx";
+import Navbar from "./common/navbar.tsx";
+import NotFound from "./common/not-found.tsx";
+import NewRequestForm from "./new-request/new-request-form.tsx";
+import Requests from "./requests/requests.tsx";
+import Profile from "./profile/profile.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/new-request" element={<NewRequestForm />} />
+          <Route path="/requests" element={<Requests />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
