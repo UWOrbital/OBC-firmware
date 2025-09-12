ae1b541cd3b3af8b8ed1b373d1ef32ad46134335
Edison Wang <ew040607@gmail.com>
2025-09-12 14:15:45 -0400

    created data wrappers for PacketTelemetry

---
+++ gs/backend/data/data_wrappers/mcc_wrappers/packet_telemetry_wrapper.py
@@ -0,0 +1,38 @@
+from sqlmodel import select
+
+from gs.backend.data.database.engine import get_db_session
+from gs.backend.data.tables.transactional_tables import PacketTelemetry
+
+
+def get_all_packet_telemetries() -> list:
+    """
+    @brief get all data wrapper for PacketTelemetry
+    """
+    with get_db_session() as session:
+        telemetries = session.exec(select(PacketTelemetry)).all()
+        return telemetries
+
+
+def create_packet_telemetry(telemetry_data: dict) -> PacketTelemetry:
+    """
+    @brief post data wrapper for PacketTelemetry
+    """
+    with get_db_session() as session:
+        telemetry = PacketTelemetry(**telemetry_data)
+        session.add(telemetry)
+        session.commit()
+        session.refresh(telemetry)
+        return telemetry
+
+
+def delete_packet_telemetry_by_id(telemetry_id: int) -> None:
+    """
+    @brief delete data wrapper for PacketTelemetry
+    """
+    with get_db_session() as session:
+        telemetry = session.get(PacketTelemetry, telemetry_id)
+        if not telemetry:
+            return False
+        session.delete(telemetry)
+        session.commit()
+        return True
