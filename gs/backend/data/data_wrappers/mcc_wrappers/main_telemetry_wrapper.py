8f539b64cee124cfa4449235e187c51eea98ab31
Edison Wang <ew040607@gmail.com>
2025-09-11 20:14:48 -0400

    created data wrappers for MainTelemetry

---
+++ gs/backend/data/data_wrappers/mcc_wrappers/main_telemetry_wrapper.py
@@ -0,0 +1,38 @@
+from sqlmodel import select
+
+from gs.backend.data.database.engine import get_db_session
+from gs.backend.data.tables.main_tables import MainTelemetry
+
+
+def get_all_telemetries() -> list:
+    """
+    @brief get all data wrapper for MainTelemetry
+    """
+    with get_db_session() as session:
+        telemetries = session.exec(select(MainTelemetry)).all()
+        return telemetries
+
+
+def create_telemetry(telemetry_data: dict) -> MainTelemetry:
+    """
+    @brief post data wrapper for MainTelemetry
+    """
+    with get_db_session() as session:
+        telemetry = MainTelemetry(**telemetry_data)
+        session.add(telemetry)
+        session.commit()
+        session.refresh(telemetry)
+        return telemetry
+
+
+def delete_telemetry_by_id(telemetry_id: int) -> None:
+    """
+    @brief delete data wrapper for MainTelemetry
+    """
+    with get_db_session() as session:
+        telemetry = session.get(MainTelemetry, telemetry_id)
+        if not telemetry:
+            return False
+        session.delete(telemetry)
+        session.commit()
+        return True
