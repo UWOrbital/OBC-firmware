354534e2cdcce1ff3438853b1b0e27231dadab16
Edison Wang <ew040607@gmail.com>
2025-09-12 14:13:51 -0400

    created data wrappers for Commands

---
+++ gs/backend/data/data_wrappers/mcc_wrappers/commands_wrapper.py
@@ -0,0 +1,38 @@
+from sqlmodel import select
+
+from gs.backend.data.database.engine import get_db_session
+from gs.backend.data.tables.transactional_tables import Commands
+
+
+def get_all_commands() -> list:
+    """
+    @brief get all data wrapper for Commands
+    """
+    with get_db_session() as session:
+        commands = session.exec(select(Commands)).all()
+        return commands
+
+
+def create_commands(command_data: dict) -> Commands:
+    """
+    @brief post data wrapper for Commands
+    """
+    with get_db_session() as session:
+        command = Commands(**command_data)
+        session.add(command)
+        session.commit()
+        session.refresh(command)
+        return command
+
+
+def delete_commands_by_id(command_id: int) -> None:
+    """
+    @brief delete data wrapper for Commands
+    """
+    with get_db_session() as session:
+        command = session.get(Commands, command_id)
+        if not command:
+            return False
+        session.delete(command)
+        session.commit()
+        return True
