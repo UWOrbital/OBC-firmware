3afd2fe52461578e7dbca4e8fab69e463e98eec2
Edison Wang <ew040607@gmail.com>
2025-09-12 16:24:46 -0400

    updated main commands endpoint

--- gs/backend/api/v1/mcc/endpoints/main_commands.py
+++ gs/backend/api/v1/mcc/endpoints/main_commands.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from gs.backend.api.v1.mcc.models.responses import MainCommandsResponse
-from gs.backend.data.data_wrappers.mcc_wrappers.main_command_wrapper import get_all_commands
+from gs.backend.data.data_wrappers.mcc_wrappers.main_command_wrapper import get_all_main_commands
 
 main_commands_router = APIRouter(tags=["MCC", "Main Commands"])
 
@@ -11,5 +11,5 @@ async def get_main_commands() -> MainCommandsResponse:
     """
     @brief Gets the main commands that are available for the MCC
     """
-    items = get_all_commands()
+    items = get_all_main_commands()
     return MainCommandsResponse(data=items)
