0efca0dd7a07bd7323198da00c7bd4204a91d7ca
ColeMeikle <cole.miekle@gmail.com>
2025-09-16 21:26:30 -0400

    Added react-query for fetches inside of useEffect

--- gs/frontend/mcc/src/aro-requests/aro-requests.tsx
+++ gs/frontend/mcc/src/aro-requests/aro-requests.tsx
@@ -1,5 +1,11 @@
 import Table from "react-bootstrap/Table";
-import { useEffect, useState } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 type AROItemProps = {
   id: number;
@@ -21,52 +27,52 @@ function AROItem({ id, latitude, longitude, status }: AROItemProps) {
 }
 
 function ARORequests() {
-  const [loading, setLoading] = useState(true);
-  const [aroRequests, setARORequests] = useState<AROItemProps[]>([]);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/aro-request")
-      .then((response) => response.json())
-      .then((data) => {
-        setARORequests(data);
-        setLoading(false);
-      });
-  }, []);
+  const { data, isLoading } = useQuery({
+    queryKey: ["aro-requests"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:5000/aro-request");
+      if(!response.ok) throw new Error("Network response was not ok");
+      return response.json();
+    }
+  });
 
   return (
-    <div className="arorequests layout">
-      <Table striped bordered hover variant="light">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Latitude</th>
-            <th>Longitude</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {loading
-            ? (
-              <tr>
-                <td colSpan={4}>Loading...</td>
-              </tr>
-            )
-            : (
-              aroRequests.map((item) => {
-                return (
-                  <AROItem
-                    key={item.id}
-                    id={item.id}
-                    longitude={item.longitude}
-                    latitude={item.latitude}
-                    status={item.status}
-                  />
-                );
-              })
-            )}
-        </tbody>
-      </Table>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="arorequests layout">
+        <Table striped bordered hover variant="light">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {isLoading
+              ? (
+                <tr>
+                  <td colSpan={4}>Loading...</td>
+                </tr>
+              )
+              : (
+                data.map((item: AROItemProps) => {
+                  return (
+                    <AROItem
+                      key={item.id}
+                      id={item.id}
+                      longitude={item.longitude}
+                      latitude={item.latitude}
+                      status={item.status}
+                    />
+                  );
+                })
+              )}
+          </tbody>
+        </Table>
+      </div>
+    </QueryClientProvider>
   );
 }
 
