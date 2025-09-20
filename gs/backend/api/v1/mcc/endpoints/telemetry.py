fe5159c04a3a59f96d68e06df675830cafae9847
Edison Wang <ew040607@gmail.com>
2025-09-20 13:07:05 -0400

    created telemetry endpoint will all optional params

--- gs/backend/api/v1/mcc/endpoints/telemetry.py
+++ gs/backend/api/v1/mcc/endpoints/telemetry.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+
+from gs.backend.data.data_wrappers.mcc_wrappers.telemetry_wrapper import get_all_telemetries
+from gs.backend.data.tables.transactional_tables import Telemetry
 
 telemetry_router = APIRouter(tags=["MCC", "Telemetry"])
+
+
+@telemetry_router.get("/")
+async def get_telemetry(
+    count: int | None = 100, offset: int | None = 0, filters: list[int] | None = Query(default=[])
+) -> list[Telemetry]:
+    """ """
+    telemetries = get_all_telemetries()
+    telemetries.reverse()
+    if filters:
+        telemetries = [obj for obj in telemetries if obj.type_ in filters]
+    telemetries = telemetries[offset : offset + count] if count > 0 else telemetries[offset:]
+    return telemetries
