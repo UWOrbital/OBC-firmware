00cae94f1b9b450a4ab16a825b5ecb9e129b43c4
sunray4 <sunray4@users.noreply.github.com>
2025-09-20 13:22:38 -0400

    fixed tests

--- gs/frontend/mcc/src/components/Background.test.tsx
+++ gs/frontend/mcc/src/components/Background.test.tsx
@@ -6,12 +6,12 @@ import Background from "./Background";
 describe("Background", () => {
   it("renders background image", () => {
     render(<Background />);
-    expect(screen.getByAltText("background image")).toBeInTheDocument();
+    expect(screen.getByAltText("background-image")).toBeInTheDocument();
   });
 
   it("has correct CSS classes", () => {
     render(<Background />);
-    const image = screen.getByAltText("background image");
+    const image = screen.getByAltText("background-image");
     expect(image).toHaveClass("opacity-40 bottom-0 -z-10");
   });
 });
