f9083bb5b46fc549119d721d00dbde4631ad50d2
Adityya Kaushal <adityyakaushal@gmail.com>
2025-09-19 21:31:15 -0400

    fix: pytests

--- python_test/test_mcc_command_wrapper.py
+++ python_test/test_mcc_command_wrapper.py
@@ -4,10 +4,8 @@ from gs.backend.data.database import engine as prod_engine
 from gs.backend.data.tables.main_tables import MAIN_SCHEMA_METADATA, MainCommand
 from sqlmodel import Session, SQLModel, create_engine
 
-# ---- Tests ----
 
-
-def test_create_main_command(session):
+def test_create_main_command(db_session):
     data = {"id": 1, "name": "Test Command", "params": None, "format": None, "data_size": 4, "total_size": 4}
     command = wrapper.create_main_command(data)
 
@@ -15,16 +13,16 @@ def test_create_main_command(session):
     assert command.name == "Test Command"
 
     # Verify it was actually inserted
-    stored = session.get(MainCommand, 1)
+    stored = db_session.get(MainCommand, 1)
     assert stored is not None
     assert stored.name == "Test Command"
 
 
-def test_get_all_main_commands(session):
+def test_get_all_main_commands(db_session):
     # Insert a couple of rows manually
-    session.add(MainCommand(id=1, name="CmdA", params=None, format=None, data_size=1, total_size=1))
-    session.add(MainCommand(id=2, name="CmdB", params="x", format="y", data_size=2, total_size=2))
-    session.commit()
+    db_session.add(MainCommand(id=1, name="CmdA", params=None, format=None, data_size=1, total_size=1))
+    db_session.add(MainCommand(id=2, name="CmdB", params="x", format="y", data_size=2, total_size=2))
+    db_session.commit()
 
     commands = wrapper.get_all_main_commands()
 
@@ -33,12 +31,12 @@ def test_get_all_main_commands(session):
     assert names == {"CmdA", "CmdB"}
 
 
-def test_delete_main_command_by_id(session):
+def test_delete_main_command_by_id(db_session):
     # Insert one command
-    session.add(MainCommand(id=1, name="CmdX", params=None, format=None, data_size=1, total_size=1))
-    session.commit()
+    db_session.add(MainCommand(id=1, name="CmdX", params=None, format=None, data_size=1, total_size=1))
+    db_session.commit()
 
     # Should delete successfully
     result = wrapper.delete_main_command_by_id(1)
     assert isinstance(result, MainCommand)
-    assert session.get(MainCommand, 1) is None
+    assert db_session.get(MainCommand, 1) is None
