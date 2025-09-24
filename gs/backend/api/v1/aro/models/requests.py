c947b906b31fc9c0233ee83b4df17b28e0693351
Zaid Abdulameer <zabdulam33r@gmail.com>
2025-09-24 16:06:08 -0400

    Fix errors

--- gs/backend/api/v1/aro/models/requests.py
+++ gs/backend/api/v1/aro/models/requests.py
@@ -1,27 +1,30 @@
-from pydantic import BaseModel
-
 from datetime import datetime
 from decimal import Decimal
 from uuid import UUID
 
-from gs.backend.data.tables.transactional_tables import ARORequest
+from pydantic import BaseModel
+
 from gs.backend.data.enums.aro_requests import ARORequestStatus
+
 
 class PictureRequests(BaseModel):
     """
     Request model for fetching picture requests.
-    
+
     Parameters:
         count (int): Optional, default 100. Gets the X most recent commands.
         offset (int): Optional, default 0. Starting point for paging.
     """
+
     count: int = 100
     offset: int = 0
+
 
 class CreatePictureRequest(BaseModel):
     """
     Request model for creating picture requests.
     """
+
     aro_id: UUID
     long: Decimal
     lat: Decimal
