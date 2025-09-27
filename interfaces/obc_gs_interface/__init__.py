a4a2b10b2c51faa26047f1b35662055fda63b78f
c4bae <123281198+c4bae@users.noreply.github.com>
2025-09-27 12:58:07 -0400

    implement tmux and split gs cli

--- interfaces/obc_gs_interface/__init__.py
+++ interfaces/obc_gs_interface/__init__.py
@@ -2,5 +2,5 @@ from ctypes import CDLL
 from pathlib import Path
 
 # The shared object file we are using the access the c functions via ctypes
-path = (Path(__file__).parent / "../../build_gs/interfaces/libobc-gs-interface.so").resolve()
+path = (Path(__file__).parent / "../../build_gs/interfaces/libobc-gs-interface.dylib").resolve()
 interface = CDLL(str(path))
