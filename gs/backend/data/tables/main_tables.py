97d7c521a9e8e1c949c4e56df1e450d0e8d2e90f
Hanson Wei <h57wei@uwaterloo.ca>
2025-09-29 23:48:34 -0400

    Added type annotation and docstring for MainCommand data validation

--- gs/backend/data/tables/main_tables.py
+++ gs/backend/data/tables/main_tables.py
@@ -42,8 +42,11 @@ class MainCommand(BaseSQLModel, table=True):
     __tablename__ = MAIN_COMMAND_TABLE_NAME
 
     @model_validator(mode="after")
-    def validate_params_format(self):
-
+    def validate_params_format(self) -> "MainCommand":
+        """
+        Returns self if params and format are both None or have the same number of comma-separated values.
+        If one of params or format is missing, or the numbers of comma-separated values do not match, raise DatabaseError.
+        """
         if self.format is None and self.params is None:
             return self
         
