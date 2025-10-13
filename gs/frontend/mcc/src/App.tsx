704214c199b1532ae22b6513d6fae4d876077756
Qinkai Li <q95li@uwaterloo.ca>
2025-10-13 15:58:49 -0400

    Added standardized toast functionality for MCC, not ARO yet.

--- gs/frontend/mcc/src/App.tsx
+++ gs/frontend/mcc/src/App.tsx
@@ -1,17 +1,20 @@
-import { Routes } from "react-router-dom";
 import Nav from "./components/Nav";
+import { Routes } from "react-router-dom";
 import Background from "./components/Background";
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/toasts.css';
 
 /**
  * @brief App component displaying the main application
  * @return tsx element of App component
  */
 function App() {
-  return (
+return (
     <>
-      <Nav />
-      <Background />
-      <Routes>
+    <Nav/>
+    <Routes>
         {/* uncomment these routes as the pages are created */}
         {/* <Route path="*" element={<Dashboard />} />
         <Route path="/" element={<Dashboard />} />
@@ -19,9 +22,11 @@ function App() {
         <Route path="/aro-admin" element={<AROAdmin />} />
         <Route path="/live-sessions" element={<LiveSessions />} />
         <Route path="/login" element={<Login />} /> */}
-      </Routes>
+    </Routes>
+    <Background/>
+    <ToastContainer />
     </>
-  );
+);
 }
 
 export default App;
