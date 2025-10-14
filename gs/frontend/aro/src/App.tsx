aaeacb1f7af3569332c309205e7838e6bc0be405
Qinkai Li <q95li@uwaterloo.ca>
2025-10-14 13:52:36 -0400

    Completed toast service for ARO.

--- gs/frontend/aro/src/App.tsx
+++ gs/frontend/aro/src/App.tsx
@@ -2,15 +2,28 @@ import { Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Background from "./components/Background";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/toasts.css';
+
+import toastService from './services/Toast.service'
+
 /**
  * @brief App component displaying the main application
  * @return tsx element of App component
  */
 function App() {
+const showSuccess = () => {
+    toastService.success("This is a success message for testing!");
+};
+
+const showError = () => {
+    toastService.error("This is an error message for testing.");
+};
   return (
     <>
-      <Nav />
-      <Background />
+      <Nav/>
+      <Background/>
       <Routes>
         {/* uncomment these routes as the pages are created */}
         {/* <Route path="*" element={<Dashboard />} />
@@ -21,6 +34,14 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} /> */}
       </Routes>
+      {/* --- TEST BUTTONS --- */}
+    <div className="w-1/4 flex flex-row mt-[10%] ms-[34.5%]">
+        <h3>Toast Test Controls</h3>
+        <button onClick={showSuccess} className="bg-lime-400 w-1/2 cursor-pointer">Success</button>
+        <button onClick={showError} className="bg-red-400 btn w-1/2 cursor-pointer">Error</button>
+    </div>
+    {/* --- END --- */}
+    <ToastContainer />
     </>
   );
 }
