effeb5f8b6fab630d2f484152f11c3ff728947e8
Brain256 <brian@BrianYoga7.localdomain>
2025-11-04 19:31:14 -0500

    add pydantic class for logger config

---
+++ gs/backend/config/logger_config.py
@@ -0,0 +1,14 @@
+from collections.abc import Sequence
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class LoggerConfig(BaseSettings):
+
+    """
+    Pydantic Class for storing logger middleware configuration settings
+    """
+
+    model_config = SettingsConfigDict(env_prefix="LOGGER_")
+
+    excluded_endpoints: Sequence[str] = []
