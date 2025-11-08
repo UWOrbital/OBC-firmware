ca20d1f880e2ccd98c73355b247c262092c58893
Brain256 <brian@BrianYoga7.localdomain>
2025-11-08 13:34:31 -0500

    update routes to login pages

--- gs/frontend/aro/src/App.tsx
+++ gs/frontend/aro/src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Background from "./components/Background";
 import NewRequestForm from "./components/new-request/new-request-form.tsx";
+import Login from "./components/auth/login.tsx"
+import Signup from "./components/auth/sign-up.tsx"
+import Verify from "./components/auth/verify.tsx"
 
 /**
  * @brief App component displaying the main application
@@ -19,9 +22,11 @@ function App() {
         <Route path="/commands" element={<Commands />} />
         <Route path="/new-request" element={<NewRequestForm />} />
         <Route path="/requests" element={<Requests />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} /> */}
-        {<Route path="/new-request" element={<NewRequestForm />} />}
+        <Route path="/profile" element={<Profile />} />*/}
+        <Route path="/login" element={<Login />} />
+        <Route path="/verify" element={<Verify />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/new-request" element={<NewRequestForm />} />
       </Routes>
     </>
   );
