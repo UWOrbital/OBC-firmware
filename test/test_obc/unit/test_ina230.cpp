0f451a631a9565ba949b92eb3b77d2e2c744e4b2
erinjhu <erinjhu@gmail.com>
2025-11-11 19:38:16 -0500

    group related tests

--- test/test_obc/unit/test_ina230.cpp
+++ test/test_obc/unit/test_ina230.cpp
@@ -4,7 +4,7 @@
 
 #include "obc_errors.h"
 #include "ina230.h"
-#include "mock_i2c_hal.h" // included to manually choose values mock i2c for different test cases
+#include "mock_i2c_hal.h" 
 #include <assert.h>
 #include <gtest/gtest.h>
 
@@ -21,89 +21,39 @@
 // --------- INIT TESTS ---------
 
 TEST(TestINA230, InitSuccess) {
-  obc_error_code_t err = initINA230();
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
+  EXPECT_EQ(initINA230(), OBC_ERR_CODE_SUCCESS);
 }
 
 TEST(TestINA230, ReadAndDisableIfAlert) {
   ina230_device_t device;
-  obc_error_code_t err = readAndDisableIfAlert(device);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
+  EXPECT_EQ(readAndDisableIfAlert(device), OBC_ERR_CODE_SUCCESS);
 }
 
 // --------- SHUNT VOLTAGE TESTS ---------
 
-TEST(TestINA230, ShuntVoltageMaxRegisterVal) {
-  // bus voltage register is 16-bits, so max value is 0xFFFF
-  // 0xFFFF = -1 since the register stores signed values
-  // since each bit is 2.5 μV, the voltage is -1 * 0.0000025 = -0.0000025 V
-  // in mock_i2c_hal.c, i2cReadReg() returns 0xFFFF
-  float voltage = 0;
-  setMockShuntVoltageValue(-0.0000025);
-  obc_error_code_t err = getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(voltage, -0.0000025); }
-
-TEST(TestINA230, ShuntVoltageMinRegisterVal) {
-  // bus voltage register is 16-bits, so min value is 0x0000
-  // in mock_i2c_hal.c, i2cReadReg() returns 0x0000
-  float voltage = 0;
-  setMockShuntVoltageValue(0);
-  obc_error_code_t err = getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(voltage, 0); }
-
-TEST(TestINA230, ShuntVoltageLowestVal) {
-  // bus voltage register is 16-bits, so min value is 0x8000
-  // use 2's complement on 0x8000 = 1000 0000 0000 0000 --> -32768
-  // since each bit is 2.5 μV, the  voltage is -32768 * 0.0000025 = -0.08192 V
-  // in mock_i2c_hal.c, i2cReadReg() returns 0x8000
-  float voltage = 0;
-  setMockShuntVoltageValue(-0.08192);
-  obc_error_code_t err = getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(voltage, -0.08192); }
-
-TEST(TestINA230, GetShuntVoltage_PositiveValue) {
-  float voltage = 0;
-  setMockShuntVoltageValue(0.08f);
-  obc_error_code_t err = getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
-  EXPECT_NEAR(voltage, 0.08f, INA230_TEST_FLOAT_TOLERANCE); 
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS); 
-}
-
-TEST(TestINA230, GetShuntVoltage_NullBufferPointer) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, NULL);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, ShuntVoltage_InvalidI2CAddress) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230ShuntVoltage(0b1010110, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, GetShuntVoltage_LastI2CAddress) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230ShuntVoltage(INA230_I2C_ADDRESS_TWO, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-}
-
-TEST(TestINA230, GetShuntVoltage_I2CAddressOutOfRange) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230ShuntVoltage(INA230_DEVICE_COUNT, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, ShuntVoltageInvalidDeviceIndex) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230ShuntVoltageForDevice(5, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, GetShuntVoltage_MultipleFunctionCalls) {
-  for (int i = 0; i < 5; ++i) {
+TEST(TestINA230, ShuntVoltageValues) {
+    float voltage = 0;
+    // Max register value: 0xFFFF
+    // since each bit is 2.5 μV, the voltage is -1 * 0.0000025 = -0.0000025 V
+    setMockShuntVoltageValue(-0.0000025);
+    EXPECT_EQ(getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage), OBC_ERR_CODE_SUCCESS);
+    EXPECT_FLOAT_EQ(voltage, -0.0000025);
+    // Min register value: 0x0000
+    setMockShuntVoltageValue(0);
+    EXPECT_EQ(getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage), OBC_ERR_CODE_SUCCESS);
+    EXPECT_FLOAT_EQ(voltage, 0);
+    // Lowest val: 0x8000
+    // use 2's complement on 0x8000 = 1000 0000 0000 0000 --> -32768
+    // since each bit is 2.5 μV, the  voltage is -32768 * 0.0000025 = -0.08192 V
+    setMockShuntVoltageValue(-0.08192);
+    EXPECT_EQ(getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage), OBC_ERR_CODE_SUCCESS);
+    EXPECT_FLOAT_EQ(voltage, -0.08192);
+    // Test positive value
+    setMockShuntVoltageValue(0.08f);
+    EXPECT_EQ(getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage), OBC_ERR_CODE_SUCCESS);
+    EXPECT_FLOAT_EQ(voltage, 0.08f);
+    // Multiple function calls
+    for (int i = 0; i < 5; ++i) {
     float voltage = 0;
     setMockShuntVoltageValue(0.05f);
     obc_error_code_t err = getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
@@ -112,167 +62,151 @@ TEST(TestINA230, GetShuntVoltage_MultipleFunctionCalls) {
   }
 }
 
-// --------- BUS VOLTAGE TESTS ---------
-
-
-TEST(TestINA230, GetBusVoltage_NullBufferPointer) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, NULL);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, GetBusVoltage_InvalidI2CAddress) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230BusVoltage(0b1010110, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, GetBusVoltage_LastI2CAddress) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230BusVoltage(INA230_I2C_ADDRESS_TWO, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-}
-
-TEST(TestINA230, GetBusVoltage_I2CAddressOutOfRange) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230BusVoltage(INA230_DEVICE_COUNT, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, BusVoltageInvalidDeviceIndex) {
-  float voltage = 0;
-  obc_error_code_t err = getINA230BusVoltageForDevice(5, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, GetBusVoltage_PositiveValue) {
-  float voltage = 0;
-  setMockBusVoltageValue(5.12f);
-  obc_error_code_t err = getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
-  EXPECT_FLOAT_EQ(voltage, 5.12f);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);  // 0x1000 = 4096, voltage = 4096 * 0.00125 = 5.12
-}
-
-TEST(TestINA230, GetBusVoltage_MultipleFunctionCalls) {
-  for (int i = 0; i < 5; ++i) {
+TEST(TestINA230, ShuntVoltageInvalidArguments ) {
     float voltage = 0;
+    EXPECT_EQ(getINA230ShuntVoltage(INA230_I2C_ADDRESS_ONE, NULL), OBC_ERR_CODE_INVALID_ARG);
+    EXPECT_EQ(getINA230ShuntVoltage(0b1010110, &voltage), OBC_ERR_CODE_INVALID_ARG);
+}
+
+TEST(TestINA230, GetShuntVoltage_I2CAddress) {
+  float voltage = 0;
+  EXPECT_EQ(getINA230ShuntVoltage(INA230_I2C_ADDRESS_TWO, &voltage), OBC_ERR_CODE_SUCCESS);
+  EXPECT_EQ(getINA230ShuntVoltage(INA230_DEVICE_COUNT, &voltage), OBC_ERR_CODE_INVALID_ARG);
+  EXPECT_EQ(getINA230ShuntVoltageForDevice(5, &voltage), OBC_ERR_CODE_INVALID_ARG);
+}
+
+// --------- BUS VOLTAGE TESTS ---------
+// bus voltage is unsigned
+TEST(TestINA230, BusVoltageValues) {
+    float voltage = 0;
+    // bus voltage register is 16-bits, so max value is 0xFFFF
+    // since each bit is 1.25 mV, the max voltage is 65535 * 0.00125 = 81.91875 V
+    setMockBusVoltageValue(81.91875f);
+    EXPECT_EQ(getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, &voltage), OBC_ERR_CODE_SUCCESS);
+    EXPECT_FLOAT_EQ(voltage, 81.91875f);
+    // Min register value: 0x0000
+    setMockBusVoltageValue(0);
+    EXPECT_EQ(getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, &voltage), OBC_ERR_CODE_SUCCESS);
+    EXPECT_FLOAT_EQ(voltage, 0);
+    // Test positive value 0x1000 = 4096, voltage = 4096 * 0.00125 = 5.12
     setMockBusVoltageValue(5.12f);
-    obc_error_code_t err = getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
-    EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
+    EXPECT_EQ(getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, &voltage), OBC_ERR_CODE_SUCCESS);
     EXPECT_FLOAT_EQ(voltage, 5.12f);
-  }
+    // Multiple function calls
+    for (int i = 0; i < 5; ++i) {
+      float voltage = 0;
+      setMockBusVoltageValue(5.12f);
+      obc_error_code_t err = getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
+      EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
+      EXPECT_FLOAT_EQ(voltage, 5.12f);
+    }
 }
 
-TEST(TestINA230, BusVoltageMaxRegisterVal) {
-  // bus voltage register is 16-bits, so max value is 0xFFFF
-  // since each bit is 1.25 mV, the max voltage is 65535 * 0.00125 = 81.91875 V
-  // in mock_i2c_hal.c, i2cReadReg() returns 0xFFFF
-  float voltage = 0;
-  setMockBusVoltageValue(81.91875f);
-  obc_error_code_t err = getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(voltage, 81.91875f);
+TEST(TestINA230, BusVoltageInvalidArguments ) {
+    float voltage = 0;
+    EXPECT_EQ(getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, NULL), OBC_ERR_CODE_INVALID_ARG);
+    EXPECT_EQ(getINA230BusVoltage(0b1010110, &voltage), OBC_ERR_CODE_INVALID_ARG);
 }
 
-TEST(TestINA230, BusVoltageMinRegisterVal) {
-  // bus voltage register is 16-bits, so min value is 0x0000
-  // in mock_i2c_hal.c, i2cReadReg() returns 0x0000
+TEST(TestINA230, BusVoltage_I2CAddress) {
   float voltage = 0;
-  setMockBusVoltageValue(0);
-  obc_error_code_t err = getINA230BusVoltage(INA230_I2C_ADDRESS_ONE, &voltage);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(voltage, 0); 
+  EXPECT_EQ(getINA230BusVoltage(INA230_I2C_ADDRESS_TWO, &voltage), OBC_ERR_CODE_SUCCESS);
+  EXPECT_EQ(getINA230BusVoltage(INA230_DEVICE_COUNT, &voltage), OBC_ERR_CODE_INVALID_ARG);
+  EXPECT_EQ(getINA230BusVoltageForDevice(5, &voltage), OBC_ERR_CODE_INVALID_ARG);
 }
+
+
 
 
 
 
 // --------- POWER TESTS ---------
-
-
-TEST(TestINA230, GetPower_NullBufferPointer) {
-  float power = 0;
-  obc_error_code_t err = getINA230Power(INA230_I2C_ADDRESS_ONE, NULL);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
+// power is unsigned
+TEST(TestINA230, PowerVoltageValues) {
+    float power = 0;
+    
+    // Max register value: 0xFFFF
+    // power LSB = 25 * current LSB = 25 * 0.001f = 0.025f
+    // since each bit is 2.5 mW, the max power is 65535 * 0.025 = 1638.375 W
+    setMockPowerValue(1638.375f);
+    EXPECT_EQ(getINA230Power(INA230_I2C_ADDRESS_ONE, &power), OBC_ERR_CODE_SUCCESS);
+    EXPECT_FLOAT_EQ(power, 1638.375f);
+    // Min register value: 0x0000
+    setMockPowerValue(0);
+    EXPECT_EQ(getINA230Power(INA230_I2C_ADDRESS_ONE, &power), OBC_ERR_CODE_SUCCESS);
+    EXPECT_FLOAT_EQ(power, 0);
+    // Test positive value
+    setMockPowerValue(5.12f);
+    EXPECT_EQ(getINA230Power(INA230_I2C_ADDRESS_ONE, &power), OBC_ERR_CODE_SUCCESS);
+    EXPECT_NEAR(power, 5.12f, INA230_TEST_FLOAT_TOLERANCE);
+    // Multiple function calls
+    for (int i = 0; i < 5; ++i) {
+      float power = 0;
+      setMockPowerValue(0.05f);
+      obc_error_code_t err = getINA230Power(INA230_I2C_ADDRESS_ONE, &power);
+      EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
+      EXPECT_NEAR(power, 0.05f, INA230_TEST_FLOAT_TOLERANCE); 
+    }
 }
 
-TEST(TestINA230, GetPower_InvalidI2CAddress) {
-  float power = 0;
-  obc_error_code_t err = getINA230Power(0b1010110, &power);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
+TEST(TestINA230, PowerInvalidArguments ) {
+    float power = 0;
+    EXPECT_EQ(getINA230Power(INA230_I2C_ADDRESS_ONE, NULL), OBC_ERR_CODE_INVALID_ARG);
+    EXPECT_EQ(getINA230Power(0b1010110, &power), OBC_ERR_CODE_INVALID_ARG);
 }
 
-TEST(TestINA230, PowerMaxRegisterVal) {
-  // bus voltage register is 16-bits, so max value is 0xFFFF (unsigned)
-  // power LSB = 25 * current LSB = 25 * 0.001f = 0.025f
-  // since each bit is 2.5 mW, the max power is 65535 * 0.025 = 1638.375 W
-  // in mock_i2c_hal.c, i2cReadReg() returns 0xFFFF (power is unsigned)
+TEST(TestINA230, Power_I2CAddress) {
   float power = 0;
-  setMockPowerValue(1638.375f);
-  obc_error_code_t err = getINA230Power(INA230_I2C_ADDRESS_ONE, &power);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(power, 1638.375f);
+  EXPECT_EQ(getINA230Power(INA230_I2C_ADDRESS_TWO, &power), OBC_ERR_CODE_SUCCESS);
+  EXPECT_EQ(getINA230Power(INA230_DEVICE_COUNT, &power), OBC_ERR_CODE_INVALID_ARG);
+  EXPECT_EQ(getINA230PowerForDevice(5, &power), OBC_ERR_CODE_INVALID_ARG);
 }
 
-TEST(TestINA230, GetPower_PositiveValue) {
-  float power = 0;
-  setMockPowerValue(5.12f);
-  obc_error_code_t err = getINA230Power(INA230_I2C_ADDRESS_ONE, &power);
-  EXPECT_NEAR(power, 5.12f, INA230_TEST_FLOAT_TOLERANCE);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);  
-}
 
 // --------- CURRENT TESTS ---------
 
-
-TEST(TestINA230, GetCurrent_NullBufferPointer) {
-  float power = 0;
-  obc_error_code_t err = getINA230Current(INA230_I2C_ADDRESS_ONE, NULL);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, GetCurrent_InvalidI2CAddress) {
-  float power = 0;
-  obc_error_code_t err = getINA230Current(0b1010110, &power);
-  EXPECT_EQ(err, OBC_ERR_CODE_INVALID_ARG);
-}
-
-TEST(TestINA230, CurrentMultipleCalls) {
-  float power = 0;
-  obc_error_code_t err;
-  for (int i = 0; i < 4; ++i) {
-    err = getINA230CurrentForDevice(0, &power);
+TEST(TestINA230, CurrentValues) {
+  float current = 0;
+  // Max register value: 0xFFFF
+  // since each bit is 1 mA, -1 * 0.001 = -0.001
+  setMockCurrentValue(-0.001);
+  EXPECT_EQ(getINA230Current(INA230_I2C_ADDRESS_ONE, &current), OBC_ERR_CODE_SUCCESS);
+  EXPECT_FLOAT_EQ(current, -0.001);
+  // Min register value: 0x0000
+  setMockCurrentValue(0);
+  EXPECT_EQ(getINA230Current(INA230_I2C_ADDRESS_ONE, &current), OBC_ERR_CODE_SUCCESS);
+  EXPECT_FLOAT_EQ(current, 0);
+  // Lowest register value: 0x8000
+  // use 2's complement on 0x8000 = 1000 0000 0000 0000 --> -32768
+  // since each bit is 1 mA, -32768 * 0.001 = -32.768
+  setMockCurrentValue(-32.768);
+  EXPECT_EQ(getINA230Current(INA230_I2C_ADDRESS_ONE, &current), OBC_ERR_CODE_SUCCESS);
+  EXPECT_FLOAT_EQ(current, -32.768);
+  // Test positive value
+  setMockCurrentValue(5.12f);
+  EXPECT_EQ(getINA230Current(INA230_I2C_ADDRESS_ONE, &current), OBC_ERR_CODE_SUCCESS);
+  EXPECT_NEAR(current, 5.12f, INA230_TEST_FLOAT_TOLERANCE);
+  // Multiple function calls
+  for (int i = 0; i < 5; ++i) {
+    float current = 0;
+    setMockCurrentValue(0.05f);
+    obc_error_code_t err = getINA230Current(INA230_I2C_ADDRESS_ONE, &current);
     EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
+    EXPECT_NEAR(current, 0.05f, INA230_TEST_FLOAT_TOLERANCE); 
   }
 }
 
-TEST(TestINA230, CurrentMaxRegisterVal) {
-  // current register is 16-bits, so max value is 0xFFFF
-  // 0xFFFF = -1 since the current register holds signed values
-  // since each bit is 1 mA, -1 * 0.001 = -0.001
-  // in mock_i2c_hal.c, i2cReadReg() returns -1
-  float current = 0;
-  setMockCurrentValue(-0.001);
-  obc_error_code_t err = getINA230Current(INA230_I2C_ADDRESS_ONE, &current);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(current, -0.001);
+TEST(TestINA230, CurrentInvalidArguments ) {
+    float current = 0;
+    EXPECT_EQ(getINA230Current(INA230_I2C_ADDRESS_ONE, NULL), OBC_ERR_CODE_INVALID_ARG);
+    EXPECT_EQ(getINA230Current(0b1010110, &current), OBC_ERR_CODE_INVALID_ARG);
 }
 
-TEST(TestINA230, CurrentMinRegisterVal) {
-  // current register is 16-bits, so min value is 0x0000
-  float current = 0;
-  setMockCurrentValue(0);
-  obc_error_code_t err = getINA230Current(INA230_I2C_ADDRESS_ONE, &current);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(current, 0); }
+TEST(TestINA230, Current_I2CAddress) {
+  float power = 0;
+  EXPECT_EQ(getINA230Current(INA230_I2C_ADDRESS_TWO, &power), OBC_ERR_CODE_SUCCESS);
+  EXPECT_EQ(getINA230Current(INA230_DEVICE_COUNT, &power), OBC_ERR_CODE_INVALID_ARG);
+  EXPECT_EQ(getINA230CurrentForDevice(5, &power), OBC_ERR_CODE_INVALID_ARG);
+}
 
-TEST(TestINA230, CurrentLowestVal) {
-  // current register is 16-bits, so min value is 0x8000
-  // use 2's complement on 0x8000 = 1000 0000 0000 0000 --> -32768
-  // since each bit is 1 mA, -32768 * 0.001 = -32.768
-  // in mock_i2c_hal.c, i2cReadReg() returns 0x8000
-  float current = 0;
-  setMockCurrentValue(-32.768);
-  obc_error_code_t err = getINA230Current(INA230_I2C_ADDRESS_ONE, &current);
-  EXPECT_EQ(err, OBC_ERR_CODE_SUCCESS);
-  EXPECT_FLOAT_EQ(current, -32.768); }
+
