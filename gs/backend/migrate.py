8ee187d8252bcc4c15e00b43500fe835eb591103
Edison Wang <ew040607@gmail.com>
2026-01-13 23:02:40 -0500

    Eddie/callsign upgrade (#642)
    # Purpose
    Closes #634 . Click to open!
    
    # New Changes
    
    - Replaced the old callsign csv with the one containing all data
    - Extended AROUserCallsigns table to accommodate added fields
    - Refactored callsigns.py to parse all values of csv
    - Created migrate.py in backend directory to serve as the data migration
    command handler with input validation
    - Created alembic migration file for callsign table
    
    Call ```python3 gs/backend/migrate.py``` to migrate all data.
    
    Call ```python3 gs/backend/migrate.py callsigns``` or ```python3
    gs/backend/migrate.py commands``` to migrate only callsigns or only main
    commands respectively.
    
    # Testing
    Explain tests that you ran to verify code functionality.
    - [x] I have unit-tested this PR. Otherwise, explain why it cannot be
    unit-tested.
    
    See screenshots below.
    
    - [x] I have included screenshots of the tests performed below.
    
    <img width="695" height="159" alt="Screenshot 2025-11-22 at 6 37 09 PM"
    src="https://github.com/user-attachments/assets/b35dca92-86a4-46c7-b4f6-4a5bca9701be"
    />
    
    <img width="1275" height="237" alt="Screenshot 2025-11-22 at 6 39 01 PM"
    src="https://github.com/user-attachments/assets/95d1968b-1d58-42d5-a944-ceca379b770f"
    />
    
    # Outstanding Changes
    If there are non-critical changes (i.e. additional features) that can be
    made to this feature in the future, indicate them here.
    
    Someone please create a migration guide, took me a while to figure it
    out.

---
+++ gs/backend/migrate.py
@@ -0,0 +1,21 @@
+import sys
+
+from gs.backend.data.database.engine import get_db_session
+from gs.backend.data.resources.utils import add_callsigns, add_main_commands
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        raise ValueError(f"Invalid input. Expected at most 1 argument, received {len(sys.argv)}")
+    elif len(sys.argv[1:]) == 0:
+        print("Migrating callsign data...")
+        add_callsigns(get_db_session())
+        print("Migrating main command data...")
+        add_main_commands(get_db_session())
+    elif sys.argv[1] == "callsigns":
+        print("Migrating callsign data...")
+        add_callsigns(get_db_session())
+    elif sys.argv[1] == "commands":
+        print("Migrating main command data...")
+        add_main_commands(get_db_session())
+    else:
+        raise ValueError("Invalid input. Optional arguments include 'callsigns' or 'commands'.")
