ff60a57eacd08005d8fbdf2613fa9a88a609e1a1
Edison Wang <ew040607@gmail.com>
2026-01-14 15:49:22 -0500

    add documentation for migraion command system

--- gs/backend/migrate.py
+++ gs/backend/migrate.py
@@ -3,6 +3,16 @@ import sys
 from gs.backend.data.database.engine import get_db_session
 from gs.backend.data.resources.utils import add_callsigns, add_main_commands
 
+"""
+To migrate pre-determined datainto your local database,
+you can run `python3 gs/backend/migrate.py` from the top
+level directory.
+
+Alternatively, you can include `callsigns` or `commands`
+as command arguments to migrate those respective datasets
+individually.
+"""
+
 if __name__ == "__main__":
     if len(sys.argv) > 2:
         raise ValueError(f"Invalid input. Expected at most 1 argument, received {len(sys.argv)}")
