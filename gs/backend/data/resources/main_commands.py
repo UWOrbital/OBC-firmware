e3a8c430b3f683819132bca6172d6ca8ee6a3726
Kai Zhang <68821592+Scr4tch587@users.noreply.github.com>
2026-01-16 18:59:52 -0500

    Implement Main Table Migration (#653)
    # Purpose
    Closes #630.
    Explain the purpose of the PR here if it doesn't match the linked issue.
    Be sure to add a comment in the linked issue explaining the changes.
    
    # New Changes
    Implemented main_commands.py, main_telemetry.py, main_commands.csv,
    telemetry.csv, updated migration.py to include telemetry
    
    # Testing
    Explain tests that you ran to verify code functionality.
    - [x] I have unit-tested this PR. Otherwise, explain why it cannot be
    unit-tested.
    - [ ] I have tested this PR on a board if the code will run on a board
    (Only required for firmware developers).
    - [ ] I have tested this PR by running the ARO website (Only required if
    the code will impact the ARO website).
    - [ ] I have tested this PR by running the MCC website (Only required if
    the code will impact the MCC website).
    - [x] I have included screenshots of the tests performed below.
    HOW TO TEST:
    1. Follow database setup from documentation
    2. Make sure existing main command / telemetry data is cleared
    psql -U username -d db
    gs=# DELETE FROM main.commands;
    gs=# DELETE FROM main.telemetry;
    3. Run the migration script (make sure venv is active)
    python3 gs/backend/migrate.py
    4. Verify the main commands and telemetry in the DB
    psql -U username -d db
    gs=# SELECT id FROM main.commands ORDER BY id;
    gs=# SELECT id FROM main.telemetry ORDER BY id;
    
    <img width="1360" height="136" alt="image"
    src="https://github.com/user-attachments/assets/89c085f3-2d4b-4033-8575-0fa3b88abc35"
    />
    <img width="804" height="590" alt="image"
    src="https://github.com/user-attachments/assets/4b083f9b-8282-4921-b2cd-80a9ffe92f24"
    />
    <img width="1322" height="818" alt="image"
    src="https://github.com/user-attachments/assets/8b203c33-c08d-4252-8ad9-a9004146f13d"
    />
    
    
    # Outstanding Changes
    If there are non-critical changes (i.e. additional features) that can be
    made to this feature in the future, indicate them here.

--- gs/backend/data/resources/main_commands.py
+++ gs/backend/data/resources/main_commands.py
@@ -1,27 +1,23 @@
+import csv
+
 from gs.backend.data.tables.main_tables import MainCommand
 
 
 # TODO: Pull this from a config file so that it's synced with the OBC
-# TODO: Add more main commands
 def main_commands() -> list[MainCommand]:
     """
     Returns the list of main commands
     """
-    return [
-        MainCommand(
-            id=1,
-            name="RTC Sync",
-            params="time",
-            format="int 7 bytes",
-            data_size=7,
-            total_size=8,
-        ),
-        MainCommand(
-            id=2,
-            name="Manually activate an emergency mode for a specified amount of time",
-            params="mode_state_number,time",
-            format="int 1 byte, int 7 bytes",
-            data_size=8,
-            total_size=9,
-        ),
-    ]
+    with open("gs/backend/data/resources/main_commands.csv", newline="") as csvfile:
+        file = csv.reader(csvfile)
+        return [
+            MainCommand(
+                id=int(row[0]),
+                name=row[1],
+                params=row[2].strip() or None,
+                format=row[3].strip() or None,
+                data_size=int(row[4]),
+                total_size=int(row[5]),
+            )
+            for row in file
+        ]
--- gs/backend/migrate.py
+++ gs/backend/migrate.py
@@ -1,14 +1,14 @@
 import sys
 
 from gs.backend.data.database.engine import get_db_session
-from gs.backend.data.resources.utils import add_callsigns, add_main_commands
+from gs.backend.data.resources.utils import add_callsigns, add_main_commands, add_telemetry
 
 """
 To migrate pre-determined datainto your local database,
 you can run `python3 gs/backend/migrate.py` from the top
 level directory.
 
-Alternatively, you can include `callsigns` or `commands`
+Alternatively, you can include `callsigns`, `commands`, or 'telemetries'
 as command arguments to migrate those respective datasets
 individually.
 """
@@ -21,11 +21,18 @@ if __name__ == "__main__":
         add_callsigns(get_db_session())
         print("Migrating main command data...")
         add_main_commands(get_db_session())
-    elif sys.argv[1] == "callsigns":
-        print("Migrating callsign data...")
-        add_callsigns(get_db_session())
-    elif sys.argv[1] == "commands":
-        print("Migrating main command data...")
-        add_main_commands(get_db_session())
+        print("Migrating telemetry data...")
+        add_telemetry(get_db_session())
     else:
-        raise ValueError("Invalid input. Optional arguments include 'callsigns' or 'commands'.")
+        match sys.argv[1]:
+            case "callsigns":
+                print("Migrating callsign data...")
+                add_callsigns(get_db_session())
+            case "commands":
+                print("Migrating main command data...")
+                add_main_commands(get_db_session())
+            case "telemetries":
+                print("Migrating telemetry data...")
+                add_telemetry(get_db_session())
+            case _:
+                raise ValueError("Invalid input. Optional arguments include 'callsigns', 'commands', or 'telemetries'.")
