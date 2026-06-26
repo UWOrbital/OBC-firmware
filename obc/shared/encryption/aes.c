fab3545c09391159f5d59c63bc18e9960716603f
GitWorkingTime <dlee28262@gmail.com>
2026-06-26 12:01:32 -0400

    Added structure for aes-128

--- obc/shared/encryption/aes.c
+++ obc/shared/encryption/aes.c
@@ -1,6 +1,38 @@
+// ─── Imports ────────────────────────────────────────────────────
 #include <stdio.h>
 
+/**
+ * Reference for AES-128: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197-upd1.pdf
+ */
+// ─── Transformations/Operations ─────────────────────────────────
+
+void subBytes() { return; }
+
+void shiftRows() { return; }
+
+void mixColumns() { return; }
+
+void addRoundKey() { return; }
+
+void keyExpansion() { return; }
+
+void addRoundKey() { return; }
+
+void invShiftRows() { return; }
+
+void invSubBytes() { return; }
+
+void invMixColumns() { return; }
+
+void sanitizeKey() { return; }
+
+// ─── User function ──────────────────────────────────────────────
+void encrypt() { return; }
+
+void decrypt() { return; }
+
+// Used for testing during development
 int main() {
-  printf("Hello World!\n");
+  encrypt();
   return 0;
 }
